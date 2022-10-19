22d9152c796124da9d85ce6ed91e9fc2358b1caf
Jojish Pathrose <jojish.pathrose@speridian.com>
2022-10-19 16:09:15 -0600

    (fix) case360 page routing issue fixed.

--- cms/libs/case-management/feature-case/src/lib/case-management-feature-case-routing.module.ts
+++ cms/libs/case-management/feature-case/src/lib/case-management-feature-case-routing.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CasePageComponent } from './containers/case-page/case-page.component';
+import { Case360PageComponent } from './containers/case360-page/case360-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,13 @@ const routes: Routes = [
       title: null
     }, 
   },  
+  {
+    path: 'case360/:id',
+    component: Case360PageComponent,
+    data: {
+      title: null,
+    }, 
+  },
 ];
 
 @NgModule({
