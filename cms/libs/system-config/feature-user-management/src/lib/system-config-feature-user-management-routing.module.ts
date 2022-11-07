e938a0753815894ede39920043b2f1fed4188df6
Dinesh <dinesh.panneerselvam@speridian.com>
2022-11-07 22:44:29 +0530

    fixing system config imports

--- cms/libs/system-config/feature-user-management/src/lib/system-config-feature-user-management-routing.module.ts
+++ cms/libs/system-config/feature-user-management/src/lib/system-config-feature-user-management-routing.module.ts
@@ -1,8 +1,10 @@
 /** Angular **/
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { GenderListComponent, LanguageListComponent, PronounsListComponent, RacialOrEthnicIdentityListComponent, SexualOrientationListComponent } from '@cms/system-config/feature-client-profile-management';
-// import { CaseAvailabilityListComponent, EidLifetimePeriodListComponent, SlotListComponent } from 'libs/system-config/feature-housing-coordination-management/src';
+import { GenderListComponent, LanguageListComponent, PronounsListComponent, RacialOrEthnicIdentityListComponent, SexualOrientationListComponent } from '@cms/system-config/feature-client-profile-management';
+import { ExpenseTypesListComponent, FundsListComponent, IncomeTypesListComponent, IndexListComponent, PcaCodesListComponent } from '@cms/system-config/feature-financials';
+import { CaseAvailabilityListComponent, EidLifetimePeriodListComponent, HousingAcuityLevelListComponent, IncomeInclusionsExclusionsListComponent, PsMfrZipListComponent, RegionAssignmentListComponent, ServiceProviderListComponent, SlotListComponent } from '@cms/system-config/feature-housing-coordination-management';
+import { EmailTemplateListComponent, LetterTemplateListComponent, SmsTextTemplateListComponent } from '@cms/system-config/feature-communication';
 import { RolesAndPermissionsDetailComponent } from './components/roles-and-permissions-detail/roles-and-permissions-detail.component';
 import { RolesAndPermissionsListComponent } from './components/roles-and-permissions-list/roles-and-permissions-list.component';
 import { UserListComponent } from './components/user-list/user-list.component';
@@ -25,38 +27,94 @@ const routes: Routes = [
         redirectTo: 'users',
         pathMatch: 'full',
       },
-      // {
-      //   path: 'pronouns',
-      //   component: PronounsListComponent,
-      // },
-      // {
-      //   path: 'gender',
-      //   component: GenderListComponent,
-      // },
-      // {
-      //   path: 'sexual-orientation',
-      //   component: SexualOrientationListComponent,
-      // },
-      // {
-      //   path: 'racial-or-ethnic-identity',
-      //   component: RacialOrEthnicIdentityListComponent,
-      // },
-      // {
-      //   path: 'languages',
-      //   component: LanguageListComponent,
-      // },
-      // {
-      //   path: 'slots',
-      //   component: SlotListComponent,
-      // },
-      // {
-      //   path: 'case-availability',
-      //   component: CaseAvailabilityListComponent,
-      // },
-      // {
-      //   path: 'eid-period',
-      //   component: EidLifetimePeriodListComponent,
-      // },
+      {
+        path: 'pronouns',
+        component: PronounsListComponent,
+      },
+      {
+        path: 'gender',
+        component: GenderListComponent,
+      },
+      {
+        path: 'sexual-orientation',
+        component: SexualOrientationListComponent,
+      },
+      {
+        path: 'racial-or-ethnic-identity',
+        component: RacialOrEthnicIdentityListComponent,
+      },
+      {
+        path: 'languages',
+        component: LanguageListComponent,
+      },
+      {
+        path: 'slots',
+        component: SlotListComponent,
+      },
+      {
+        path: 'case-availability',
+        component: CaseAvailabilityListComponent,
+      },
+      {
+        path: 'eid-period',
+        component: EidLifetimePeriodListComponent,
+      },
+      {
+        path: 'housing-acuity-level',
+        component: HousingAcuityLevelListComponent,
+      },
+      {
+        path: 'income-inclusions-exlusions',
+        component: IncomeInclusionsExclusionsListComponent
+      },
+      
+      {
+        path: 'region-assignment',
+        component: RegionAssignmentListComponent
+      },
+      
+      {
+        path: 'service-provider',
+        component: ServiceProviderListComponent
+      },
+      {
+        path: 'ps-fmr-zip',
+        component: PsMfrZipListComponent
+      },
+      {
+        path: 'funds',
+        component: FundsListComponent
+      },
+      {
+        path: 'expende-types',
+        component: ExpenseTypesListComponent
+      },
+      {
+        path: 'income-types',
+        component: IncomeTypesListComponent
+      },
+      {
+        path: 'index',
+        component: IndexListComponent
+      },
+      {
+        path: 'pca-codes',
+        component: PcaCodesListComponent
+      },
+      {
+        path: 'email-template',
+        component: EmailTemplateListComponent
+      },
+      {
+        path: 'letter-template',
+        component: LetterTemplateListComponent
+      },
+      {
+        path: 'sms-text-template',
+        component: SmsTextTemplateListComponent
+      },
+     
+      
     ],
   },
   {
