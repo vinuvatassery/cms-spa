832d49e1aa85142096a7bd065b46c5a6944dca4b
Dinesh <dinesh.panneerselvam@speridian.com>
2022-11-10 14:30:00 +0530

    #1408 updated the kendo input  fillmode style method and fixed issue in detailed and list components

--- cms/libs/system-config/feature-housing-coordination-management/src/lib/components/housing-acuity-level-detail/housing-acuity-level-detail.component.ts
+++ cms/libs/system-config/feature-housing-coordination-management/src/lib/components/housing-acuity-level-detail/housing-acuity-level-detail.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { UIFormStyle } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'system-config-housing-acuity-level-detail',
   templateUrl: './housing-acuity-level-detail.component.html',
@@ -8,7 +8,7 @@ import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HousingAcuityLevelDetailComponent implements OnInit {
-
+  public formUiStyle : UIFormStyle = new UIFormStyle();
   constructor() { }
 
   ngOnInit(): void {
--- cms/libs/system-config/feature-housing-coordination-management/src/lib/components/ps-mfr-zip-detail/ps-mfr-zip-detail.component.ts
+++ cms/libs/system-config/feature-housing-coordination-management/src/lib/components/ps-mfr-zip-detail/ps-mfr-zip-detail.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { UIFormStyle } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'system-config-ps-mfr-zip-detail',
   templateUrl: './ps-mfr-zip-detail.component.html',
@@ -8,7 +8,7 @@ import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PsMfrZipDetailComponent implements OnInit {
-
+  public formUiStyle : UIFormStyle = new UIFormStyle();
   constructor() { }
 
   ngOnInit(): void {
