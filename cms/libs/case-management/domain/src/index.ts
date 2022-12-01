0310eef0356027ddb593a132f14904bf21771797
Anoop Jose <anoop.jose@speridian.com>
2022-12-01 13:07:26 -0800

    Merged PR 59: Smoking Cessation Implementation
    If the User select Yes will save the smoking cessation note with flag
    else will update the no flag in database
    
    Related work items: #1661, #1662, #1663, #1664, #1665, #1666, #1950, #2078

--- cms/libs/case-management/domain/src/index.ts
+++ cms/libs/case-management/domain/src/index.ts
@@ -20,14 +20,17 @@ export * from './lib/entities/healthcare-provider';
 export * from './lib/entities/provider';
 export * from './lib/entities/search';
 export * from './lib/entities/verification';
+export * from './lib/entities/smoking-cessation';
 export * from './lib/entities/workflow';
 export * from './lib/entities/workflow-stage-completion-status';
+
 
 /** Enums **/
 export * from './lib/enums/communication-event.enum';
 export * from './lib/enums/screen-type.enum';
 export * from './lib/enums/workflow-type.enum';
 export * from './lib/enums/case-screen-tab.enum';
+export * from './lib/enums/yes-no-flag.enum';
 export * from './lib/enums/navigation-type.enum';
 export * from './lib/enums/status-flag.enum';
 export * from './lib/enums/adjustment-operator.enum';
