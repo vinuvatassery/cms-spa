5cf698f7e650776269ed4dbf27e4fb22df05d825
Mahesh K Chacko <mahesh.chacko@speridian.com>
2022-12-21 15:43:49 -0700

    #1757: New Case - HIV Healthcare Provider Section -Search for Providers - UI

--- cms/libs/case-management/feature-healthcare-provider/src/index.ts
+++ cms/libs/case-management/feature-healthcare-provider/src/index.ts
@@ -5,3 +5,6 @@ export * from './lib/case-management-feature-healthcare-provider.module';
 export * from './lib/containers/healthcare-provider-page/healthcare-provider-page.component';
 export * from './lib/components/health-care-provider-list/health-care-provider-list.component';
 export * from './lib/components/health-care-provider-detail/health-care-provider-detail.component';
+export * from './lib/components/health-care-provider-search/health-care-provider-search.component';
+export * from './lib/components/health-care-provider-card/health-care-provider-card.component';
+export * from './lib/components/remove-health-care-provider-confirmation/remove-health-care-provider-confirmation.component';
