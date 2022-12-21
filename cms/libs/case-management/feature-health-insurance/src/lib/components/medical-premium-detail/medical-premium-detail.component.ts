e941fd211f16fe86cc7442619b0b66e5c43dec8b
Anoop V J <anoop.jose@speridian.com>
2022-12-21 23:31:38 +0530

    920:changes
    920:changes

--- cms/libs/case-management/feature-health-insurance/src/lib/components/medical-premium-detail/medical-premium-detail.component.ts
+++ cms/libs/case-management/feature-health-insurance/src/lib/components/medical-premium-detail/medical-premium-detail.component.ts
@@ -359,7 +359,7 @@ export class MedicalPremiumDetailComponent implements OnInit,OnChanges  {
           (data:any) => {
               this.onModalCloseClicked();
                this.loaderService.hide();
-               this.insurancePolicyFacade.ShowHideSnackBar(SnackBarNotificationType.SUCCESS , 'Insurance plan saved successfully.');
+               this.insurancePolicyFacade.ShowHideSnackBar(SnackBarNotificationType.SUCCESS , 'Insurance plan updated successfully.');
           },
           (error:any) => {
               if(error){
