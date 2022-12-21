ca95b61b4cdbf261cdca8c3048113c3c5c14b368
sobia <sobia.kiran@speridian.com>
2022-12-21 22:51:16 +0500

    integration_prescription_drug
    integration_prescription_drug

---
+++ cms/libs/case-management/domain/src/lib/infrastructure/prescription-drug.data.service.ts
@@ -0,0 +1,25 @@
+/** Angular **/
+import { Injectable } from '@angular/core';
+/** External libraries **/
+import { HttpClient } from '@angular/common/http';
+import { ConfigurationProvider } from '@cms/shared/util-core';
+import { PrescriptionDrug } from '../entities/prescription-drug';
+
+@Injectable({ providedIn: 'root' })
+export class PrescriptionDrugDataService {
+  /** Constructor**/
+  constructor(private readonly http: HttpClient, private readonly configurationProvider: ConfigurationProvider) {}
+
+  /** Public methods **/
+  updatePrescriptionDrugService(prescriptionDrug: any) {
+    return this.http.put(
+      `${this.configurationProvider.appSettings.caseApiUrl}` +
+        `/case-management/prescription-drug/prescription`, prescriptionDrug
+    );
+  }
+  loadPrescriptionDrug(clientCaseEligibilityId:any){
+    return this.http.get<PrescriptionDrug>(
+      `${this.configurationProvider.appSettings.caseApiUrl}/case-management/prescription-drug?clientCaseEligibilityId=${clientCaseEligibilityId}`,);
+  }
+
+}
