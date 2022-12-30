fb969ac484ad1b2f0599960ab6e719d7207a9bbd
Jojish Pathrose <jojish.pathrose@speridian.com>
2022-12-30 09:34:15 -0800

    Merged PR 178: Total count and Completed count implementation.
    Workflow count implemented till prescription and drugs.
    - Case Details
    - Applicant Info
    - Contact Info
    - Family Dependents
    - Income
    - Employment
    - Smoking Cessation
    - Health Insurance
    - Prescription and Drugs (Partial, still some functionalities are pending)
    
    Related work items: #2695

--- cms/libs/case-management/feature-client/src/lib/components/client-edit-view-gender/client-edit-view-gender.component.ts
+++ cms/libs/case-management/feature-client/src/lib/components/client-edit-view-gender/client-edit-view-gender.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { CompletionChecklist, StatusFlag, WorkflowFacade } from '@cms/case-management/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { LovFacade } from '@cms/system-config/domain';
 
@@ -19,9 +20,11 @@ export class ClientEditViewGenderComponent implements OnInit {
   GenderLovs$ = this.lovFacade.genderlov$;
   ControlPrefix = 'Gender';
   DescriptionField = 'GenderDescription';
+  private countOfSelection=0; 
   constructor(
     private readonly lovFacade: LovFacade,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private readonly workflowFacade : WorkflowFacade
   ) {
     this.appInfoForm = this.formBuilder.group({ Gender: [''] });
   }
@@ -29,6 +32,7 @@ export class ClientEditViewGenderComponent implements OnInit {
   ngOnInit(): void {
     this.lovFacade.getGenderLovs();
     this.loadGendersLov();
+    this.formChangeSubscription();
   }
   private loadGendersLov() {
     this.GenderLovs$.subscribe((data) => {
@@ -48,6 +52,29 @@ export class ClientEditViewGenderComponent implements OnInit {
     });
   }
 
+  private formChangeSubscription(){
+    this.appInfoForm.controls['GenderGroup'].valueChanges.subscribe(value=>{
+      if(value && this.countOfSelection >= 0){
+        this.updateWorkflowCount(true);
+        this.countOfSelection++;
+      }else{
+        this.countOfSelection = this.countOfSelection > 0 ? --this.countOfSelection : this.countOfSelection;
+        if(this.countOfSelection <= 0){
+          this.updateWorkflowCount(false);
+        }
+      }
+    });
+  }
+
+  private updateWorkflowCount(isCompleted:boolean){
+    const workFlowdata: CompletionChecklist[] = [{
+      dataPointName: 'gender',
+      status: isCompleted ? StatusFlag.Yes : StatusFlag.No
+    }];
+  
+    this.workflowFacade.updateChecklist(workFlowdata);
+  }
+
   onCheckChange(event: any, lovCode: string) {
     if (event.target.checked) {
       this.appInfoForm.controls['GenderGroup'].setValue(lovCode);
@@ -55,14 +82,14 @@ export class ClientEditViewGenderComponent implements OnInit {
         this.appInfoForm.controls[this.DescriptionField].setErrors({
           incorrect: true,
         });
-      }
+      }      
     } else {
       this.appInfoForm.controls['GenderGroup'].setValue('');
 
       if (lovCode === 'NOT_LISTED') {
         this.appInfoForm.controls[this.DescriptionField].setErrors(null);
         this.appInfoForm.controls[this.DescriptionField].setValue('');
-      }
-    }
+      }     
+    }   
   }
 }
