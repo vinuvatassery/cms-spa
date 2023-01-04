c58ea812375ec59f978be3c748d68851d5573617
Anoop V J <anoop.jose@speridian.com>
2023-01-04 12:55:12 +0530

    1303: added changes
    1303: added changes for url

--- cms/libs/case-management/domain/src/lib/infrastructure/search.data.service.ts
+++ cms/libs/case-management/domain/src/lib/infrastructure/search.data.service.ts
@@ -18,7 +18,7 @@ export class SearchDataService {
   loadCaseByHeaderSearchText(text : string) {     
     return this.http.get<ClientCase[]>(
       `${this.configurationProvider.appSettings.caseApiUrl}`+
-      `/case-management/client-header/SearchText=${text}`
+      `/case-management/client-search/SearchText=${text}`
     );   
 
 }
