b06455ba69b0afc285dac7dcd66f1f136e205b6e
Talha Ashraf <talha.ashraf@speridian.com>
2023-01-11 19:45:27 +0500

    implemented Duplicate client Popup UI

--- cms/libs/case-management/feature-client/src/lib/components/duplicate-client-found/duplicate-client-found.component.ts
+++ cms/libs/case-management/feature-client/src/lib/components/duplicate-client-found/duplicate-client-found.component.ts
@@ -1,10 +1,60 @@
 /** Angular **/
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-
+import {
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { CaseFacade, CaseStatusCode } from '@cms/case-management/domain';
+import { LoaderService, LoggingService, SnackBarNotificationType, NotificationSnackbarService } from '@cms/shared/util-core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'case-management-duplicate-client-found',
   templateUrl: './duplicate-client-found.component.html',
   styleUrls: ['./duplicate-client-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DuplicateClientFoundComponent {}
+export class DuplicateClientFoundComponent implements OnInit {
+
+  @Input() currentClientInfo: any = {};
+  @Input() matchingClientInfo: any = {};
+
+  @Output() closeModalClick = new EventEmitter<any>();
+  ssn: string = '';
+  constructor(private router: Router,
+    private caseFacade: CaseFacade,
+    private loaderService: LoaderService,
+    private loggingService: LoggingService) {
+
+  }
+
+  ngOnInit(): void {
+    if (this.currentClientInfo.ssn != this.matchingClientInfo.ssn) {
+      this.ssn = '000-00-0000'
+    }
+    else {
+      this.ssn = this.currentClientInfo.ssn
+    }
+  }
+
+  onDuplicateFoundClick() {
+    this.loaderService.show();
+    this.caseFacade.cancelCase(this.currentClientInfo.clientCaseId, CaseStatusCode.CANCELLED).subscribe({
+      next: (response: any) => {
+        this.router.navigate([`/case-management/cases/case360/${this.matchingClientInfo.clientId}`])
+        this.closeModalClick.next(true);
+        this.loaderService.hide();
+      },
+      error: (err: any) => {
+        this.loaderService.hide();
+        this.loggingService.logException(err);
+      }
+    })
+  }
+
+  onNotaDuplicateClick() {
+    this.closeModalClick.next(true);
+  }
+}
+
