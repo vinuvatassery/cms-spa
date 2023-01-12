ecde6e939d8e0061df016f4dcccc80fd03c037ff
Talha Ashraf <talha.ashraf@speridian.com>
2023-01-12 11:11:53 -0800

    Merged PR 232: #1302 - (feat): Possible Duplicate Client
    -Integration of backend api to search duplicate client
    -implement duplicate client popup functionality
    
    Related work items: #1302, #2674, #2675

--- cms/libs/shared/ui-tpa/src/lib/kendo-form-element-config.ts
+++ cms/libs/shared/ui-tpa/src/lib/kendo-form-element-config.ts
@@ -1,5 +1,7 @@
  
-import { NumberFormatOptions } from "@progress/kendo-angular-intl";
+import { Injectable } from '@angular/core';
+import { groupBy } from '@progress/kendo-data-query';
+import { NumberFormatOptions, IntlService } from "@progress/kendo-angular-intl";
 import { FileRestrictions } from '@progress/kendo-angular-upload';
 export class UploadFileRistrictionOptions  {
  public fileRestrictions: FileRestrictions = {
@@ -16,3 +18,22 @@ export class CurrencyFormat  {
   };
 }
 
+@Injectable({ providedIn: 'root' })
+export class IntlDateService {
+
+  constructor(public intl: IntlService){
+
+  }
+
+  formatDate(date:any,format:any){
+    return this.intl.formatDate(date, format);
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class DataQuery {
+
+  groupBy(data:any,options:any){
+    return groupBy(data,options); 
+  }
+}
