faf0c56fb6f1123eb9dea4df6927a487925bc384
Muhammad Umer1 <muhammad.umer1@speridian.com>
2023-01-17 10:58:42 -0800

    Merged PR 265: Income UI bugs
    Some UI Bug Fixes For income section.
    
    Related work items: #3340

--- cms/libs/case-management/domain/src/index.ts
+++ cms/libs/case-management/domain/src/index.ts
@@ -67,6 +67,8 @@ export * from './lib/enums/partb-medicare-type.enum';
 export * from './lib/enums/case-status-code.enum';
 export * from './lib/enums/pronoun-code.enum';
 export * from './lib/enums/button-type.enum';
+export * from './lib/enums/income-type-code.enum';
+
 
 /** Data services **/
 export * from './lib/infrastructure/authorization.data.service';
