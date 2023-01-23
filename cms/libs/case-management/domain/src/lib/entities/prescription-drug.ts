b5d4cc7b4d61459b35a44d6bf8d3c58d84d0fe52
Jojish Pathrose <jojish.pathrose@speridian.com>
2023-01-23 10:16:46 -0800

    Merged PR 324: Prescription Drugs
    Related work items: #932

--- cms/libs/case-management/domain/src/lib/entities/prescription-drug.ts
+++ cms/libs/case-management/domain/src/lib/entities/prescription-drug.ts
@@ -1,8 +1,18 @@
 export interface PrescriptionDrug {
-   clientCaseEligibilityId :string
-   clientId:string
-   clientCaseId:string
-   hivPositiveFlag :  string 
-   nonPreferredPharmacyFlag :  string 
-  }
-  
+   clientCaseEligibilityId: string;
+   clientId: string;
+   clientCaseId: string;
+   prescriptionDrugsForHivCode: string;
+   nonPreferredPharmacyCode: string;
+   noSummaryOfBenefitsFlag: string;
+   concurrencyStamp: string;
+   document: PrescriptionDrugDocument;
+}
+
+export interface PrescriptionDrugDocument {
+   documentId?: string;
+   documentName?: string;
+   documentPath?: string;
+   documentSize?: string;
+   concurrencyStamp?: string;
+}
