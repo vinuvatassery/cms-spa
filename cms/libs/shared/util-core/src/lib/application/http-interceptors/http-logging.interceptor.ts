b7c2c4c4845b6b4e7a3447008db1232d219e7509
Ajeesh Chandran <Ajeesh.chandran@speridian.com>
2023-02-15 22:30:38 +0000

    Updated http-logging.interceptor.ts

--- cms/libs/shared/util-core/src/lib/application/http-interceptors/http-logging.interceptor.ts
+++ cms/libs/shared/util-core/src/lib/application/http-interceptors/http-logging.interceptor.ts
@@ -28,6 +28,7 @@ export class HttpLoggingInterceptor implements HttpInterceptor {
       finalize(() => {
         const elapsed = Date.now() - started;
         const msg = `${req.method} "${req.urlWithParams}" ${ok} in ${elapsed} ms.`;
+        //TODO: uncomment this later after reviewing performance.
         console.log(msg);
       })
     );
