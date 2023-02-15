686f3e368281accb01a7d044390d8a6288cf6475
Ajeesh Chandran <Ajeesh.chandran@speridian.com>
2023-02-15 14:32:20 -0800

    Merged PR 517: add'l changes in v0.0.7 into main

--- cms/libs/shared/util-core/src/lib/application/http-interceptors/http-logging.interceptor.ts
+++ cms/libs/shared/util-core/src/lib/application/http-interceptors/http-logging.interceptor.ts
@@ -28,7 +28,8 @@ export class HttpLoggingInterceptor implements HttpInterceptor {
       finalize(() => {
         const elapsed = Date.now() - started;
         const msg = `${req.method} "${req.urlWithParams}" ${ok} in ${elapsed} ms.`;
-        //console.log(msg);
+        //TODO: uncomment this later after reviewing performance.
+        console.log(msg);
       })
     );
   }
