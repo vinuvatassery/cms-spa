b8ab6832667301936bddbd052bc8025be80e3b53
Muhammad Umer <muhammad.umer1@speridian.com>
2023-03-22 11:47:04 +0500

    Sort banner changes

--- cms/libs/case-management/feature-case/src/lib/components/case-list/case-list.component.ts
+++ cms/libs/case-management/feature-case/src/lib/components/case-list/case-list.component.ts
@@ -22,7 +22,9 @@ export class CaseListComponent implements OnInit, OnChanges {
 
 public isGridLoaderShow = true;
 @Input() searchLoaderVisibility$!: Observable<boolean>;
-
+sortColumn = "Client Name";
+sortDir = "Ascending";
+columnsReordered = false;
 
 public state!: State;
   /*** Input properties ***/
@@ -33,6 +35,26 @@ public state!: State;
   @Input() sort : any;
   @Input() columnDroplist$ : any;
   @Input() selectedTab: CaseScreenTab = 0;
+  columns : any = {
+    clientFullName:"Client Name",
+    officialIdFullName:"Name on Official ID",
+    insuranceFullName:"Name on Primary Insurance Card",
+    pronouns:"Pronouns",
+    clientId:"ID",
+    urn:"URN",
+    preferredContact:"Preferred Contact",
+    caseStatus:"Status",
+    group:"Group",
+    eilgibilityStartDate:"Eligibility Start Date",
+    eligibilityEndDate:"Eligibility End Date",
+    email:"Email",
+    phone:"Phone",
+    genders:"Gender",
+    homeAddress:"Home Address",
+    ssn:"SSN",
+    insurancePolicyId:"Insurance Policy Id",
+    assignedCw:"Assigned to"
+  }
 
   /** Public properties **/
   ddlGridColumns$ = this.caseFacade.ddlGridColumns$;
@@ -74,6 +96,8 @@ public state!: State;
     this.sortValue = stateData.sort[0]?.field ?? 'clientFullName'
     this.sortType = stateData.sort[0]?.dir ?? 'asc'
     this.state=stateData;
+    this.sortColumn = this.columns[stateData.sort[0]?.field];
+    this.sortDir = stateData.sort[0]?.dir === 'asc'? 'Ascending': stateData.sort[0]?.dir === 'desc'? 'Descending': "";
     this.loadProfileCasesList();
 }
   private loadProfileCasesList(): void {
@@ -107,4 +131,28 @@ public state!: State;
     this.filter = event;
     this.loadProfileCasesList();
   }
+  setToDefault()
+  {
+    //this.grid.reorderColumn([]);
+    this.pageSizes = this.caseFacade.gridPageSizes;
+    this.sortValue  = this.caseFacade.sortValue;
+    this.sortType  = this.caseFacade.sortType;
+    this.sort  = this.caseFacade.sort;
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort
+      };
+    this.sortColumn = this.columns[this.sort[0]?.field];
+    this.sortDir = this.sort[0]?.dir === 'asc'? 'Ascending': this.sort[0]?.dir === 'desc'? 'Descending': "";
+    this.loadProfileCasesList();
+  }
+  onColumnReorder(event:any)
+  {
+    this.columnsReordered = true;
+  }
+  // onColumnMenuSelect(event : any)
+  // {
+  //   debugger;
+  // }
 }
