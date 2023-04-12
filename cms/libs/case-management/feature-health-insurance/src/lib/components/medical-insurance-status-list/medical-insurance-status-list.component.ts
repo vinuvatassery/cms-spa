85fad4996eb4625bb24a33457db0765581d7b362
Anoop V J <anoop.jose@speridian.com>
2023-04-12 11:21:24 +0530

    2180:changes for the  Health insurance popup
    2180:changes for the  Health insurance popup

--- cms/libs/case-management/feature-health-insurance/src/lib/components/medical-insurance-status-list/medical-insurance-status-list.component.ts
+++ cms/libs/case-management/feature-health-insurance/src/lib/components/medical-insurance-status-list/medical-insurance-status-list.component.ts
@@ -1,5 +1,6 @@
 /** Angular **/
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy,Input } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 /** Facades **/
 import { HealthInsurancePolicyFacade } from '@cms/case-management/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
@@ -22,6 +23,12 @@ export class MedicalInsuranceStatusListComponent implements OnInit {
   public formUiStyle : UIFormStyle = new UIFormStyle(); 
   // gridOptionData: Array<any> = [{ text: 'Options' }];
   popupClassAction = 'TableActionPopup app-dropdown-action-list';
+  isOpenedHealthInsuranceModal:boolean= false;
+  insuranceType!: string;
+  dialogTitle!: string;
+  @Input() healthInsuranceForm: FormGroup;
+  @Input() caseEligibilityId: any;
+  @Input() clientId:any;
   public gridOptionData = [
     {
       buttonType:"btn-h-primary",
@@ -44,15 +51,38 @@ export class MedicalInsuranceStatusListComponent implements OnInit {
   ];
 
   /** Constructor **/
-  constructor( private insurancePolicyFacade: HealthInsurancePolicyFacade) {}
+  constructor( private insurancePolicyFacade: HealthInsurancePolicyFacade, private readonly formBuilder: FormBuilder) {
+    this.healthInsuranceForm = this.formBuilder.group({});
+  }
 
   /** Lifecycle hooks **/
   ngOnInit(): void {
     this.loadHealthInsuranceStatus();
+  }
+  handleHealthInsuranceOpenClicked(value: string) {
+    this.isOpenedHealthInsuranceModal = true;
+    switch (value) {
+      // case 'view':
+      //   this.dialogTitle = 'View';
+      //   this.isEdit = false;
+      //   break;
+      // case 'edit':
+      //   this.dialogTitle = 'Edit';
+      //   this.isEdit = true;
+      //   break;
+      case 'add':
+        this.dialogTitle = 'Add';
+        //this.isEdit = false;
+        break;
+    }
+  }
+  handleHealthInsuranceCloseClicked() {
+    this.isOpenedHealthInsuranceModal = false;
   }
 
   /** Private methods **/
   private loadHealthInsuranceStatus() {
     this.insurancePolicyFacade.loadHealthInsuranceStatus();
   }
+  
 }
--- cms/libs/case-management/feature-health-insurance/src/lib/containers/profile-healthe-insurance/profile-health-insurance-page.component.ts
+++ cms/libs/case-management/feature-health-insurance/src/lib/containers/profile-healthe-insurance/profile-health-insurance-page.component.ts
@@ -1,5 +1,6 @@
 /** Angular **/
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ClientProfileTabs } from '@cms/case-management/domain';
 import { filter, Subject, Subscription } from 'rxjs';
@@ -14,7 +15,8 @@ export class ProfileHealthInsurancePageComponent implements OnInit,OnDestroy {
 
   constructor(
     private route: ActivatedRoute,  
-    private readonly router: Router
+    private readonly router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   tabChangeSubscription$ = new Subscription();
@@ -23,10 +25,15 @@ export class ProfileHealthInsurancePageComponent implements OnInit,OnDestroy {
   profileClientId!: number;
   clientCaseEligibilityId!: any;
   clientCaseId!: any;
+  healthInsuranceForm!: FormGroup;
   tabId! : any
+  clientId: any;
+
   ngOnInit(): void {  
     this.routeChangeSubscription();
     this.loadQueryParams()
+    this.buildForm();
+    this.getQueryParam();
   }
 
   /** Private properties **/
@@ -49,10 +56,55 @@ export class ProfileHealthInsurancePageComponent implements OnInit,OnDestroy {
       });
   }
 
+   /** Private Methods **/
+   private buildForm() {
+    this.healthInsuranceForm = this.formBuilder.group({
+      clientInsurancePolicyId: [''],
+      insuranceType: [''],
+      insuranceStartDate: [''],
+      insuranceEndDate: [''],
+      insuranceIdNumber: [''],
+      insuranceCarrierName: [''],
+      metalLevel: [{}],
+      insurancePlanName: [''],
+      aptcFlag: [''],
+      aptcMonthlyAmt: [''],
+      careassistPayingPremiumFlag: [''],
+      premiumPaidThruDate: [''],
+      nextPremiumDueDate: [''],
+      premiumAmt: [''],
+      premiumFrequencyCode: [''],
+      paymentIdNbr: [''],
+      paymentIdNbrSameAsInsuranceIdNbrFlag: [''],
+      groupPlanType: [''],
+      medicareBeneficiaryIdNbr: [''],
+      medicareCoverageTypeCode: [''],
+      medicarePartAStartDate: [''],
+      medicarePartBStartDate: [''],
+      onQmbFlag: [''],
+      onLisFlag: [''],
+      othersCoveredOnPlanFlag: [''],
+      othersCoveredOnPlan: this.formBuilder.array([]),
+      newOthersCoveredOnPlan: this.formBuilder.array([]),
+      othersCoveredOnPlanSaved: [[]],
+      othersCoveredOnPlanSelection: [''],
+      isClientPolicyHolderFlag: [''],
+      policyHolderFirstName: [''],
+      policyHolderLastName: [''],
+      proofOfPremium: [''],
+      copyOfInsuranceCard: [''],
+      copyOfSummary: ['']
+    });
+
+  }
   ngOnDestroy(): void {
     this.tabChangeSubscription$.unsubscribe();
   }
-
+  getQueryParam() {
+    this.clientId = this.route.snapshot.queryParams['id']; 
+    this.clientCaseId = this.route.snapshot.queryParams['cid']; 
+    this.clientCaseEligibilityId = this.route.snapshot.queryParams['e_id']; 
+  }
 }
 
 
