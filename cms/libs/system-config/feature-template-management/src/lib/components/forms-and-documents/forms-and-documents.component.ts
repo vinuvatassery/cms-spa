25165ab1cef41c53c7137a962394956e9dc482f4
Renuka Bhupathi <renuka.bhupathi@speridian.com>
2023-04-18 19:35:40 +0530

    User story 1125

--- cms/libs/system-config/feature-template-management/src/lib/components/forms-and-documents/forms-and-documents.component.ts
+++ cms/libs/system-config/feature-template-management/src/lib/components/forms-and-documents/forms-and-documents.component.ts
@@ -1,6 +1,8 @@
 /** Angular **/
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { LoaderService, LoggingService, NotificationSnackbarService, SnackBarNotificationType } from '@cms/shared/util-core';
 import { TemplateManagementFacade } from '@cms/system-config/domain';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'system-config-forms-and-documents',
@@ -11,30 +13,81 @@ export class FormsAndDocumentsComponent {
   /** Public properties **/
   isOpenAttachment = false;
   foldersList: any = [];
-
+  foldersTree: any = [];
+  selectedfolder: string = "";
+  isShowLoader: boolean = true;
   public constructor(
-    private readonly templateManagementFacade: TemplateManagementFacade
+    private readonly templateManagementFacade: TemplateManagementFacade,
+    private readonly loaderService: LoaderService,
+    private readonly snackbarService: NotificationSnackbarService,
+    private readonly loggingService: LoggingService,
   ) {
   }
 
   ngOnInit() {
-    this.templateManagementFacade.getTemplates();
-    this.templateManagementFacade.templatesList$.subscribe((templates) => {
-      if (!!templates) {
-        this.foldersList = templates;
+    this.templateManagementFacade.getDirectoryContent('Forms','').subscribe((documentlist: any) => {
+      this.isShowLoader = false;
+      this.loaderService.hide();
+      if (!!documentlist && this.foldersTree.length == 0) {
+        this.foldersTree = documentlist;
       }
     })
   }
   /** Internal event methods **/
   onCloseAttachmentClicked() {
+
     this.isOpenAttachment = false;
   }
   onOpenAttachmentClicked() {
     this.isOpenAttachment = true;
+    if (this.isShowLoader)
+      this.loaderService.show();
+    else
+      this.loaderService.hide();
   }
 
-  //NOSONAR TODO - Add the download or view API call
-  onDownloadViewTemplateClick(viewType: string, documentTemplateId: string, name: string) {
+  fetchSubfolders = (node: any) =>
+    this.templateManagementFacade.getDirectoryContent('Forms',node.templateId).pipe(map((response: any[]) => {
+      return node.files = response;
+    }));
+
+  hasFiles = function (data: any) {
+
+    return data.isFolder;
+  }
+  /** Public methods **/
+  showSnackBar(type: SnackBarNotificationType, subtitle: any) {
+    if (type == SnackBarNotificationType.ERROR) {
+      const err = subtitle;
+      this.loggingService.logException(err)
+    }
+    this.snackbarService.manageSnackBar(type, subtitle);
   }
 
+  onDownloadViewFileClick(viewType: string, templateId: string,templateName:string) {
+
+    if (templateId === undefined || templateId === '') {
+      return;
+    }
+    this.loaderService.show()
+    this.templateManagementFacade.getFormsandDocumentsViewDownload(templateId).subscribe({
+      next: (data: any) => {
+
+        const fileUrl = window.URL.createObjectURL(data);
+        if (viewType === 'view') {
+          window.open(fileUrl, "_blank");
+        } else {
+          const downloadLink = document.createElement('a');
+          downloadLink.href = fileUrl;
+          downloadLink.download = templateName;
+          downloadLink.click();
+        }
+        this.loaderService.hide();
+      },
+      error: (error: any) => {
+        this.loaderService.hide();
+        this.showSnackBar(SnackBarNotificationType.ERROR, error)
+      }
+    })
+  }
 }
