e18cd4c1070e8a9c353706217d7426949a80cd0a
Srinivasa Rao Yalamanchi <srinivasarao.y@speridian.com>
2023-04-24 14:17:33 -0700

    Merged PR 1010: Status Tab - Group history & Fpl History  & RamSell info
    Status Tab - Group history & Fpl History & Ramsell info  grids added
    
    Related work items: #2183, #3668, #3669

--- cms/libs/case-management/domain/src/lib/infrastructure/status-period.data.service.ts
+++ cms/libs/case-management/domain/src/lib/infrastructure/status-period.data.service.ts
@@ -1,10 +1,12 @@
 /** Angular **/
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs/internal/observable/of';
 import { ConfigurationProvider } from "@cms/shared/util-core";
 @Injectable({ providedIn: 'root' })
 export class StatusPeriodDataService {
+
+  private baseUrl = "/case-management";
+
   /** Constructor**/
   constructor(private readonly http: HttpClient,private configurationProvider: ConfigurationProvider) {
      
@@ -14,5 +16,26 @@ export class StatusPeriodDataService {
   loadStatusPeriod(caseId:any,clientId:any,showHistorical:any,gridDataRefinerValue:any) {
     return this.http.get(
       `${this.configurationProvider.appSettings.caseApiUrl}/case-management/cases/${caseId}/status-periods?clientId=${clientId}&showHistorical=${showHistorical}&SkipCount=${gridDataRefinerValue.skipCount}&MaxResultCount=${gridDataRefinerValue.pagesize}`);
+  }
+
+  loadStatusGroupHistory(eligibilityId: string) {
+    return this.http.get<any[]>(
+      `${this.configurationProvider.appSettings.caseApiUrl}` + this.baseUrl +
+      `/eligibility-periods/${eligibilityId}/group-history`
+    );
+  }
+
+  loadStatusFplHistory(eligibilityId: string) {
+    return this.http.get<any[]>(
+      `${this.configurationProvider.appSettings.caseApiUrl}` + this.baseUrl +
+      `/eligibility-periods/${eligibilityId}/fpl-history`
+    );
+  }
+
+  loadRamSellInfo(clientId: string) {
+    return this.http.get<any[]>(
+      `${this.configurationProvider.appSettings.sysInterfaceApiUrl}` 
+      + `/system-interface/ramsell/clients/${clientId}`
+    );
   }
 }
