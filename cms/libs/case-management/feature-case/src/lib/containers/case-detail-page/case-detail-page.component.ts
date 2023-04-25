33f5e688d063c7a9d17c6ebfda3cb0af86780648
Dinesh <dinesh.panneerselvam@speridian.com>
2023-04-25 21:42:57 +0530

    #7645 Checking CER form type

--- cms/libs/case-management/feature-case/src/lib/containers/case-detail-page/case-detail-page.component.ts
+++ cms/libs/case-management/feature-case/src/lib/containers/case-detail-page/case-detail-page.component.ts
@@ -30,7 +30,7 @@ export class CaseDetailPageComponent implements OnInit, OnDestroy {
   clientCaseId: any;
   clientId: any;
   clientCaseStatusData: any = {};
-
+  prevClientCaseEligibilityId! : string;
   public formUiStyle: UIFormStyle = new UIFormStyle();
   workflowNavigationEvent = new EventEmitter<string>();
   openedSaveLater = false;
@@ -195,6 +195,8 @@ export class CaseDetailPageComponent implements OnInit, OnDestroy {
       .subscribe((session: any) => {
         this.clientCaseId = JSON.parse(session.sessionData).ClientCaseId
         this.caseFacade.loadCasesById(this.clientCaseId);
+        JSON.parse( session.sessionData)?.prevClientCaseEligibilityId
+        if (this.prevClientCaseEligibilityId) { this.isCerForm = true; }
       });
   }
   hideButton(type: any) {
