0aab292457a87731e4dbfa274580216f71e2534a
Mahesh K Chacko <mahesh.chacko@speridian.com>
2023-05-04 22:12:19 -0600

    fixes

--- cms/libs/case-management/feature-management/src/lib/components/case-manager-effective-dates/case-manager-effective-dates.component.ts
+++ cms/libs/case-management/feature-management/src/lib/components/case-manager-effective-dates/case-manager-effective-dates.component.ts
@@ -15,26 +15,34 @@ import { UIFormStyle } from '@cms/shared/ui-tpa';
 })
 export class CaseManagerEffectiveDatesComponent {
   @Input() assignedcaseManagerId: any;
-@Input() clientCaseManagerId : any
+  @Input() clientCaseManagerId: any;
 
-@Input() endDate!: any;
-@Input() startDate!: any ;
+  @Input() endDate!: any;
+  @Input() startDate!: any;
   showError = false;
   public formUiStyle: UIFormStyle = new UIFormStyle();
   @Output() changeDateConfimEvent = new EventEmitter<any>();
   onUnAssignConfirm(confirm: boolean) {
-    if (this.endDate && this.startDate) {
-      this.showError = false;
-      const existCaseManagerData = {
-        startDate: this.startDate,
-        endDate: this.endDate,
-        confirm: confirm,
-        assignedcaseManagerId: this.assignedcaseManagerId,
-        clientCaseManagerId: this.clientCaseManagerId
+    if (confirm) {
+      if (this.endDate && this.startDate) {
+        this.showError = false;
+        const existCaseManagerData = {
+          startDate: this.startDate,
+          endDate: this.endDate,
+          confirm: confirm,
+          assignedcaseManagerId: this.assignedcaseManagerId,
+          clientCaseManagerId: this.clientCaseManagerId,
+        };
+        this.changeDateConfimEvent.emit(existCaseManagerData);
+      } else {
+        this.showError = true;
+      }
+    } else {
+      const existCaseManagerData = {     
+        confirm: confirm
+       
       };
       this.changeDateConfimEvent.emit(existCaseManagerData);
-    } else {
-      this.showError = true;
     }
   }
 }
