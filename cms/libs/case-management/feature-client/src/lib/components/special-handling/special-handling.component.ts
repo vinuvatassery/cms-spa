f69f321d22e6e77b502c85dc6fdc49ee9766b3b5
sobia <sobia.kiran@speridian.com>
2023-05-22 21:03:53 +0500

    remove comments

--- cms/libs/case-management/feature-client/src/lib/components/special-handling/special-handling.component.ts
+++ cms/libs/case-management/feature-client/src/lib/components/special-handling/special-handling.component.ts
@@ -36,55 +36,6 @@ export class SpecialHandlingComponent implements OnInit {
     })
     this.UpdateApplicantInfo.emit();
   }
-  // private loadApplicantInfo() {
-   
-  //   this.loaderService.show();
-  //   this.clientFacade
-  //     .load(
-  //       this.clientId,
-  //       this.clientCaseId,
-  //       this.clientCaseEligibilityId)
-  //     .subscribe({
-  //       next: (response: any) => {
-  //         if (response) {
-  //           this.loaderService.hide();
-  //           /**Populating Client */
-  //           //this.applicantInfo.client = response.client;
-  //           if(response.clientNotes?.length > 0){
-  //             this.clientNotes = response.clientNotes;
-  //           }else {
-  //             this.clientNotes = [];
-  //           }
-            
-  //           this.answersKeys = Object.entries(response?.client).map(([key, value]) => ({key, value}));
-            
-  //           if(this.answersKeys && this.answersKeys.length > 0){
-  //             debugger;
-  //              this.questions.forEach(question =>{
-  //               question.answer = this.answersKeys.find(answer =>answer.key == question.key)?.value;
-  //               if(question.answer == "Yes" && question.otherKey != 'interpreterType' && question.otherFormatKey != 'materialInAlternateFormatOther' && question.descKey != 'materialInAlternateFormatCodeOtherDesccription' && question.key != 'limitingConditionDescription'){
-  //                 question.answer = question.answer+' ' +', Since age'+ ' ' +this.answersKeys.find(answer =>answer.key == question.otherKey)?.value; 
-  //               } else if(question.id == 1  ){
-  //                 question.answer =this.clientNotes.length > 0 ? this.clientNotes.map(function (e) { return e?.note;}).join(', ') : 'No Notes'
-  //               } else if(question.answer == "Yes" && question.otherKey == 'interpreterType'){
-  //                 question.answer ='Yes' + ' ,' + this.answersKeys.find(answer =>answer.key == question.otherKey)?.value;
-  //               }
-  //               else if(question.answer == "Yes"  &&  question.descKey == 'materialInAlternateFormatCodeOtherDesccription' && !this.answersKeys.find(answer =>answer.key == question.otherFormatKey)?.value){
-  //                 question.answer = 'Yes' + ' ,' + this.answersKeys.find(answer =>answer.key == question.descKey)?.value;
-  //               }
-  //               else if(question.answer == "Yes"  &&  question.otherFormatKey == 'materialInAlternateFormatOther' ){
-  //                 question.answer ='Yes' + ' ,' + this.answersKeys.find(answer =>answer.key == question.descKey)?.value +' ,'+ this.answersKeys.find(answer =>answer.key == question.otherFormatKey)?.value;
-  //               }
-  //             });
-  //             this.cdRef.detectChanges();
-  //           }
-  //         }
-  //       },
-  //       error: (error: any) => {
-  //         this.loaderService.hide();
-  //       },
-  //     });
-  // }
   /** Private methods **/
   private loadSpecialHandlings() { 
     this.clientFacade.loadSpecialHandlings();
@@ -94,7 +45,6 @@ export class SpecialHandlingComponent implements OnInit {
   onEditSpecialHandlingClosed() {
     this.isEditSpecialHandlingPopup = false;
     this.UpdateApplicantInfo.emit();
-    //this.loadApplicantInfo();
   }
 
   onEditSpecialHandlingClicked() {
