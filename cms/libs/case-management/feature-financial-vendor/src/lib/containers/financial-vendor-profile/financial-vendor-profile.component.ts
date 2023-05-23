9bd7c09d2058d07225114ada2b66a1cfd0f990ff
Dinesh <dinesh.panneerselvam@speridian.com>
2023-05-23 21:40:03 +0530

    #9251 started Vendor Profile UI

--- cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-profile/financial-vendor-profile.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-profile/financial-vendor-profile.component.ts
@@ -6,4 +6,44 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./financial-vendor-profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FinancialVendorProfileComponent {}
+export class FinancialVendorProfileComponent {
+  popupClassAction = 'TableActionPopup app-dropdown-action-list';
+  public sendActions = [
+    {
+      buttonType: 'btn-h-primary',
+      text: 'New Letter',
+      icon: 'markunread_mailbox',
+      isVisible: true,
+      click: (): void => { 
+      },
+    },
+    {
+      buttonType: 'btn-h-primary',
+      text: 'New Email',
+      icon: 'mail_outline',
+      isVisible: false,
+      click: (): void => {
+     
+      },
+    },
+    {
+      buttonType: 'btn-h-primary',
+      text: 'New SMS Text',
+      icon: 'comment',
+      isVisible: false,
+      click: (): void => {
+       
+      },
+    },
+    {
+      buttonType: 'btn-h-primary',
+      text: 'New ID Card',
+      icon: 'call_to_action',
+      isVisible: true,
+      click: (): void => {
+        
+      },
+    },
+  ];
+
+}
