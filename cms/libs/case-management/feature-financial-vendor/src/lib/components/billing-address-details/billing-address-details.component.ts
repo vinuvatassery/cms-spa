6979b425ef109897394283f08ccd11ee5f5bfc86
Dinesh <dinesh.panneerselvam@speridian.com>
2023-05-31 16:32:13 +0530

    #10274 code update

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/billing-address-details/billing-address-details.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/billing-address-details/billing-address-details.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { UIFormStyle } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'cms-billing-address-details',
   templateUrl: './billing-address-details.component.html',
   styleUrls: ['./billing-address-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BillingAddressDetailsComponent {}
+export class BillingAddressDetailsComponent {
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+}
