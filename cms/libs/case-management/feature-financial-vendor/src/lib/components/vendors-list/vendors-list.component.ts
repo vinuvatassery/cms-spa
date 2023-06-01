8cee8ff37fafac2c65f24a43ae943afd07e7c0f1
Mahesh K Chacko <mahesh.chacko@speridian.com>
2023-05-31 19:48:16 -0600

    changes

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/vendors-list/vendors-list.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/vendors-list/vendors-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { FinancialVendorTypeCode } from '@cms/case-management/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa'
@@ -11,7 +11,7 @@ import { Subject } from 'rxjs';
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class VendorsListComponent implements OnChanges{
+export class VendorsListComponent implements OnChanges , OnInit{
   public formUiStyle : UIFormStyle = new UIFormStyle();
 @Input() financeTabTypeCode! : string
 @Input() vendorTypeCode! : string
@@ -35,11 +35,13 @@ sortColumn = "Vendor Name";
   isFiltered = false;
   filter! : any
 
-selectedColumn! : string
+  selectedColumn!: any;
 
 gridDataResult! : GridDataResult
 gridVendorsDataSubject = new Subject<any>();
 gridVendorsData$ = this.gridVendorsDataSubject.asObservable();
+columnDroplistSubject =  new Subject<any[]>();
+columnDroplist$ = this.columnDroplistSubject.asObservable();
 filterData : CompositeFilterDescriptor={logic:'and',filters:[]};
 loader = false;
 
@@ -64,8 +66,126 @@ columns : any = {
   physicalAddress:"Physical Address",
 }
 
+dropDowncolumns : any = [
+  {   
+    "columnCode": "vendorName",    
+    "columnDesc": "Vendor Name"   ,
+    "vendorTypeCode": "ALL",
+  },
+  {   
+    "columnCode": "tin",    
+    "columnDesc": "Tin"   ,
+    "vendorTypeCode": "ALL",
+  }
+  ,
+  {   
+    "columnCode": "paymentMethod",    
+    "columnDesc": "Payment Method"   ,
+    "vendorTypeCode": "INSURANCE_PROVIDER",
+  }
+  ,
+  {   
+    "columnCode": "totalPayments",    
+    "columnDesc": "Total Payments"   ,
+    "vendorTypeCode": "INSURANCE_PROVIDER",
+  }
+  ,
+  {   
+    "columnCode": "unreconciledPayments",    
+    "columnDesc": "Unreconciled Payments"   ,
+    "vendorTypeCode": "INSURANCE_PROVIDER",
+  }
+  ,
+  {   
+    "columnCode": "insurancePlans",    
+    "columnDesc": "Insurance Plans"   ,
+    "vendorTypeCode": "INSURANCE_PROVIDER",
+  }
+  ,
+  {   
+    "columnCode": "clients",    
+    "columnDesc": "Clients"   ,
+    "vendorTypeCode": "INSURANCE_PROVIDER",
+  }
+  ,
+  {   
+    "columnCode": "invoiceDelivery",    
+    "columnDesc": "Invoice Delivery"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  }
 
-constructor(private route: Router) {
+  ,
+  {   
+    "columnCode": "totalDrugs",    
+    "columnDesc": "Total Drugs"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  },
+  {   
+    "columnCode": "openInvoices",    
+    "columnDesc": "Open Invoices"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  }
+  ,
+  {   
+    "columnCode": "phones",    
+    "columnDesc": "Phones"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  }
+  ,
+  {   
+    "columnCode": "emails",    
+    "columnDesc": "Emails"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  }
+  ,
+  {   
+    "columnCode": "mailCode",    
+    "columnDesc": "Mail Code"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  }
+  ,
+  {   
+    "columnCode": "address",    
+    "columnDesc": "Address"   ,
+    "vendorTypeCode": "MANUFACTURERS",
+  },
+  {   
+    "columnCode": "unreconciledPayments",    
+    "columnDesc": "Unreconciled Payments"   ,
+    "vendorTypeCode": "PHARMACY",
+  }
+  ,
+  {   
+    "columnCode": "emails",    
+    "columnDesc": "Emails"   ,
+    "vendorTypeCode": "PHARMACY",
+  }
+  ,
+  {   
+    "columnCode": "preferredFlag",    
+    "columnDesc": "Preferred Flag"   ,
+    "vendorTypeCode": "PHARMACY",
+  }
+  ,
+  {   
+    "columnCode": "nabp",    
+    "columnDesc": "Nabp"   ,
+    "vendorTypeCode": "PHARMACY",
+  }
+  ,
+  {   
+    "columnCode": "ncpdp",    
+    "columnDesc": "Ncpdp"   ,
+    "vendorTypeCode": "PHARMACY",
+  }
+  ,  
+  {   
+    "columnCode": "physicalAddress",    
+    "columnDesc": "Physical Address"   ,
+    "vendorTypeCode": "PHARMACY",
+  }
+]
+constructor(private route: Router, private readonly  cdr :ChangeDetectorRef) {
  
 }
 ngOnChanges(): void {  
@@ -75,10 +195,25 @@ ngOnChanges(): void {
     sort: this.sort
     };
   if(this.vendorTypeCode)
-  {
+  {  
    this.loadFinancialVendorsList()
   }
 }
+
+ngOnInit(): void {
+  this.bindDropdownClumns()
+  if(!this.selectedColumn)
+      {
+        this.selectedColumn = "";     
+      }
+}
+
+
+private bindDropdownClumns()
+{    
+  this.dropDowncolumns = this.dropDowncolumns.filter((x : any)=>x.vendorTypeCode === this.vendorTypeCode || x.vendorTypeCode === 'ALL') 
+}
+
 get financeVendorTypeCodes(): typeof FinancialVendorTypeCode {
   return FinancialVendorTypeCode;
 }
@@ -115,8 +250,36 @@ loadVendors(skipcountValue : number,maxResultCountValue : number ,sortValue : st
     
   }
 
-  onChange($event : any)
-  {}
+  
+
+  onChange(data: any)
+  {
+    this.defaultGridState()
+    
+    this.sortColumn = this.columns[this.selectedColumn];
+    this.filterData = {logic:'and',filters:[{
+      "filters": [
+          {
+              "field": this.selectedColumn ?? "vendorName",
+              "operator": "contains",
+              "value": data
+          }
+      ],
+      "logic": "and"
+  }]}
+  let stateData = this.state
+  stateData.filter = this.filterData
+  this.dataStateChange(stateData);
+  }
+
+  defaultGridState(){
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+      filter:{logic:'and',filters:[]}
+      };
+  }
 
   onColumnReorder($event : any)
   {
@@ -126,7 +289,7 @@ loadVendors(skipcountValue : number,maxResultCountValue : number ,sortValue : st
   columnChange($event : any)
   {}
 
-  dataStateChange(stateData: any): void {
+  dataStateChange(stateData: any): void {    
     this.sort = stateData.sort;
     this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
     this.sortType = stateData.sort[0]?.dir ?? 'asc'
@@ -163,8 +326,13 @@ pageselectionchange(data: any) {
   this.loadFinancialVendorsList()
 }
 
-  filterChange($event : any)
-  {}
+public filterChange(filter: CompositeFilterDescriptor): void {
+  debugger
+  this.filterData = filter;
+debugger
+ // this.gridDataResult.data = filterBy(this.gridDataResult.data, filter)
+  //this.gridCERDataSubject.next(this.gridDataResult);  
+ }
 
   gridDataHandle() {   
 
