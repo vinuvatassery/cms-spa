39ffd7c7a2eec5a69aa58fb9c6f64dd79f35284e
Muhammad Umer <muhammad.umer1@speridian.com>
2023-06-06 12:04:28 +0500

    eligibility period overlap bug fix

--- cms/libs/case-management/feature-client-eligibility/src/lib/components/eligibility-period-detail/eligibility-period-detail.component.ts
+++ cms/libs/case-management/feature-client-eligibility/src/lib/components/eligibility-period-detail/eligibility-period-detail.component.ts
@@ -112,9 +112,18 @@ export class EligibilityPeriodDetailComponent implements OnInit {
         this.acceptedApplication.otherReasonDesc = this.eligibilityPeriodForm.controls['otherReasonDesc'].value
         this.clientEligibilityFacade.saveNewStatusPeriod(this.acceptedApplication, this.clientCaseId, this.clientCaseEligibilityId).subscribe({
           next: (response) => {
-            this.clientEligibilityFacade.showHideSnackBar(SnackBarNotificationType.SUCCESS, "Status changed and new Eligibility Period started!.")
-            this.loaderService.hide();
-            this.isModalSavedClicked.emit(true);
+            if(response)
+            {
+              this.clientEligibilityFacade.showHideSnackBar(SnackBarNotificationType.SUCCESS, "Status changed and new Eligibility Period started!.")
+              this.loaderService.hide();
+              this.isModalSavedClicked.emit(true);
+            }
+            else{
+              this.clientEligibilityFacade.showHideSnackBar(
+                SnackBarNotificationType.WARNING,
+                'There cannot be two eligibility periods with overlapping date ranges.'
+              );
+            }
           },
           error: (err) => {
             this.loaderService.hide();
