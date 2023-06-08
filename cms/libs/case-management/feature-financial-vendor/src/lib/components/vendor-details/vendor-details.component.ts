136a424337849743ca67369709582212258dda31
Talha Afzal <talha.afzal@speridian.com>
2023-06-08 11:32:34 +0500

    Updated modal as per story requirements

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/vendor-details/vendor-details.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/vendor-details/vendor-details.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Input, ChangeDetectionStrategy, Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+import { VendorFacade } from '@cms/case-management/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'cms-vendor-details',
@@ -6,7 +8,85 @@ import { UIFormStyle } from '@cms/shared/ui-tpa';
   styleUrls: ['./vendor-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VendorDetailsComponent {
+export class VendorDetailsComponent implements OnInit {
+  @Input() isMedicalProvider: boolean = false;
+  @Input() medicalProviderForm: FormGroup;
+
   SpecialHandlingLength = 100;
-  public formUiStyle : UIFormStyle = new UIFormStyle();
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  
+  isViewContentEditable!: boolean;
+  isValidateForm: boolean = false;
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private vendorFacade: VendorFacade,
+    private readonly cdr: ChangeDetectorRef
+  ) {
+    this.medicalProviderForm = this.formBuilder.group({});
+  }
+
+  ngOnInit(): void {
+  }
+
+  get newAddContactForm() : FormArray{
+    return this.medicalProviderForm.get("newAddContactForm") as FormArray;
+  }
+
+  onToggleAddNewContactClick(){
+    let addContactForm = this.formBuilder.group({
+      contactName: new FormControl('', Validators.maxLength(40)),
+      description: new FormControl(),
+      phoneNumber: new FormControl(),
+      fax: new FormControl(),
+      email: new FormControl()
+    });
+    this.newAddContactForm.push(addContactForm);
+    this.cdr.detectChanges();
+  }
+
+  removeContact(i: number) {
+    this.newAddContactForm.removeAt(i);
+  }
+
+  getContactControl(index: number, fieldName: string) {
+    return (<FormArray>this.medicalProviderForm.get('newAddContactForm')).at(index).get(fieldName);
+  }
+
+  save() {
+    this.validateForm();
+    this.isValidateForm = true
+    this.vendorFacade.showLoader();
+  }
+
+  validateForm() {
+    this.medicalProviderForm.markAllAsTouched();
+    var mailCode = this.medicalProviderForm.controls['mailCode'].value;
+    if(mailCode) {
+      this.medicalProviderForm.controls['addressLine1']
+      .setValidators([
+        Validators.required,
+      ]);
+      this.medicalProviderForm.controls['addressLine1'].updateValueAndValidity();
+
+      this.medicalProviderForm.controls['city']
+      .setValidators([
+        Validators.required,
+      ]);
+      this.medicalProviderForm.controls['city'].updateValueAndValidity();
+
+      this.medicalProviderForm.controls['state']
+      .setValidators([
+        Validators.required,
+      ]);
+      this.medicalProviderForm.controls['state'].updateValueAndValidity();
+
+      this.medicalProviderForm.controls['zip']
+      .setValidators([
+        Validators.required,
+      ]);
+      this.medicalProviderForm.controls['zip'].updateValueAndValidity();      
+    }
+    
+  }
 }
--- cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-page/financial-vendor-page.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-page/financial-vendor-page.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CaseFacade, FinancialVendorFacade, FinancialVendorProviderTabCode, FinancialVendorTypeCode, SearchHeaderType } from '@cms/case-management/domain';
 import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'cms-financial-vendor-page',
@@ -10,6 +12,8 @@ import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
 })
 export class FinancialVendorPageComponent implements OnInit{
   isVendorDetailFormShow = false;
+  isMedicalProvider: boolean = false;
+  medicalProviderForm: FormGroup;
 
   data = [
     {
@@ -21,6 +25,8 @@ export class FinancialVendorPageComponent implements OnInit{
     {
       text: 'Medical Provider',
       click: (dataItem: any): void => {
+        this.isMedicalProvider = true;
+        this.buildMPForm();
         this.clickOpenVendorDetails(dataItem);
       },
     },
@@ -54,7 +60,10 @@ export class FinancialVendorPageComponent implements OnInit{
   sortType  = this.financialVendorFacade.sortType;
   sort  = this.financialVendorFacade.sort;
 
-  constructor(private caseFacade: CaseFacade , private financialVendorFacade : FinancialVendorFacade) {}
+  constructor(private caseFacade: CaseFacade , private financialVendorFacade : FinancialVendorFacade,
+    private readonly formBuilder: FormBuilder) {
+    this.medicalProviderForm = this.formBuilder.group({});
+  }
 
     /** Lifecycle hooks **/
     ngOnInit() {    
@@ -81,6 +90,32 @@ export class FinancialVendorPageComponent implements OnInit{
   {   
     
     this.financialVendorFacade.getVendors(data?.skipCount,data?.pagesize,data?.sortColumn,data?.sortType,data?.vendorTypeCode)
+  }
+
+  buildMPForm() {
+    this.medicalProviderForm = this.formBuilder.group({
+      providerName: ['', Validators.required],
+      tinNumber: [''],
+      paymentMethodRB: [''],
+      specialHandling: [''],
+      mailCode: [''],
+      nameOnCheck: [''],
+      nameOnEnvolop: [''],
+      addressLine1: [''],
+      addressLine2: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+      newAddContactForm: this.formBuilder.array([
+        this.formBuilder.group({
+          contactName: new FormControl('', Validators.maxLength(40)),
+          description: new FormControl(),
+          phoneNumber: new FormControl(),
+          fax: new FormControl(),
+          email: new FormControl()
+        })
+      ]),
+    });
   }
 
 
