47427c1f3bbf766ee479a72b095a91f5c645fb9b
sobia <sobia.kiran@speridian.com>
2023-06-14 17:06:29 +0500

    deactive

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/contacts-deactivate/contacts-deactivate.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/contacts-deactivate/contacts-deactivate.component.ts
@@ -1,9 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component,EventEmitter,Output } from '@angular/core';
+import { ContactsFacade } from '@cms/case-management/domain';
 @Component({
   selector: 'cms-contacts-deactivate',
   templateUrl: './contacts-deactivate.component.html',
   styleUrls: ['./contacts-deactivate.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactsDeactivateComponent {}
+export class ContactsDeactivateComponent {
+  @Output() closeDeactivateContactAddress = new EventEmitter();
+
+ /** Constructor **/
+ constructor(
+  private readonly contactsFacade: ContactsFacade
+) {
+}
+  onCloseDeactivateContactAddressClicked() {
+    this.closeDeactivateContactAddress.emit();
+  }
+
+  onDeactivateClick()
+  {
+   // this.contactsFacade.deactiveContactAddress(this.clientPharmacyDetails.clientPharmacyId);
+  }
+}
