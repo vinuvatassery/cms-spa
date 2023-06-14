7e2e484579721995de3985478387b29ec822d435
Toqeer Yousaf <toqeer.yousaf@speridian.com>
2023-06-14 17:06:16 +0500

    multipal Contacts Add

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/contact-address-details/contact-address-details.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/contact-address-details/contact-address-details.component.ts
@@ -1,7 +1,7 @@
 import { Component,OnInit,  Output,Input,
   EventEmitter } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder,FormControl,FormArray, FormGroup, Validators } from '@angular/forms';
 import { VendorContacts,ContactFacade,ContactData,EmailTypeCode,PhoneTypeCode,PaymentsFacade,ContactsFacade } from '@cms/case-management/domain';
 import { LoaderService,SnackBarNotificationType} from '@cms/shared/util-core';
 @Component({
@@ -18,6 +18,7 @@ export class ContactAddressDetailsComponent implements OnInit {
   public contactDetailForm!: FormGroup;
   contactAddress = new VendorContacts();
   contact = new ContactData();
+  contactForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,7 +26,37 @@ export class ContactAddressDetailsComponent implements OnInit {
     private contactsFacade:ContactsFacade,
     private readonly paymentsFacade: PaymentsFacade,
     private readonly loaderService: LoaderService){
-
+      this.contactForm = this.formBuilder.group({
+        mailcode: [this.contact.mailCode, Validators.required],
+        vendorId: [this.contact.vendorId],
+        vendorAddressId: [this.contact.vendorAddressId],
+        vendorContacts: new FormArray([]),
+      });
+      const vendorContacts = this.contactForm.get('vendorContacts') as FormArray;
+      let addContactForm = this.formBuilder.group({
+        contactName: new FormControl(this.contactAddress.contactName, [
+          Validators.required,
+          Validators.maxLength(40),
+        ]),
+        contactDesc: new FormControl(this.contactAddress.contactDesc),
+        phoneNbr: new FormControl(this.contactAddress.phoneNbr),
+        faxNbr: new FormControl(this.contactAddress.faxNbr),
+        emailAddress: new FormControl(this.contactAddress.emailAddress),
+        emailAddressTypeCode: new FormControl(EmailTypeCode.Email),
+        phoneTypeCode: new FormControl(PhoneTypeCode.Phone),
+        vendorContactId: new FormControl(this.contactAddress.vendorContactId),
+        vendorAddressId: new FormControl(this.contactAddress.vendorAddressId),
+        vendorContactPhoneId: new FormControl(
+          this.contactAddress.vendorContactPhoneId
+        ),
+        vendorContactEmailId: new FormControl(
+          this.contactAddress.vendorContactEmailId
+        ),
+        jobTitle: new FormControl(this.contactAddress.jobTitle),
+        vendorName: new FormControl(this.contactAddress.vendorName),
+        effectiveDate: new FormControl(this.contactAddress.effectiveDate),
+      });
+      vendorContacts.push(addContactForm);
   }
 ngOnInit(): void {
   this.contactFacade.loadMailCodes(this.vendorId);
@@ -72,36 +103,69 @@ ngOnInit(): void {
      this.contact.vendorContacts.push(this.contactAddress);
   }
 
-  public save (){
-    this.setValidators()
-      if (this.contactDetailForm.valid) {
-        this.loaderService.show();
-        this.populateContactAddress();
-      this.contactsFacade
-        .saveContactAddress(this.contact).subscribe({
-          next: (response: any) =>{
-            if(response)
-            { 
-             this.contactFacade.showHideSnackBar(
-               SnackBarNotificationType.SUCCESS,
-               'Contact Address added successfully'
-             );
-             this.contactsFacade.loadcontacts('123');
-             this.contactFacade.hideLoader();
-             this.isContactDetailPopupClose.emit(true);
-            }
-           },
-           error: (error: any) => {
-             this.loaderService.hide();
-             this.contactFacade.showHideSnackBar(
-               SnackBarNotificationType.ERROR,
-               error)
-           }
-        }
-       
-        );
-      }
-    
-   
+
+  public save() {
+    this.contactForm.controls['vendorId'].setValue(this.vendorId);
+    const dat = this.contactForm.value;
+    if (this.contactForm.valid) {
+      this.loaderService.show();
+      this.contactsFacade.saveContactAddress(this.contactForm.value).subscribe({
+        next: (response: any) => {
+          if (response) {
+            this.contactFacade.showHideSnackBar(
+              SnackBarNotificationType.SUCCESS,
+              'Contact Address added successfully'
+            );
+            this.contactsFacade.loadcontacts('CO1');
+            this.contactFacade.hideLoader();
+            this.isContactDetailPopupClose.emit(true);
+          }
+        },
+        error: (error: any) => {
+          this.loaderService.hide();
+          this.contactFacade.showHideSnackBar(
+            SnackBarNotificationType.ERROR,
+            error
+          );
+        },
+      });
+    }
+  }
+  get AddContactForm(): FormArray {
+    return this.contactForm.get('vendorContacts') as FormArray;
+  }
+  IsContactNameValid(index: any) {
+    var contactNameIsvalid = this.AddContactForm.at(index) as FormGroup;
+    return contactNameIsvalid.controls['contactName'].status == 'INVALID';
+  }
+  onToggleAddNewContactClick() {
+    let addContactForm = this.formBuilder.group({
+      contactName: new FormControl(
+        this.contactAddress.contactName,
+        Validators.maxLength(40)
+      ),
+      contactDesc: new FormControl(this.contactAddress.contactDesc),
+      phoneNbr: new FormControl(this.contactAddress.phoneNbr),
+      faxNbr: new FormControl(this.contactAddress.faxNbr),
+      emailAddress: new FormControl(this.contactAddress.emailAddress),
+      emailAddressTypeCode: new FormControl(EmailTypeCode.Email),
+      phoneTypeCode: new FormControl(PhoneTypeCode.Phone),
+      vendorContactId: new FormControl(this.contactAddress.vendorContactId),
+      vendorAddressId: new FormControl(this.contactAddress.vendorAddressId),
+      vendorContactPhoneId: new FormControl(
+        this.contactAddress.vendorContactPhoneId
+      ),
+      vendorContactEmailId: new FormControl(
+        this.contactAddress.vendorContactEmailId
+      ),
+      jobTitle: new FormControl(this.contactAddress.jobTitle),
+      vendorName: new FormControl(this.contactAddress.vendorName),
+      effectiveDate: new FormControl(this.contactAddress.effectiveDate),
+    });
+    this.AddContactForm.push(addContactForm);
+  }
+
+  removeContact(i: number) {
+    this.AddContactForm.removeAt(i);
   }
 }
