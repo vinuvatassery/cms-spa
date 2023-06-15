d4f660f3dab477368d0d1a22fff3e66bc2ebf0e9
Jojish Pathrose <jojish.pathrose@speridian.com>
2023-06-15 13:10:52 -0600

    #6501-(feat)-vendor-profile-insurance-provider grid.

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/financial-insurance-provider-list/financial-insurance-provider-list.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/financial-insurance-provider-list/financial-insurance-provider-list.component.ts
@@ -1,20 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { VendorInsurancePlanFacade } from '@cms/case-management/domain';
 import { State } from '@progress/kendo-data-query';
 @Component({
-  selector: 'cms-financial-insurance-plans',
-  templateUrl: './financial-insurance-plans.component.html',
+  selector: 'cms-financial-insurance-provider-list',
+  templateUrl: './financial-insurance-provider-list.component.html',
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FinancialInsurancePlansComponent {
+export class FinancialInsuranceProviderListComponent implements OnInit  {
+  /* Input Properties */
+  @Input() vendorId!: string;
+
   public formUiStyle: UIFormStyle = new UIFormStyle();
   isInsurancePlanDetailShow = false;
   isInsurancePlanDeactivateShow = false;
   isInsurancePlanDeleteShow = false;
   popupClassAction = 'TableActionPopup app-dropdown-action-list';
-  isVendorInsurancePlanGridLoaderShow = false;
   public sortValue = this.vendorInsurancePlanFacade.sortValue;
   public sortType = this.vendorInsurancePlanFacade.sortType;
   public pageSizes = this.vendorInsurancePlanFacade.gridPageSizes;
@@ -22,25 +24,26 @@ export class FinancialInsurancePlansComponent {
   public sort = this.vendorInsurancePlanFacade.sort;
   public state!: State;
   vendorInsurancePlanGridView$ = this.vendorInsurancePlanFacade.vendorInsurancePlanData$;
+  gridLoader$= this.vendorInsurancePlanFacade.vendorInsuranceGridLoader$;
 
   public emailBillingAddressActions = [
     {
       buttonType: 'btn-h-primary',
       text: 'Edit',
       icon: 'edit',
-      click: (data: any): void => {        
+      click: (data: any): void => {
         console.log(data);
         this.clickOpenAddEditInsurancePlanDetails();
-         
+
       },
-    }, 
+    },
     {
       buttonType: 'btn-h-primary',
       text: 'Deactivate',
       icon: 'block',
       click: (data: any): void => {
         console.log(data);
-       this.clickOpenDeactivateInsurancePlanDetails();
+        this.clickOpenDeactivateInsurancePlanDetails();
       },
     },
     {
@@ -48,23 +51,24 @@ export class FinancialInsurancePlansComponent {
       text: 'Delete',
       icon: 'delete',
       click: (data: any): void => {
-        console.log(data); 
+        console.log(data);
         this.clickOpenDeleteInsurancePlanDetails();
       },
     },
   ];
 
+  /** Constructor **/
+  constructor(private readonly vendorInsurancePlanFacade: VendorInsurancePlanFacade) { }
 
-  
-  
-   /** Constructor **/
-   constructor(private readonly vendorInsurancePlanFacade: VendorInsurancePlanFacade) {}
-
-
-   
   ngOnInit(): void {
-    this.loadVendorInsurancePlanListGrid();
+    this.state = {
+      skip: this.gridSkipCount,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+    };
+    this.loadVendorInsuranceProviderListGrid();
   }
+
   ngOnChanges(): void {
     this.state = {
       skip: this.gridSkipCount,
@@ -77,25 +81,19 @@ export class FinancialInsurancePlansComponent {
   pageSelectionchange(data: any) {
     this.state.take = data.value;
     this.state.skip = 0;
+    this.loadVendorInsuranceProviderListGrid();
   }
 
   public dataStateChange(stateData: any): void {
     this.sort = stateData.sort;
-    this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
-    this.sortType = stateData.sort[0]?.dir ?? 'asc';
     this.state = stateData;
-  }
-  loadVendorInsurancePlanListGrid() {
-    this.state = {
-      skip: this.gridSkipCount,
-      take: this.pageSizes[0]?.value,
-      sort: this.sort,
-    };
-    this.vendorInsurancePlanFacade.loadVendorInsurancePlanListGrid();
+    this.loadVendorInsuranceProviderListGrid();
   }
 
+  loadVendorInsuranceProviderListGrid() {
+    this.vendorInsurancePlanFacade.loadVendorInsuranceProviderListGrid(this.vendorId, this.state);
+  }
 
-  
   clickOpenAddEditInsurancePlanDetails() {
     this.isInsurancePlanDetailShow = true;
   }
