e1f0c8c50957a1c8049132465ca54e5978ffa926
Mahesh Chacko <mahesh.chacko@speridian.com>
2023-06-15 18:19:07 -0700

    Merged PR 1394: (6518) : vendor profile header
    User Story 6497: Insurance Vendors Profile (IVP) - Header
    User Story 6505: Medical Provider Profile (MPP) - Header
    User Story 6518: Pharmacy Profile (PP) - Header
    User Story 6491: Manufacturer Profile - Header
    User Story 6512: Dental Provider Profile (DPP) - Header
    
    Related work items: #6491, #6497, #6505, #6512, #6518

--- cms/libs/case-management/domain/src/lib/application/financial-management/vendor.facade.ts
+++ cms/libs/case-management/domain/src/lib/application/financial-management/vendor.facade.ts
@@ -7,7 +7,7 @@ import { SortDescriptor } from '@progress/kendo-data-query';
 
 /** Internal libraries **/
 import { ConfigurationProvider, LoggingService, NotificationSnackbarService, SnackBarNotificationType, LoaderService } from '@cms/shared/util-core';
-import { FinancialVendorDataService } from '../infrastructure/financial-vendor.data.service';
+import { FinancialVendorDataService } from '../../infrastructure/financial-management/vendor.data.service';
 
 
 @Injectable({ providedIn: 'root' })
@@ -16,12 +16,13 @@ export class FinancialVendorFacade {
   /** Private properties **/
   private vendorsSubject = new Subject<any>();
   private selectedVendorSubject = new Subject<any>();
-
+  private vendorProfileSubject = new Subject<any>();
   /** Public properties **/
   vendorsList$ = this.vendorsSubject.asObservable();
   selectedVendor$ = this.selectedVendorSubject.asObservable();
-
-  public gridPageSizes = this.configurationProvider.appSettings.gridPageSizeValues;
+  vendorProfile$ = this.vendorProfileSubject.asObservable();
+  
+  public gridPageSizes =this.configurationProvider.appSettings.gridPageSizeValues;
   public sortValue = 'vendorName'
   public sortType = 'asc'
   public sort: SortDescriptor[] = [{
@@ -30,33 +31,37 @@ export class FinancialVendorFacade {
 
   /** Constructor**/
   constructor(private readonly financialVendorDataService: FinancialVendorDataService,
-    private readonly loaderService: LoaderService,
-    private configurationProvider: ConfigurationProvider,
-    private loggingService: LoggingService,
-    private readonly notificationSnackbarService: NotificationSnackbarService,
-  ) { }
+    private readonly loaderService: LoaderService ,
+    private configurationProvider : ConfigurationProvider ,
+    private loggingService : LoggingService,
+    private readonly notificationSnackbarService : NotificationSnackbarService,
+     ) {}
 
-  /** Public methods **/
-  showLoader() {
+       /** Public methods **/
+  showLoader()
+  {
     this.loaderService.show();
   }
 
-  hideLoader() {
+  hideLoader()
+  {
     this.loaderService.hide();
   }
-  showHideSnackBar(type: SnackBarNotificationType, subtitle: any) {
-    if (type == SnackBarNotificationType.ERROR) {
-      const err = subtitle;
+  showHideSnackBar(type : SnackBarNotificationType , subtitle : any)
+  {
+    if(type == SnackBarNotificationType.ERROR)
+    {
+       const err= subtitle;
       this.loggingService.logException(err)
     }
-    this.notificationSnackbarService.manageSnackBar(type, subtitle)
+    this.notificationSnackbarService.manageSnackBar(type,subtitle)
     this.hideLoader();
   }
 
-
-  getVendors(skipcount: number, maxResultCount: number, sort: string, sortType: string, vendorTypeCode: string): void {
-
-    this.financialVendorDataService.getVendors(skipcount, maxResultCount, sort, sortType, vendorTypeCode).subscribe({
+  
+  getVendors(skipcount: number,maxResultCount: number,sort: string,sortType: string,vendorTypeCode: string): void {
+   
+    this.financialVendorDataService.getVendors(skipcount,maxResultCount,sort,sortType,vendorTypeCode).subscribe({
       next: (vendorsResponse: any) => {
         if (vendorsResponse) {
           const gridView = {
@@ -65,10 +70,25 @@ export class FinancialVendorFacade {
           };
           this.vendorsSubject.next(gridView);
         }
-
+       
       },
-      error: (err) => {
-        this.showHideSnackBar(SnackBarNotificationType.ERROR, err)
+      error: (err) => {     
+        this.showHideSnackBar(SnackBarNotificationType.ERROR , err)
+      },
+    });
+  }
+  getVendorProfile(vendorId: string,tabCode: string): void {
+    this.showLoader();
+    this.financialVendorDataService.getVendorProfile(vendorId,tabCode).subscribe({
+      next: (vendorResponse: any) => {
+        if (vendorResponse) {        
+          this.vendorProfileSubject.next(vendorResponse);
+          this.hideLoader();
+        }       
+      },
+      error: (err) => {     
+        this.hideLoader();
+        this.showHideSnackBar(SnackBarNotificationType.ERROR , err)
       },
     });
   }
