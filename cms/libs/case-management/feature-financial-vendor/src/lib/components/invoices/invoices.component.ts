7499ba1f12694bf2a1704cfe7118a8c1563af693
Anoop V J <anoop.jose@speridian.com>
2023-06-21 14:58:54 +0530

    6514: changes for the Services for the invoice tab
    changes for the Services for the invoice tab

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/invoices/invoices.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/invoices/invoices.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'; 
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'; 
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { InvoiceFacade } from '@cms/case-management/domain';
 import { State } from '@progress/kendo-data-query';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'cms-invoices',
   templateUrl: './invoices.component.html', 
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class InvoicesComponent {
   public formUiStyle: UIFormStyle = new UIFormStyle();
   popupClassAction = 'TableActionPopup app-dropdown-action-list';
@@ -18,31 +22,27 @@ export class InvoicesComponent {
   public sort = this.invoiceFacade.sort;
   public state!: State;
   invoiceGridView$ = this.invoiceFacade.invoiceData$;
- 
+  providerId:any;
+  isInvoiceLoading$=  this.invoiceFacade.isInvoiceLoading$
+  isInvoiceLoadingSubscription!:Subscription;
+  @Input() tabCode: any;
+  @Input() vendorId: any;
 
-  
-  ClientGridLists = [
-    {
-      ServiceStart: 'MMDDYYYY_XXX `',
-      ServiceEnd:'MMDDYYYY_XXX', 
-      CPTCode: 'XXXXXXX',
-      MedicalService: 'LIPID Testing',
-      ServiceCost: '500.000',
-      AmountDue: '20.00',
-      PaymentType: 'Regular Pay', 
-      ClientAnnualTotal: '5,600.00', 
-      ClientBalance: '2,000.00', 
-    },
-  ];
-   
    /** Constructor **/
-   constructor(private readonly invoiceFacade: InvoiceFacade) {}
+   constructor(private readonly invoiceFacade: InvoiceFacade,private readonly router: Router,) {}
 
-
-   
   ngOnInit(): void {
+    this.state = {
+      skip: this.gridSkipCount,
+      take: this.pageSizes[0]?.value
+    };
+    this.vendorId='F49E42C5-1F8A-4297-A06A-C84F8EF187BF'
     this.loadInvoiceListGrid();
+    this.isInvoiceLoadingSubscription = this.isInvoiceLoading$.subscribe(data=>{
+      this.isInvoiceGridLoaderShow = data;
+    })
   }
+
   ngOnChanges(): void {
     this.state = {
       skip: this.gridSkipCount,
@@ -50,25 +50,30 @@ export class InvoicesComponent {
       sort: this.sort,
     };
   }
-
-  // updating the pagination info based on dropdown selection
-  pageSelectionchange(data: any) {
-    this.state.take = data.value;
-    this.state.skip = 0;
-  }
-
   public dataStateChange(stateData: any): void {
     this.sort = stateData.sort;
     this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
     this.sortType = stateData.sort[0]?.dir ?? 'asc';
     this.state = stateData;
+    this.loadInvoiceListGrid();
+  }
+  pageSelectionChange(data: any) {
+    this.state.take = data.value;
+    this.state.skip = 0;
+    this.loadInvoiceListGrid();
   }
   loadInvoiceListGrid() {
-    this.state = {
-      skip: this.gridSkipCount,
-      take: this.pageSizes[0]?.value,
-      sort: this.sort,
-    };
-    this.invoiceFacade.loadInvoiceListGrid();
+    this.invoiceFacade.loadInvoiceListGrid(this.vendorId,this.state,this.tabCode,this.sortValue,this.sortType);
+  }
+
+  onClientClicked(clientId: any) {
+      this.router.navigate([`/case-management/cases/case360/${clientId}`]);
+      
+  }
+  onBatchClicked() {
+    this.router.navigate([`/financial-management/medical-claims`]);    
+  } 
+  onExpand(event:any) {
+    this.invoiceFacade.loadPaymentRequestServices(event.dataItem,this.vendorId,this.tabCode)   
   }
 }
