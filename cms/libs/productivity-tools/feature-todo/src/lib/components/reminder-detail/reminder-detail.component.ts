c27e3d40bd6361aefabdee7a78e47625c2d628d1
sobia <sobia.kiran@speridian.com>
2023-06-23 11:54:42 +0500

    10397_fixed

--- cms/libs/productivity-tools/feature-todo/src/lib/components/reminder-detail/reminder-detail.component.ts
+++ cms/libs/productivity-tools/feature-todo/src/lib/components/reminder-detail/reminder-detail.component.ts
@@ -1,5 +1,6 @@
 /** Angular **/
 import { Component, OnInit, Output,ChangeDetectionStrategy, EventEmitter} from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { CaseFacade } from '@cms/case-management/domain';
 import { TodoFacade } from '@cms/productivity-tools/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa'
@@ -14,19 +15,22 @@ currentDate = new Date();
 public formUiStyle : UIFormStyle = new UIFormStyle();
   
   /** Public properties **/
+  clientReminderForm !:FormGroup
   caseSearched$ = this.caseFacade.caseSearched$;
   search$ = this.todoFacade.search$;
   tareaRemindermaxLength = 200;
   tareaReminderCharachtersCount!: number;
   tareaReminderCounter!: string;
   tareaReminderDescription = '';
-  isCustomReminder= false;
-  @Output() isModalNewReminderCloseClicked  = new EventEmitter();
+  isShow= false;
+  dateValidator: boolean = false;
+  @Output() closeReminderEvent = new EventEmitter();
 
   constructor(private readonly todoFacade: TodoFacade,private readonly caseFacade: CaseFacade,) {}
 
   /** Lifecycle hooks **/
   ngOnInit(): void {
+    this.buildForm();
     this.tareaVariablesIntialization();
   }
 
@@ -43,7 +47,46 @@ public formUiStyle : UIFormStyle = new UIFormStyle();
     this.tareaReminderCharachtersCount = event.length;
     this.tareaReminderCounter = `${this.tareaReminderCharachtersCount}/${this.tareaRemindermaxLength}`;
   }
-  closeReminderDetails(){
-    this.isModalNewReminderCloseClicked.emit(true);
+  private buildForm() {
+    this.clientReminderForm = new FormGroup({
+      dueDate: new FormControl(''),
+      time: new FormControl(''),
+      description:new FormControl(''),
+      addToOutlookCalender:new FormControl(''),
+    });
   }
+  onCloseReminderClicked() 
+  {
+    this.closeReminderEvent.emit();
+  }
+  setValidators()
+  {
+    this.clientReminderForm.markAllAsTouched();
+    this.clientReminderForm.controls['dueDate'].setValidators([Validators.required,]);
+    this.clientReminderForm.controls['dueDate'].updateValueAndValidity();
+    this.clientReminderForm.controls['description'].setValidators([Validators.required,]);
+    this.clientReminderForm.controls['description'].updateValueAndValidity();
+
+  }
+  dateValidate(event: Event) {
+    this.dateValidator = false;
+    const signedDate = this.clientReminderForm.controls['dueDate'].value;
+    const todayDate = new Date();
+    signedDate.setHours(0, 0, 0, 0);
+    todayDate.setHours(0, 0, 0, 0);
+    if (signedDate.getTime() === todayDate.getTime()) {
+      this.dateValidator = false;
+    }
+   else if (signedDate < todayDate) {
+      this.dateValidator = true;
+    }
+  }
+  
+  public save (){
+    this.setValidators();
+    if (this.clientReminderForm.valid && this.dateValidator==false) {
+       // this.clientReminderFacade.showHideSnackBar( SnackBarNotificationType.SUCCESS, 'Client Reminder added successfully');
+    }
+  
+           }
 }
