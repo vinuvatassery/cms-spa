c8703c67bab03c5ebd6e9f5353df0014c59bbbd6
Mahesh K Chacko <mahesh.chacko@speridian.com>
2023-06-24 17:11:46 -0600

    bugfix/bug-10721

--- cms/libs/case-management/feature-contact/src/lib/components/phone-detail/phone-detail.component.ts
+++ cms/libs/case-management/feature-contact/src/lib/components/phone-detail/phone-detail.component.ts
@@ -72,7 +72,7 @@ export class PhoneDetailComponent implements OnInit {
   {    
       this.clientPhoneForm = this.formBuilder.group({   
         clientPhoneId: ['']   ,
-        phoneNbr: ['',Validators.required]   ,
+        phoneNbr: ['',[Validators.required, Validators.min(10)]] ,
         detailMsgConsentFlag: ['']   ,
         deviceTypeCode: ['',Validators.required]   ,
         smsTextConsentFlag: ['']   ,       
@@ -86,6 +86,10 @@ export class PhoneDetailComponent implements OnInit {
       }
   }
 
+  get f(){  
+    return this.clientPhoneForm.controls;  
+  }  
+  
   onSelectedPhoneFormLoad()
   {     
     this.selectedclientPhoneId = this.selectedPhoneData?.clientPhoneId 
