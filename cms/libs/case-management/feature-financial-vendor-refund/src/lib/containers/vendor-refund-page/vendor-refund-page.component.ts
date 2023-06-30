24fb9e8fbbb85780d2aba4f40a58dfa62d2a04ec
Dinesh <dinesh.panneerselvam@speridian.com>
2023-06-30 11:08:28 +0530

    clean up

--- cms/libs/case-management/feature-financial-vendor-refund/src/lib/containers/vendor-refund-page/vendor-refund-page.component.ts
+++ cms/libs/case-management/feature-financial-vendor-refund/src/lib/containers/vendor-refund-page/vendor-refund-page.component.ts
@@ -1,15 +1,13 @@
 /** Angular **/
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component  } from '@angular/core';
 import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'cms-vendor-refund-page',
   templateUrl: './vendor-refund-page.component.html', 
 })
-export class VendorRefundPageComponent implements OnInit, OnChanges {
+export class VendorRefundPageComponent   {
   public formUiStyle: UIFormStyle = new UIFormStyle();
   public uiTabStripScroll: UITabStripScroll = new UITabStripScroll();
 
-  constructor() {}
-  ngOnInit(): void {}
-  ngOnChanges(): void {}
+ 
 }
