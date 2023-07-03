3587517a7e09d3cca1fbac9a45ed4a852dc29576
Dinesh <dinesh.panneerselvam@speridian.com>
2023-07-03 13:59:01 +0530

    claims list updated

--- cms/libs/case-management/feature-financial-vendor-refund/src/lib/components/refund-new-form-details/refund-new-form-details.component.ts
+++ cms/libs/case-management/feature-financial-vendor-refund/src/lib/components/refund-new-form-details/refund-new-form-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy , Output, EventEmitter} from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
+import { State } from '@progress/kendo-data-query';
+import { FinancialVendorRefundFacade } from '@cms/case-management/domain'; 
 @Component({
   selector: 'cms-refund-new-form-details',
   templateUrl: './refund-new-form-details.component.html',
@@ -8,6 +10,14 @@ import { UIFormStyle } from '@cms/shared/ui-tpa';
 })
 export class RefundNewFormDetailsComponent{
   public formUiStyle: UIFormStyle = new UIFormStyle();
+  claimsListData$ =
+    this.financialVendorRefundFacade.claimsListData$;
+    sortValue = this.financialVendorRefundFacade.sortValueRefundProcess;
+    sortType = this.financialVendorRefundFacade.sortType;
+    pageSizes = this.financialVendorRefundFacade.gridPageSizes;
+    gridSkipCount = this.financialVendorRefundFacade.skipCount;
+    sort = this.financialVendorRefundFacade.sortProcessList;
+    state!: State;
   clientsearchresult =[
 
     {
@@ -58,8 +68,15 @@ export class RefundNewFormDetailsComponent{
 ];
   @Output() modalCloseAddEditRefundFormModal = new EventEmitter();
 
+  constructor(  private readonly financialVendorRefundFacade: FinancialVendorRefundFacade) {}
 
   closeAddEditRefundFormModalClicked(){
     this.modalCloseAddEditRefundFormModal.emit(true);  
+  }
+
+
+  loadClaimsListGrid() {
+ 
+    this.financialVendorRefundFacade.loadClaimsListGrid();
   }
 }
