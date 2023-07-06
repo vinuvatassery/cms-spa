420a79a007caf7a195b73fabbd08814985da1cf8
Dinesh <dinesh.panneerselvam@speridian.com>
2023-07-06 16:49:09 +0530

    #11558 bug fixes

--- cms/libs/system-config/feature-template-management/src/lib/components/forms-and-documents/forms-and-documents.component.ts
+++ cms/libs/system-config/feature-template-management/src/lib/components/forms-and-documents/forms-and-documents.component.ts
@@ -1,9 +1,9 @@
 /** Angular **/
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy , TemplateRef,} from '@angular/core';
 import { LoaderService, LoggingService, NotificationSnackbarService, SnackBarNotificationType } from '@cms/shared/util-core';
 import { TemplateManagementFacade } from '@cms/system-config/domain';
 import { map } from "rxjs/operators";
-import { UIFormStyle } from '@cms/shared/ui-tpa'
+import { DialogService } from '@progress/kendo-angular-dialog';
 
 @Component({
   selector: 'system-config-forms-and-documents',
@@ -17,11 +17,13 @@ export class FormsAndDocumentsComponent {
   foldersTree: any = [];
   selectedfolder: string = "";
   isShowLoader: boolean = true;
+  public formsDocumentDialog : any;
   public constructor(
     private readonly templateManagementFacade: TemplateManagementFacade,
     private readonly loaderService: LoaderService,
     private readonly snackbarService: NotificationSnackbarService,
-    private readonly loggingService: LoggingService,
+    private readonly loggingService: LoggingService, 
+    private dialogService: DialogService
   ) {
   }
 
@@ -36,10 +38,16 @@ export class FormsAndDocumentsComponent {
   }
   /** Internal event methods **/
   onCloseAttachmentClicked() {
-
+    this.formsDocumentDialog.close()
     this.isOpenAttachment = false;
   }
-  onOpenAttachmentClicked() {
+
+  
+  onOpenAttachmentClicked(template: TemplateRef<unknown>): void {
+    this.formsDocumentDialog = this.dialogService.open({
+      content: template,
+      cssClass: 'app-c-modal app-c-modal-xls app-c-modal-np app-c-modal-top',
+    }); 
     this.isOpenAttachment = true;
     if (this.isShowLoader)
       this.loaderService.show();
