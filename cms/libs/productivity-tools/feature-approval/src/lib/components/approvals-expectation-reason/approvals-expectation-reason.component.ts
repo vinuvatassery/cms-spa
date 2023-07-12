d3a0c0742acb27a2d6cc8aac74ac9eab8a428f1c
Dinesh <dinesh.panneerselvam@speridian.com>
2023-07-12 14:41:29 +0530

    #11654 Search Clients, Make Exception added

--- cms/libs/productivity-tools/feature-approval/src/lib/components/approvals-expectation-reason/approvals-expectation-reason.component.ts
+++ cms/libs/productivity-tools/feature-approval/src/lib/components/approvals-expectation-reason/approvals-expectation-reason.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { UIFormStyle } from '@cms/shared/ui-tpa'; 
 @Component({
   selector: 'productivity-tools-approvals-expectation-reason',
   templateUrl: './approvals-expectation-reason.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ApprovalsExpectationReasonComponent {}
+export class ApprovalsExpectationReasonComponent {
+  
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+}
--- cms/libs/productivity-tools/feature-approval/src/lib/components/approvals-search-clients/approvals-search-clients.component.ts
+++ cms/libs/productivity-tools/feature-approval/src/lib/components/approvals-search-clients/approvals-search-clients.component.ts
@@ -1,8 +1,45 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { UIFormStyle } from '@cms/shared/ui-tpa'; 
 @Component({
   selector: 'productivity-tools-approvals-search-clients',
-  templateUrl: './approvals-search-clients.component.html',
+  templateUrl: './approvals-search-clients.component.html', 
+
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ApprovalsSearchClientsComponent {}
+export class ApprovalsSearchClientsComponent {
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  isShownSearchLoader = false;
+  clientSearchResult =[
+
+    {
+      clientId: '12',
+      clientFullName: 'Fname Lname',
+      ssn: '2434324324234',
+      dob: '23/12/2023',
+    },
+    {
+      clientId: '12',
+      clientFullName: 'Fname Lname',
+      ssn: '2434324324234',
+      dob: '23/12/2023',
+    },
+    {
+      clientId: '12',
+      clientFullName: 'Fname Lname',
+      ssn: '2434324324234',
+      dob: '23/12/2023',
+    },
+    {
+      clientId: '12',
+      clientFullName: 'Fname Lname',
+      ssn: '2434324324234',
+      dob: '23/12/2023',
+    },
+    {
+      clientId: '12',
+      clientFullName: 'Fname Lname',
+      ssn: '2434324324234',
+      dob: '23/12/2023',
+    },
+  ];
+}
