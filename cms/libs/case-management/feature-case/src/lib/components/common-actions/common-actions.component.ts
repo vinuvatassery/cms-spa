8e74a22a688014347d7d4780d621ab6201056f95
Anoop V J <anoop.jose@speridian.com>
2023-07-14 19:35:59 +0530

    7122: event log changes

--- cms/libs/case-management/feature-case/src/lib/components/common-actions/common-actions.component.ts
+++ cms/libs/case-management/feature-case/src/lib/components/common-actions/common-actions.component.ts
@@ -1,7 +1,9 @@
 /** Angular **/
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core';
 /** External libraries **/
 import { DialItem } from '@progress/kendo-angular-buttons';
+import {ProfileTabFacade, CaseFacade} from '@cms/case-management/domain';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'case-management-common-actions',
@@ -15,8 +17,35 @@ export class CommonActionsComponent {
   isShownDirectMessage = false;
   isShownTodoReminders = false;
   clickedContact!: any;
+  clientProfileHeader$ = this.caseFacade.clientProfileHeader$;
   item: Array<DialItem> = [{}];
+  clientCaseEligibilityId: any
+  clientId: any
+  eventLogCount:any;
+  eventLog$ = this.profileTabFacade.eventLog$;
 
+  constructor(private profileTabFacade: ProfileTabFacade,private readonly caseFacade: CaseFacade,){
+
+  }
+  ngOnInit(): void {
+
+    this.profileHeaderLoad();
+    
+  }
+
+  profileHeaderLoad(){
+    this.clientProfileHeader$
+      ?.pipe(first((clientHeaderData: any) => clientHeaderData?.clientId > 0))
+      .subscribe((clientHeaderData: any) => {
+        if (clientHeaderData?.clientId > 0) {          
+          this.clientId = clientHeaderData?.clientId;
+          this.clientCaseEligibilityId = clientHeaderData?.clientCaseEligibilityId;  
+          debugger;  
+          this.loadEventLog()    
+        }
+      });
+
+  }
   /** Internal event methods **/
   onDialItemClicked(event: any): void {
     this.clickedContact = event.item;
@@ -33,6 +62,10 @@ export class CommonActionsComponent {
     this.isShownDirectMessage = !this.isShownDirectMessage;
     this.isShownEventLog = false;
     this.isShownTodoReminders = false;
+  }
+
+  loadEventLog(){
+    this.profileTabFacade.loadEventLog(this.clientId,this.clientCaseEligibilityId);
   }
 
   handleShowTodoRemindersClicked() {
