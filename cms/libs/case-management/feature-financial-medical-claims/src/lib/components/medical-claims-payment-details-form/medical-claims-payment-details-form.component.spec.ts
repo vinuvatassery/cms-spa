a2f65950e98de5590bfa19bbc9a2b66f31a3c4ed
Dinesh <dinesh.panneerselvam@speridian.com>
2023-07-18 18:34:16 +0530

    code update

--- cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-payment-details-form/medical-claims-payment-details-form.component.spec.ts
+++ cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-payment-details-form/medical-claims-payment-details-form.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MedicalClaimsPaymentDetailsFormComponent } from './medical-claims-payment-details-form.component';
 
 describe('MedicalClaimsPaymentDetailsFromComponent', () => {
--- cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-payment-details-form/medical-claims-payment-details-form.component.ts
+++ cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-payment-details-form/medical-claims-payment-details-form.component.ts
@@ -2,11 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'cms-medical-claims-payment-details-form',
-  templateUrl: './medical-claims-payment-details-form.component.html',
-  styleUrls: ['./medical-claims-payment-details-form.component.scss'],
+  templateUrl: './medical-claims-payment-details-form.component.html', 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MedicalClaimsPaymentDetailsFormComponent {
-  
+export class MedicalClaimsPaymentDetailsFormComponent {  
   public formUiStyle: UIFormStyle = new UIFormStyle()
 }
