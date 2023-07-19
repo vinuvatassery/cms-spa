32dd1dc0c77afe92b93f35557d423ef37b1aae44
Dinesh <dinesh.panneerselvam@speridian.com>
2023-07-19 11:48:02 +0530

    #11805 batch items UI added

--- cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-batch-list-detail-items/medical-claims-batch-list-detail-items.component.ts
+++ cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-batch-list-detail-items/medical-claims-batch-list-detail-items.component.ts
@@ -1,4 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'; 
+/** Angular **/
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  OnChanges,
+  Output,
+} from '@angular/core';
+import { UIFormStyle } from '@cms/shared/ui-tpa'; 
+import {  GridDataResult } from '@progress/kendo-angular-grid';
+import {
+  CompositeFilterDescriptor,
+  State,
+  filterBy,
+} from '@progress/kendo-data-query';
+import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 @Component({
   selector: 'cms-medical-claims-batch-list-detail-items',
@@ -6,8 +23,149 @@ import { Router } from '@angular/router';
   styleUrls: ['./medical-claims-batch-list-detail-items.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MedicalClaimsBatchListDetailItemsComponent {
+export class MedicalClaimsBatchListDetailItemsComponent implements OnInit, OnChanges {
+ 
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  popupClassAction = 'TableActionPopup app-dropdown-action-list';
+  isBatchLogGridLoaderShow = false;
+
+  @Input() pageSizes: any;
+  @Input() sortValue: any;
+  @Input() sortType: any;
+  @Input() sort: any;
+  @Input() batchLogGridLists$: any;
+  @Output() loadVendorRefundBatchListEvent = new EventEmitter<any>();
+  public state!: State;
+  sortColumn = 'batch';
+  sortDir = 'Ascending';
+  columnsReordered = false;
+  filteredBy = '';
+  searchValue = '';
+  isFiltered = false;
+  filter!: any;
+  selectedColumn!: any;
+  gridDataResult!: GridDataResult;
+
+  gridClaimsBatchLogDataSubject = new Subject<any>();
+  gridClaimsBatchLogData$ = this.gridClaimsBatchLogDataSubject.asObservable();
+  columnDropListSubject = new Subject<any[]>();
+  columnDropList$ = this.columnDropListSubject.asObservable();
+  filterData: CompositeFilterDescriptor = { logic: 'and', filters: [] };
+  
+  
+  /** Constructor **/
   constructor(private route: Router, ) {}
+  
+  ngOnInit(): void {
+    this.loadBatchLogListGrid();
+  }
+  ngOnChanges(): void {
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+    };
+
+    this.loadBatchLogListGrid();
+  }
+
+
+  private loadBatchLogListGrid(): void {
+    this.loadBatchLog(
+      this.state?.skip ?? 0,
+      this.state?.take ?? 0,
+      this.sortValue,
+      this.sortType
+    );
+  }
+  loadBatchLog(
+    skipCountValue: number,
+    maxResultCountValue: number,
+    sortValue: string,
+    sortTypeValue: string
+  ) {
+    this.isBatchLogGridLoaderShow = true;
+    const gridDataRefinerValue = {
+      skipCount: skipCountValue,
+      pagesize: maxResultCountValue,
+      sortColumn: sortValue,
+      sortType: sortTypeValue,
+    };
+    this.loadVendorRefundBatchListEvent.emit(gridDataRefinerValue);
+    this.gridDataHandle();
+  }
+ 
+  
+  onChange(data: any) {
+    this.defaultGridState();
+
+    this.filterData = {
+      logic: 'and',
+      filters: [
+        {
+          filters: [
+            {
+              field: this.selectedColumn ?? 'vendorName',
+              operator: 'startswith',
+              value: data,
+            },
+          ],
+          logic: 'and',
+        },
+      ],
+    };
+    let stateData = this.state;
+    stateData.filter = this.filterData;
+    this.dataStateChange(stateData);
+  }
+
+  defaultGridState() {
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+      filter: { logic: 'and', filters: [] },
+    };
+  }
+
+  onColumnReorder($event: any) {
+    this.columnsReordered = true;
+  }
+
+  dataStateChange(stateData: any): void {
+    this.sort = stateData.sort;
+    this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
+    this.sortType = stateData.sort[0]?.dir ?? 'asc';
+    this.state = stateData;
+    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+    this.loadBatchLogListGrid();
+  }
+
+  // updating the pagination infor based on dropdown selection
+  pageSelectionChange(data: any) {
+    this.state.take = data.value;
+    this.state.skip = 0;
+    this.loadBatchLogListGrid();
+  }
+
+  public filterChange(filter: CompositeFilterDescriptor): void {
+    this.filterData = filter;
+  }
+
+  gridDataHandle() {
+    this.batchLogGridLists$.subscribe((data: GridDataResult) => {
+      this.gridDataResult = data;
+      this.gridDataResult.data = filterBy(
+        this.gridDataResult.data,
+        this.filterData
+      );
+      this.gridClaimsBatchLogDataSubject.next(this.gridDataResult);
+      if (data?.total >= 0 || data?.total === -1) { 
+        this.isBatchLogGridLoaderShow = false;
+      }
+    });
+    this.isBatchLogGridLoaderShow = false;
+  }
 
   backToBatchLog(event : any){  
     this.route.navigate(['/financial-management/medical-claims/batch'] );
--- cms/libs/case-management/feature-financial-medical-claims/src/lib/containers/medical-claims-batch-items-page/medical-claims-batch-items-page.component.ts
+++ cms/libs/case-management/feature-financial-medical-claims/src/lib/containers/medical-claims-batch-items-page/medical-claims-batch-items-page.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {  ChangeDetectionStrategy,  Component, } from '@angular/core';
+import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
+import { State } from '@progress/kendo-data-query';
+import { FinancialMedicalClaimsFacade } from '@cms/case-management/domain'; 
 
 @Component({
   selector: 'cms-medical-claims-batch-items-page',
@@ -6,4 +9,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./medical-claims-batch-items-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MedicalClaimsBatchItemsPageComponent {}
+export class MedicalClaimsBatchItemsPageComponent {
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  public uiTabStripScroll: UITabStripScroll = new UITabStripScroll();
+
+   sortValue = this.financialMedicalClaimsFacade.sortValueBatchLog;
+   sortType = this.financialMedicalClaimsFacade.sortType;
+   pageSizes = this.financialMedicalClaimsFacade.gridPageSizes;
+   gridSkipCount = this.financialMedicalClaimsFacade.skipCount;
+   sort = this.financialMedicalClaimsFacade.sortBatchLogList;
+   state!: State;
+   batchLogGridLists$ = this.financialMedicalClaimsFacade.batchLogData$;
+  constructor( 
+    private readonly financialMedicalClaimsFacade: FinancialMedicalClaimsFacade 
+  ) {}
+
+  loadBatchLogListGrid(event: any) { 
+    this.financialMedicalClaimsFacade.loadBatchLogListGrid();
+  }
+}
