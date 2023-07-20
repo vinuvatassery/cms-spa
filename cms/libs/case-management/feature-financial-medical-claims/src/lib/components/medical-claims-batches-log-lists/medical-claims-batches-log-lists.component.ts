158d9e6a471fa430bab75d717660da88b8e0950f
Dinesh <dinesh.panneerselvam@speridian.com>
2023-07-20 17:10:16 +0530

    removed un used code

--- cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-batches-log-lists/medical-claims-batches-log-lists.component.ts
+++ cms/libs/case-management/feature-financial-medical-claims/src/lib/components/medical-claims-batches-log-lists/medical-claims-batches-log-lists.component.ts
@@ -131,25 +131,7 @@ export class MedicalClaimsBatchesLogListsComponent implements OnInit, OnChanges 
   columnDropListSubject = new Subject<any[]>();
   columnDropList$ = this.columnDropListSubject.asObservable();
   filterData: CompositeFilterDescriptor = { logic: 'and', filters: [] };
-  
-  testttt =[
-    {
-      vendorName: 'Address `',
-      type:'address2', 
-      clientName:'address2', 
-      refundWarrant:'address2', 
-      refundAmount:'address2', 
-      depositDate:'address2', 
-      depositMethod:'address2', 
-      indexCode:'address2', 
-      pca:'address2', 
-      grant:'address2', 
-      vp:'address2', 
-      refundNote:'address2', 
-      entryDate:'address2',  
-      by: 'by',
-    },
-  ]
+   
   /** Constructor **/
   constructor(private route: Router,private dialogService: DialogService ) {}
   
