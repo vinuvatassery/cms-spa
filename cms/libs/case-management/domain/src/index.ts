f1cb5ba41581a340c5627ca92010b69c735f1b07
Dinesh Panneerselvam <dinesh.panneerselvam@speridian.com>
2023-08-04 19:56:58 -0700

    Merged PR 1987: (UI) - FM - Funding Sources Components / UI
    ![image (4).png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/1987/attachments/image%20%284%29.png)
    
    Funding Sources Ui Completed.
    List, Add, remove
    
    ![image.png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/1987/attachments/image.png)
    ![image (2).png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/1987/attachments/image%20%282%29.png)
    ![image (3).png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/1987/attachments/image%20%283%29.png)
    
    Related work items: #11731, #12249

--- cms/libs/case-management/domain/src/index.ts
+++ cms/libs/case-management/domain/src/index.ts
@@ -159,6 +159,7 @@ export * from './lib/infrastructure/financial-management/vendor-refund.data.serv
 export * from './lib/infrastructure/financial-management/financial-claims.data.service'; 
 export * from './lib/infrastructure/financial-management/financial-premiums.data.service';
 export * from './lib/infrastructure/financial-management/pharmacy-claims.data.service';
+export * from './lib/infrastructure/financial-management/financial-funding-source.data.service';
 
 
 /** Facades **/
@@ -210,6 +211,7 @@ export * from './lib/application/financial-management/vendor-refund.facade';
 export * from './lib/application/financial-management/financial-claims.facade'; 
 export * from './lib/application/financial-management/financial-premiums.facade';
 export * from './lib/application/financial-management/pharmacy-claims.facade';
+export * from './lib/application/financial-management/financial-funding-source.facade';
 
 
 
