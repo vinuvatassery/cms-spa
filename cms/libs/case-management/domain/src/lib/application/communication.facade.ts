97eefe96acc06c262981f6bf738e41209a65a939
M B S Nayak <69388113+NayakMegavath@users.noreply.github.com>
2023-08-07 11:04:02 +0530

    removed hardcoded error logging

--- cms/libs/case-management/domain/src/lib/application/communication.facade.ts
+++ cms/libs/case-management/domain/src/lib/application/communication.facade.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { Email } from '../entities/email';
 /** Data services **/
 import { EmailDataService } from '../infrastructure/email.data.service';
-import { CommunicationEvents } from '../enums/communication-event.enum';
+import { LoggingService} from '@cms/shared/util-core';
 
 @Injectable({ providedIn: 'root' })
 export class CommunicationFacade {
@@ -25,7 +25,7 @@ export class CommunicationFacade {
   ddlEmails$ = this.ddlEmailsSubject.asObservable();
 
   /** Constructor**/
-  constructor(private readonly emailDataService: EmailDataService) {}
+  constructor(private readonly emailDataService: EmailDataService, private readonly loggingService: LoggingService,) {}
 
   /** Public methods **/
   loadEmails(): void {
@@ -34,7 +34,7 @@ export class CommunicationFacade {
         this.emailsSubject.next(emailsResponse);
       },
       error: (err: any) => {
-        console.error('err', err);
+        this.loggingService.logException(err);
       },
     });
   }
@@ -45,7 +45,7 @@ export class CommunicationFacade {
         this.ddlLetterTemplatesSubject.next(ddlLetterTemplatesResponse);
       },
       error: (err: any) => {
-        console.error('err', err);
+        this.loggingService.logException(err);
       },
     });
   }
@@ -56,7 +56,7 @@ export class CommunicationFacade {
         this.ddlEmailsSubject.next(ddlEmailsResponse);
       },
       error: (err: any) => {
-        console.error('err', err);
+        this.loggingService.logException(err);
       },
     });
   }
@@ -67,7 +67,7 @@ export class CommunicationFacade {
         this.clientVariablesSubject.next(clientVariablesResponse);
       },
       error: (err: any) => {
-        console.error('err', err);
+        this.loggingService.logException(err);
       },
     });
   }
@@ -78,89 +78,8 @@ export class CommunicationFacade {
         this.ddlEditorVariablesSubject.next(ddlEditorVariablesResponse);
       },
       error: (err: any) => {
-        console.error('err', err);
+        this.loggingService.logException(err);
       },
     });
   }
-
-  loadEmailTemplates(typeCode: string, channelTypeCode: string) {
-    return this.emailDataService.loadEmailTemplates(
-      typeCode, channelTypeCode
-    );
-  }
-
-  loadCERAuthorizationEmailEditVariables(lovType: string) {
-    return this.emailDataService.loadCERAuthorizationEmailVariables(lovType);
-  }
-
-  generateTextTemplate(clientId: number, clientCaseEligibilityId: string, selectedTemplate: any, requestType: string) {
-    return this.emailDataService.replaceAndGenerateTextTemplate(clientId, clientCaseEligibilityId, selectedTemplate, requestType);
-  }
-
-  loadAttachmentPreview(clientId: number, clientCaseEligibilityId: string, templateId: any) {
-    return this.emailDataService.loadAttachmentPreview(clientId, clientCaseEligibilityId, templateId);
-  }
-
-  sendLetterToPrint(clientId: number, clientCaseEligibilityId: string, selectedTemplate: any, requestType: string) {
-    return this.emailDataService.sendLetterToPrint(clientId, clientCaseEligibilityId, selectedTemplate, requestType);
-  }
-
-  saveForLaterEmailTemplate(draftTemplate: any){
-    return this.emailDataService.saveEmailForLater(draftTemplate);
-  }
-
-  getClientDocumentsViewDownload(clientDocumentId: string) {
-    return this.emailDataService.getClientDocumentsViewDownload(clientDocumentId);
-  }
-
-  loadCERAuthorizationTemplateAttachment(typeCode: string){
-    return this.emailDataService.getTemplateAttachment(typeCode);
-  }
-
-  loadCERAuthorizationDraftAttachment(esignRequestId: string){
-    return this.emailDataService.getDraftTemplateAttachment(esignRequestId);
-  }
-
-  getCCList(clientId: number, loginUserId: string){
-    return this.emailDataService.getCCEmailListForCER(clientId, loginUserId);
-  }
-
-  loadLetterAttachment(documentTemplateId: string, typeCode: string){
-    return this.emailDataService.getLetterAttachment(documentTemplateId, typeCode);
-}
-
-preparePreviewModelData(emailData: any) {
-  const formData = new FormData();
-      formData.append('documentTemplateId', emailData?.documentTemplateId ?? '');
-      formData.append('typeCode', emailData?.typeCode ?? '');
-      formData.append('subtypeCode', CommunicationEvents?.Email ?? '');
-      formData.append('channelTypeCode', CommunicationEvents?.Email ?? '');
-      formData.append('description', emailData?.description ?? '');
-      formData.append('templateContent', emailData?.templateContent ?? '');
-      return formData;
-}
-
-prepareSendLetterData(draftTemplate: any, cerEmailAttachedFiles: any[]) {
-  const isSaveForLater = "true";
-  const formData = new FormData();
-  formData.append('documentTemplateId', draftTemplate?.documentTemplateId ?? '');
-  formData.append('systemCode', draftTemplate?.systemCode ?? '');
-  formData.append('typeCode', draftTemplate?.typeCode ?? '');
-  formData.append('subtypeCode', draftTemplate?.subtypeCode ?? '');
-  formData.append('channelTypeCode', draftTemplate?.channelTypeCode ?? '');
-  formData.append('languageCode', draftTemplate?.languageCode ?? '');
-  formData.append('description', draftTemplate?.description ?? '');
-  formData.append('templateContent', draftTemplate?.templateContent ?? '');
-  formData.append('isSaveForLater', isSaveForLater??'');
-  let i = 0;
-  cerEmailAttachedFiles.forEach((file) => { 
-    if(file.rawFile == undefined || file.rawFile == null){
-      formData.append('savedAttachmentId', file.document.documentTemplateId);
-      i++;
-    }else{
-      formData.append('fileData', file.rawFile); 
-    }
-});  
-return formData;
-}
 }
