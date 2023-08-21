e1a574f34233ff68a6ba038aeaaa51007db1a155
Vikas Birendraprasad Shukla <vikas.shukla@speridian.com>
2023-08-21 23:05:22 +0530

    Reconciling Payments Breakout Panel for Dental/Medical SPA Task Id 12485 and 12486
    Reconciling Payments Breakout Panel for Dental/Medical SPA Task Id 12485 and 12486

--- cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-batches-reconcile-payments-breakout/financial-claims-batches-reconcile-payments-breakout.component.ts
+++ cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-batches-reconcile-payments-breakout/financial-claims-batches-reconcile-payments-breakout.component.ts
@@ -1,13 +1,9 @@
-import { ChangeDetectionStrategy, Component,Input,Output,EventEmitter, OnInit,OnChanges,TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy,ChangeDetectorRef, Component,Input,Output,EventEmitter, OnInit,OnChanges,TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
-import { GridDataResult } from '@progress/kendo-angular-grid';
+import { GridDataResult,ColumnVisibilityChangeEvent, ColumnComponent } from '@progress/kendo-angular-grid';
 import { DialogService } from '@progress/kendo-angular-dialog';
-import {
-  CompositeFilterDescriptor,
-  State,
-  filterBy,
-} from '@progress/kendo-data-query';
+import { CompositeFilterDescriptor } from '@progress/kendo-data-query';
 import { Subject } from 'rxjs';
 @Component({
   selector: 'cms-financial-claims-batches-reconcile-payments-breakout',
@@ -23,12 +19,15 @@ export class FinancialClaimsBatchesReconcilePaymentsBreakoutComponent implements
   @Input() sortType: any;
   @Input() sort: any;
   @Input() reconcilePaymentBreakoutList$ :any;
-  @Input() isBreakoutPanelShow:boolean=false;
+  @Input() claimsType: any;
+  //@Input() isBreakoutPanelShow:boolean=false;
+  @Input() batchId:any;
+  @Input() entityId:any;
   @Output() loadReconcilePaymentBreakOutGridEvent = new EventEmitter<any>();
   private addClientRecentClaimsDialog: any;
-  public state!: State;
+  public state!: any;
   public formUiStyle : UIFormStyle = new UIFormStyle();   
-  sortColumn = 'clientName';
+  sortColumn = 'invoiceNbr';
   sortDir = 'Ascending';
   columnsReordered = false;
   filteredBy = '';
@@ -38,13 +37,67 @@ export class FinancialClaimsBatchesReconcilePaymentsBreakoutComponent implements
   selectedColumn!: any;
   gridDataResult!: GridDataResult;
   
-  gridReconcilePaymentBreakoutListSubject = new Subject<any>();
-  gridReconcilePaymentBreakout$ = this.gridReconcilePaymentBreakoutListSubject.asObservable();
   columnDropListSubject = new Subject<any[]>();
   columnDropList$ = this.columnDropListSubject.asObservable();
   filterData: CompositeFilterDescriptor = { logic: 'and', filters: [] };
+  addRemoveColumns="Default Columns"
+  columns : any = {
+    invoiceNbr:"Invoice ID",
+    clientName:"Client Name",
+    serviceStartDate:"Service Start Date",
+    serviceEndDate:"Service End Date",
+    amountDue:"Amount Due",
+    paymentStatusCode:"Payment Status",
+    warrant:"Warrant Number",    
+    cptCode:"CPT Code",
+    entryDate:"Entry Date"     
+  }
+  
+  dropDowncolumns : any = [
+    {
+      "columnCode": "invoiceNbr",
+      "columnDesc": "Invoice ID"    
+    },
+    {
+      "columnCode": "clientName",
+      "columnDesc": "Client Name"    
+    },
+    {
+      "columnCode": "serviceStartDate",
+      "columnDesc": "Service Start Date"        
+    },
+    {
+      "columnCode": "serviceEndDate",
+      "columnDesc": "Service End Date"     
+    },
+    {
+      "columnCode": "amountDue",
+      "columnDesc": "Amount Due"         
+    },
+    {
+      "columnCode": "paymentStatusCode",
+      "columnDesc": "Payment Status"         
+    },
+    {
+      "columnCode": "warrant",
+      "columnDesc": "Warrant Number"         
+    },
+    {
+      "columnCode": "cptCode",
+      "columnDesc": "CPT Code"         
+    },
+    
+    {
+      "columnCode": "entryDate",
+      "columnDesc": "Entry Date"         
+    }    
+  ]
 
-  constructor(private route: Router,private dialogService: DialogService) { }
+  gridReconcilePaymentBreakoutListSubject = new Subject<any>();
+  gridReconcilePaymentBreakout$ = this.gridReconcilePaymentBreakoutListSubject.asObservable();
+
+
+  constructor(private readonly cdr: ChangeDetectorRef, private route: Router, private dialogService: DialogService) { }
   
   public filterChange(filter: CompositeFilterDescriptor): void {
     this.filterData = filter;
@@ -55,9 +108,6 @@ export class FinancialClaimsBatchesReconcilePaymentsBreakoutComponent implements
       take: this.pageSizes[0]?.value,
       sort: this.sort,
     };
-
-    if(this.isBreakoutPanelShow)
-    this.loadPaymentBreakoutGrid();   
   }
 
   ngOnChanges(): void {
@@ -71,13 +121,19 @@ export class FinancialClaimsBatchesReconcilePaymentsBreakoutComponent implements
 
   private loadPaymentBreakoutGrid(): void {
     this.loadBreakList(
+      this.batchId,
+      this.entityId,
+      this.claimsType,
       this.state?.skip ?? 0,
       this.state?.take ?? 0,
       this.sortValue,
-      this.sortType
+      this.sortType,
     );
   }
   loadBreakList(
+    batchId:any,
+    entityId:any,
+    claimsType:any,
     skipCountValue: number,
     maxResultCountValue: number,
     sortValue: string,
@@ -85,17 +141,89 @@ export class FinancialClaimsBatchesReconcilePaymentsBreakoutComponent implements
   ) {
     this.isGridLoaderShow = true;
     const gridDataRefinerValue = {
+      batchId:batchId,
+      entityId:entityId,
+      claimsType:claimsType,
       skipCount: skipCountValue,
       pagesize: maxResultCountValue,
       sortColumn: sortValue,
       sortType: sortTypeValue,
+      filter : this.state?.["filter"]?.["filters"] ?? []
     };
    this.loadPaymentBreakout(gridDataRefinerValue);
   }
 
+  onChange(data: any) {
+    this.defaultGridState();
+    let operator= "startswith"
+
+    if(this.selectedColumn ==="amountDue")
+    {
+      operator = "eq"
+    }
+    
+    this.filterData = {
+      logic: 'and',
+      filters: [
+        {
+          filters: [
+            {
+              field: this.selectedColumn ?? 'entryDate',
+              operator: operator,
+              value: data,
+            },
+          ],
+          logic: 'and',
+        },
+      ],
+    };
+    const stateData = this.state;
+    stateData.filter = this.filterData;
+    this.dataStateChange(stateData);
+  }
   loadPaymentBreakout(gridDataRefinerValue:any) {
     this.loadReconcilePaymentBreakOutGridEvent.emit(gridDataRefinerValue);
     this.isGridLoaderShow=false;
+  }
+  defaultGridState() {
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+      filter: { logic: 'and', filters: [] },
+    };
+  }
+  onColumnReorder($event: any) {
+    this.columnsReordered = true;
+  }
+
+  dataStateChange(stateData: any): void {
+    this.sort = stateData.sort;
+    this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
+    this.sortType = stateData.sort[0]?.dir ?? 'asc';
+    this.state = stateData;
+    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+
+    this.sortColumn = this.columns[stateData.sort[0]?.field];
+
+    if(stateData.filter?.filters.length > 0)
+    {
+      let stateFilter = stateData.filter?.filters.slice(-1)[0].filters[0];
+      this.filter = stateFilter.value;
+      this.isFiltered = true;
+      const filterList = []
+      for(const filter of stateData.filter.filters)
+      {
+        filterList.push(this.columns[filter.filters[0].field]);
+      }
+      this.filteredBy =  filterList.toString();
+    }
+    else
+    {
+      this.filter = "";
+      this.isFiltered = false
+    }
+    this.loadPaymentBreakoutGrid();    
   }
 
   pageselectionchange(data: any) {
@@ -103,32 +231,60 @@ export class FinancialClaimsBatchesReconcilePaymentsBreakoutComponent implements
     this.state.skip = 0;
     this.loadPaymentBreakoutGrid();
   }
-
-  public dataStateChange(stateData: any): void {
-    this.sort = stateData.sort;
-    this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
-    this.sortType = stateData.sort[0]?.dir ?? 'asc';
-    this.state = stateData;
-    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
-    this.loadPaymentBreakoutGrid();
-  }
-  
   gridDataHandle() {
     this.gridReconcilePaymentBreakout$.subscribe((data: GridDataResult) => {
       this.gridDataResult = data;
-      this.gridDataResult.data = filterBy(
-        this.gridDataResult.data,
-        this.filterData
-      );
       this.gridReconcilePaymentBreakoutListSubject.next(this.gridDataResult);
-      if (data?.total >= 0 || data?.total === -1) { 
+      if (data?.total >= 0 || data?.total === -1) {
         this.isGridLoaderShow = false;
       }
     });
-    this.isGridLoaderShow = false;
-
-  }
+  } 
   
+  public columnChange(e: any) {
+    let event = e as ColumnVisibilityChangeEvent;
+    const columnsRemoved = event?.columns.filter(x=> x.hidden).length
+    const columnsAdded = event?.columns.filter(x=> x.hidden === false).length
+
+  if (columnsAdded > 0) {
+    this.addRemoveColumns = 'Columns Added';
+  }
+  else {
+    this.addRemoveColumns = columnsRemoved > 0 ? 'Columns Removed' : 'Default Columns';
+  }
+
+  event.columns.forEach(column => {
+    if (column.hidden) {
+      const field = (column as ColumnComponent)?.field;
+      const mainFilters = this.state.filter.filters;
+
+      mainFilters.forEach((filter:any) => {
+          const filterList = filter.filters;
+
+          const foundFilter = filterList.find((x: any) => x.field === field);
+
+          if (foundFilter) {
+            filter.filters = filterList.filter((x: any) => x.field !== field);
+            this.clearSelectedColumn();
+          }
+        });
+      }
+      if (!column.hidden) {
+        let columnData = column as ColumnComponent;
+        this.columns[columnData.field] = columnData.title;
+      }
+
+    });
+  }
+
+  private clearSelectedColumn() {
+    this.selectedColumn = '';
+    this.filter = '';
+    this.state.searchValue = '';
+    this.state.selectedColumn = '';
+    this.state.columnName = '';
+  }
+
   clientRecentClaimsModalClicked (template: TemplateRef<unknown>, data:any): void {
     this.addClientRecentClaimsDialog = this.dialogService.open({
       content: template,
