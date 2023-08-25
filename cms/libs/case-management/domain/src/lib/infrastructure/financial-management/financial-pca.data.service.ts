8e46cbcad0ffcbe799b9c9c30facafcae34b522e
Zeeshan Qasim <zeeshan.qasim@speridian.com>
2023-08-25 09:58:34 -0700

    Merged PR 2170: (feat)-pcas-assignment-report-grid- cms-spa 11080
    #13471-feat(Financial Managment): Integrated the PCs Assignment report grid
    
    ![image.png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/2170/attachments/image.png)
    
    Related work items: #11080

--- cms/libs/case-management/domain/src/lib/infrastructure/financial-management/financial-pca.data.service.ts
+++ cms/libs/case-management/domain/src/lib/infrastructure/financial-management/financial-pca.data.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 /** External libraries **/
 import { of } from 'rxjs/internal/observable/of';
-import { ConfigurationProvider } from '@cms/shared/util-core'; 
+import { ConfigurationProvider } from '@cms/shared/util-core';
 import { GridFilterParam } from '../../entities/grid-filter-param';
 import { PcaDetails } from '../../entities/financial-management/pca-details';
 
@@ -14,189 +14,182 @@ export class FinancialPcaDataService {
     private readonly http: HttpClient,
     private readonly configurationProvider: ConfigurationProvider
   ) {}
- 
+
   loadFinancialPcaSetupListService(params: GridFilterParam) {
     return this.http.get<any>(
-      `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/search${params.convertToQueryString()}`);
+      `${
+        this.configurationProvider.appSettings.caseApiUrl
+      }/financial-management/pca/search${params.convertToQueryString()}`
+    );
   }
 
-  loadFinancialPcaAssignmentListService( ) {
+  loadFinancialPcaAssignmentListService() {
     return of([
       {
-        id:1,
+        id: 1,
         priority: 1,
         pca: '123123`',
-        object:'Third Party', 
-        objectCode:'234234', 
-        amount:'43324342.33',  
-        openDate:'MM/DD/YYYY', 
-        closeDate:'MM/DD/YYYY', 
-        totalAmount:'43324342.33', 
-        amountUsed:'43324342.33', 
-        amountLeft:'43324342.33',
-        isActive: true,  
+        object: 'Third Party',
+        objectCode: '234234',
+        amount: '43324342.33',
+        openDate: 'MM/DD/YYYY',
+        closeDate: 'MM/DD/YYYY',
+        totalAmount: '43324342.33',
+        amountUsed: '43324342.33',
+        amountLeft: '43324342.33',
+        isActive: true,
       },
       {
-        id:2,
+        id: 2,
         priority: 2,
         pca: '123123`',
-        object:'Third Party', 
-        objectCode:'234234', 
-        amount:'43324342.33',  
-        openDate:'MM/DD/YYYY', 
-        closeDate:'MM/DD/YYYY', 
-        totalAmount:'43324342.33', 
-        amountUsed:'43324342.33', 
-        amountLeft:'43324342.33',
-        isActive: false,  
+        object: 'Third Party',
+        objectCode: '234234',
+        amount: '43324342.33',
+        openDate: 'MM/DD/YYYY',
+        closeDate: 'MM/DD/YYYY',
+        totalAmount: '43324342.33',
+        amountUsed: '43324342.33',
+        amountLeft: '43324342.33',
+        isActive: false,
       },
-   
     ]);
   }
-  loadFinancialPcaReassignmentListService( ) {
+  loadFinancialPcaReassignmentListService() {
     return of([
       {
         batch: 'MMDDYYYY_XXX',
         vendor: 'Provider Name',
-        type:'TPA', 
-        clientName:'FName LName', 
-        primaryInsurance:'FName LName', 
-        memberID:'FName LName', 
-        refundWarrant:'address2', 
-        refundAmount:'address2', 
-        depositDate:'address2', 
-        depositMethod:'address2', 
-        originalWarranty:'XXXXXX', 
-        originalAmount:'XXXXXX', 
-        indexCode:'address2', 
-        pca:'address2', 
-        grant:'address2', 
-        vp:'address2', 
-        refundNote:'address2', 
-        entryDate:'XX-XX-XXXX',  
+        type: 'TPA',
+        clientName: 'FName LName',
+        primaryInsurance: 'FName LName',
+        memberID: 'FName LName',
+        refundWarrant: 'address2',
+        refundAmount: 'address2',
+        depositDate: 'address2',
+        depositMethod: 'address2',
+        originalWarranty: 'XXXXXX',
+        originalAmount: 'XXXXXX',
+        indexCode: 'address2',
+        pca: 'address2',
+        grant: 'address2',
+        vp: 'address2',
+        refundNote: 'address2',
+        entryDate: 'XX-XX-XXXX',
         by: 'by',
       },
       {
         batch: 'MMDDYYYY_XXX',
         vendor: 'Provider Name',
-        type:'TPA', 
-        clientName:'FName LName', 
-        primaryInsurance:'FName LName', 
-        memberID:'FName LName', 
-        refundWarrant:'address2', 
-        refundAmount:'address2', 
-        depositDate:'address2', 
-        depositMethod:'address2', 
-        originalWarranty:'XXXXXX', 
-        originalAmount:'XXXXXX', 
-        indexCode:'address2', 
-        pca:'address2', 
-        grant:'address2', 
-        vp:'address2', 
-        refundNote:'address2', 
-        entryDate:'XX-XX-XXXX',  
+        type: 'TPA',
+        clientName: 'FName LName',
+        primaryInsurance: 'FName LName',
+        memberID: 'FName LName',
+        refundWarrant: 'address2',
+        refundAmount: 'address2',
+        depositDate: 'address2',
+        depositMethod: 'address2',
+        originalWarranty: 'XXXXXX',
+        originalAmount: 'XXXXXX',
+        indexCode: 'address2',
+        pca: 'address2',
+        grant: 'address2',
+        vp: 'address2',
+        refundNote: 'address2',
+        entryDate: 'XX-XX-XXXX',
         by: 'by',
       },
       {
         batch: 'MMDDYYYY_XXX',
         vendor: 'Provider Name',
-        type:'TPA', 
-        clientName:'FName LName', 
-        primaryInsurance:'FName LName', 
-        memberID:'FName LName', 
-        refundWarrant:'address2', 
-        refundAmount:'address2', 
-        depositDate:'address2', 
-        depositMethod:'address2', 
-        originalWarranty:'XXXXXX', 
-        originalAmount:'XXXXXX', 
-        indexCode:'address2', 
-        pca:'address2', 
-        grant:'address2', 
-        vp:'address2', 
-        refundNote:'address2', 
-        entryDate:'XX-XX-XXXX',  
+        type: 'TPA',
+        clientName: 'FName LName',
+        primaryInsurance: 'FName LName',
+        memberID: 'FName LName',
+        refundWarrant: 'address2',
+        refundAmount: 'address2',
+        depositDate: 'address2',
+        depositMethod: 'address2',
+        originalWarranty: 'XXXXXX',
+        originalAmount: 'XXXXXX',
+        indexCode: 'address2',
+        pca: 'address2',
+        grant: 'address2',
+        vp: 'address2',
+        refundNote: 'address2',
+        entryDate: 'XX-XX-XXXX',
         by: 'by',
       },
       {
         batch: 'MMDDYYYY_XXX',
         vendor: 'Provider Name',
-        type:'TPA', 
-        clientName:'FName LName', 
-        primaryInsurance:'FName LName', 
-        memberID:'FName LName', 
-        refundWarrant:'address2', 
-        refundAmount:'address2', 
-        depositDate:'address2', 
-        depositMethod:'address2', 
-        originalWarranty:'XXXXXX', 
-        originalAmount:'XXXXXX', 
-        indexCode:'address2', 
-        pca:'address2', 
-        grant:'address2', 
-        vp:'address2', 
-        refundNote:'address2', 
-        entryDate:'XX-XX-XXXX',  
+        type: 'TPA',
+        clientName: 'FName LName',
+        primaryInsurance: 'FName LName',
+        memberID: 'FName LName',
+        refundWarrant: 'address2',
+        refundAmount: 'address2',
+        depositDate: 'address2',
+        depositMethod: 'address2',
+        originalWarranty: 'XXXXXX',
+        originalAmount: 'XXXXXX',
+        indexCode: 'address2',
+        pca: 'address2',
+        grant: 'address2',
+        vp: 'address2',
+        refundNote: 'address2',
+        entryDate: 'XX-XX-XXXX',
         by: 'by',
       },
     ]);
   }
-  loadFinancialPcaReportListService( ) {
-    return of([
-      {
-        vendorName: 'Address `',
-        type:'address2', 
-        clientName:'address2', 
-        refundWarrant:'address2', 
-        refundAmount:'address2', 
-        depositDate:'address2', 
-        depositMethod:'address2', 
-        indexCode:'address2', 
-        pca:'address2', 
-        grant:'address2', 
-        vp:'address2', 
-        refundNote:'address2', 
-        entryDate:'address2',  
-        by: 'by',
-      },
-      {
-        vendorName: 'Address `',
-        type:'address2', 
-        clientName:'address2', 
-        refundWarrant:'address2', 
-        refundAmount:'address2', 
-        depositDate:'address2', 
-        depositMethod:'address2', 
-        indexCode:'address2', 
-        pca:'address2', 
-        grant:'address2', 
-        vp:'address2', 
-        refundNote:'address2', 
-        entryDate:'address2',  
-        by: 'by',
-      },
-     
-    ]);
-  } 
 
-   /* PCA setup */
+  loadFinancialPcaReportListService(
+    skipcount: number,
+    maxResultCount: number,
+    sort: string,
+    sortType: string,
+    filter: string
+  ) {
+    const PcaAssignmentsPageAndSortedRequestDto = {
+      SortType: sortType,
+      Sorting: sort,
+      SkipCount: skipcount,
+      MaxResultCount: maxResultCount,
+      Filter: filter,
+    };
+
+    return this.http.post<any>(
+      `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca-assignments/report`,
+      PcaAssignmentsPageAndSortedRequestDto
+    );
+  }
+
+  /* PCA setup */
 
   loadPcaById(pcaId: string) {
-    return this.http.get<any>(`${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/${pcaId}`); 
+    return this.http.get<any>(
+      `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/${pcaId}`
+    );
   }
-  
- savePca(pcaModel: PcaDetails){
-  return this.http.post<any>(
-    `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca`, pcaModel);
- }
 
- updatePca(pcaId: string, pcaModel: PcaDetails){
-  return this.http.put<any>(
-    `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/${pcaId}`, pcaModel);
- }
+  savePca(pcaModel: PcaDetails) {
+    return this.http.post<any>(
+      `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca`,
+      pcaModel
+    );
+  }
 
- deletePca(pcaId: string){
-  return this.http.delete<any>(
-    `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/${pcaId}`);
- }
+  updatePca(pcaId: string, pcaModel: PcaDetails) {
+    return this.http.put<any>(
+      `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/${pcaId}`,
+      pcaModel
+    );
+  }
+
+  deletePca(pcaId: string) {
+    return this.http.delete<any>(
+      `${this.configurationProvider.appSettings.caseApiUrl}/financial-management/pca/${pcaId}`
+    );
+  }
 }
