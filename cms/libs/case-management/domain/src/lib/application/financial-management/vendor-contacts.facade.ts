1ab49362ff33e15a1715c8714633e9a227bac1ba
Toqeer Yousaf <toqeer.yousaf@speridian.com>
2023-08-28 07:41:08 -0700

    Merged PR 2167: feat-(feat-13680)-pagination-implementation-on-contact-grid
    there is no code Smell in my Code
    
    ![image.png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/2167/attachments/image.png)
    
    Related work items: #13680

--- cms/libs/case-management/domain/src/lib/application/financial-management/vendor-contacts.facade.ts
+++ cms/libs/case-management/domain/src/lib/application/financial-management/vendor-contacts.facade.ts
@@ -22,7 +22,7 @@ export class VendorContactsFacade {
   public gridPageSizes =
     this.configurationProvider.appSettings.gridPageSizeValues;
   public skipCount = this.configurationProvider.appSettings.gridSkipCount;
-  public sortValue = 'address1';
+  public sortValue = 'contactName';
   public sortType = 'asc';
   public sort: SortDescriptor[] = [
     {
@@ -92,12 +92,16 @@ export class VendorContactsFacade {
       },
     });
   }
-  loadcontacts(vendorAddressId:string)
+  loadcontacts(vendorAddressId:string, skip: any, pageSize: any, sortBy: any, sortType: any, filters:any)
   {
     this.showLoader();
-    this.vendorcontactsDataService.loadcontacts(vendorAddressId).subscribe({
+    this.vendorcontactsDataService.loadcontacts(vendorAddressId,skip,pageSize, sortBy, sortType, filters).subscribe({
       next:(res:any)=>{
-      this.contactsSubject.next(res);
+        const gridView: any = {
+          data: res.items,
+          total:res.totalCount,
+        };
+      this.contactsSubject.next(gridView);
       this.hideLoader();
       },
       error:(err:any)=>{
@@ -132,7 +136,7 @@ export class VendorContactsFacade {
           this.loaderService.hide();
          }
          this.loaderService.hide();
-         this.notificationSnackbarService.manageSnackBar(SnackBarNotificationType.SUCCESS, ' Address De-Activated Successfully');
+         this.notificationSnackbarService.manageSnackBar(SnackBarNotificationType.SUCCESS, 'Contact  De-Activated Successfully');
        },
        error: (err) => {
         resolve(false);
@@ -153,7 +157,7 @@ export class VendorContactsFacade {
           if (response === true) {
             this.removeContactAddressSubject.next(true);
             resolve(true);
-            this.notificationSnackbarService.manageSnackBar(SnackBarNotificationType.SUCCESS, 'Address Removed Successfully');
+            this.notificationSnackbarService.manageSnackBar(SnackBarNotificationType.SUCCESS, 'Contact Removed Successfully');
             this.loaderService.hide();
           }
           this.loaderService.hide();
