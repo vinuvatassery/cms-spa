0938dca3399abe1e1739904ef12d3adf589ef226
Srinivasa Rao Yalamanchi <srinivasarao.y@speridian.com>
2023-08-30 07:42:35 -0700

    Merged PR 2204: Defect Fixes for 13168, 13375, 13540,13544,13374,13366,13537
    Defect Fixes for 13168, 13375, 13540,13544,13374,13366,13537
    - Bug 13537 Fix.
    - Fix  BUG 13366
    - Fix  Bug 13374
    - Update financial-payment-sub-list.component.html
    - fix - 13544 - hyperlink added in client name
    - fix - 13540 - columns updated in PP claims
    - fix- 13375 - updated options column width
    - fix - 13168 - NPI column added in add PP tab
    - Button  label change for Bug 13168
    
    Related work items: #13168, #13366, #13374, #13375, #13537, #13540, #13544, #13833, #13835

--- cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-page/financial-vendor-page.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-page/financial-vendor-page.component.ts
@@ -127,6 +127,7 @@ export class FinancialVendorPageComponent implements OnInit {
       lastName:[],
       providerName: [''],
       tinNumber: [''],
+      npiNbr: [''],
       paymentMethod: [''],
       specialHandling: [''],
       mailCode: [''],
