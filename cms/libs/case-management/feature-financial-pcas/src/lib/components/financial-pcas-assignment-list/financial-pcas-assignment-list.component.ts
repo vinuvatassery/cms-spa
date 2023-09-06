429a452d9e5bba5f2c49f3e5720534c1c563c128
LakshmiKambham <103560729+LakshmiKambham@users.noreply.github.com>
2023-09-06 20:16:17 +0530

    sonar qube fixes

--- cms/libs/case-management/feature-financial-pcas/src/lib/components/financial-pcas-assignment-list/financial-pcas-assignment-list.component.ts
+++ cms/libs/case-management/feature-financial-pcas/src/lib/components/financial-pcas-assignment-list/financial-pcas-assignment-list.component.ts
@@ -22,7 +22,7 @@ import {
   CompositeFilterDescriptor,
   State,
 } from '@progress/kendo-data-query';
-import { Subject, Subscription, first, fromEvent, take, tap } from 'rxjs';
+import { Subject, Subscription, first, fromEvent, tap } from 'rxjs';
 const tableRow = (node : any) => node.tagName.toLowerCase() === 'tr';
 
 const closest = (node :any, predicate : any) =>
@@ -369,7 +369,7 @@ public rowCallback(context: RowClassArgs) {
                     debugger
                     const dataItem = this.gridDataResult.data.splice(draggedItemIndex, 1)[0];
                     const dropIndex = closest(e.target, tableRow).rowIndex;
-                    const dropItem = this.gridDataResult.data[dropIndex];                
+                    this.gridDataResult.data[dropIndex];                
                     draggedItemIndex = dropIndex;
                     this.zone.run(() => this.gridDataResult.data.splice(dropIndex, 0, dataItem));
                 })
