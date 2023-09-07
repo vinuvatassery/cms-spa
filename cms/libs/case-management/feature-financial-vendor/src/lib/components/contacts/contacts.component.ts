104fc38043533b0b11f8cd23f63fb66206744dda
Muhammad Umer1 <muhammad.umer1@speridian.com>
2023-09-07 12:38:25 +0500

    vendor bug fix

--- cms/libs/case-management/feature-financial-vendor/src/lib/components/contacts/contacts.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/components/contacts/contacts.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { VendorContactsFacade } from '@cms/case-management/domain';
 import { State } from '@progress/kendo-data-query';
+import { ColumnComponent, ColumnVisibilityChangeEvent } from '@progress/kendo-angular-grid';
 @Component({
   selector: 'cms-contacts',
   templateUrl: './contacts.component.html',
@@ -20,6 +21,7 @@ export class ContactsComponent {
   public gridSkipCount = this.contactsFacade.skipCount;
   public sort = this.contactsFacade.sort;
   public state!: State;
+  public columnState!:any
   contactsGridView$ = this.contactsFacade.contactsData$;
 
  
@@ -52,6 +54,35 @@ export class ContactsComponent {
       },
     },
   ];
+  addRemoveColumns="Default Columns"
+  columns : any = {
+    clientFullName:"Client Name",
+    officialIdFullName:"Name on Official ID",
+    insuranceFullName:"Name on Primary Insurance Card",
+    pronouns:"Pronouns",
+    clientId:"Client ID",
+    urn:"URN",
+    preferredContact:"Preferred Contact",
+    caseStatus:"Status",
+    group:"Group",
+    eilgibilityStartDate:"Eligibility Start Date",
+    eligibilityEndDate:"Eligibility End Date",
+    email:"Email",
+    phone:"Phone",
+    genders:"Gender",
+    homeAddress:"Home Address",
+    ssn:"SSN",
+    insurancePolicyId:"Insurance Policy Id",
+    assignedCw:"Assigned to",
+    dateOfBirth:"Date Of Birth",
+    caseManager:"Case Manager"
+  }
+  columnsReordered: boolean = false;
+  selectedColumn!: any;
+  filter : any = "";
+  columnName: any = "";
+  statusValue = null;
+
 
    /** Constructor **/
    constructor(private readonly contactsFacade: VendorContactsFacade) {}
@@ -87,6 +118,7 @@ export class ContactsComponent {
       take: this.pageSizes[0]?.value,
       sort: this.sort,
     };
+    this.columnsReordered = false;
     this.contactsFacade.loadContactsListGrid();
   }
   clickOpenAddEditContactsDetails() {
@@ -109,5 +141,52 @@ export class ContactsComponent {
   }
   clickCloseDeleteContacts() {
     this.isContactsDeleteShow = false;
+  }
+  columnReorder(event:any){
+    this.columnsReordered = true;
+  }
+  public columnChange(e: any) {
+    let event = e as ColumnVisibilityChangeEvent;
+    const columnsRemoved = event?.columns.filter(x=> x.hidden).length
+    const columnsAdded = event?.columns.filter(x=> x.hidden === false).length
+
+  if (columnsAdded > 0) {
+    this.addRemoveColumns = 'Columns Added';
+  }
+  else {
+    this.addRemoveColumns = columnsRemoved > 0 ? 'Columns Removed' : 'Default Columns';
+  }
+
+  event.columns.forEach(column => {
+    if (column.hidden) {
+      const field = (column as ColumnComponent)?.field;
+      const mainFilters = this.columnState.filter.filters;
+
+      mainFilters.forEach((filter:any) => {
+          const filterList = filter.filters;
+
+          const foundFilter = filterList.find((x: any) => x.field === field);
+
+          if (foundFilter) {
+            filter.filters = filterList.filter((x: any) => x.field !== field);
+            this.clearSelectedColumn();
+            this.contactsFacade.loadContactsListGrid();
+          }
+        });
+      }
+      if (!column.hidden) {
+        let columnData = column as ColumnComponent;
+        this.columns[columnData.field] = columnData.title;
+      }
+
+    });
+  }
+  private clearSelectedColumn() {
+    this.selectedColumn = '';
+    this.columnName = '';
+    this.filter = '';
+    this.columnState.searchValue = '';
+    this.columnState.selectedColumn = '';
+    this.columnState.columnName = '';
   }
 }
