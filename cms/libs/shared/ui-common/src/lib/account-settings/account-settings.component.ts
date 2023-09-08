28fc6b9379d714d0aed0f036e99fa7649ddff82d
Dinesh <dinesh.panneerselvam@speridian.com>
2023-09-08 15:01:25 +0530

    #14241 AS UI

--- cms/libs/shared/ui-common/src/lib/account-settings/account-settings.component.ts
+++ cms/libs/shared/ui-common/src/lib/account-settings/account-settings.component.ts
@@ -9,4 +9,35 @@ import { UIFormStyle } from '@cms/shared/ui-tpa';
 })
 export class AccountSettingsComponent {
   public formUiStyle : UIFormStyle = new UIFormStyle();
+  public listItems = [
+ {
+  lovDesc: 'Monday',
+  lovCode:"MO"
+ },
+ {
+  lovDesc: 'Tuesday',
+  lovCode:"TU"
+ },
+ {
+  lovDesc: 'Wednesday',
+  lovCode:"WE"
+ },
+ {
+  lovDesc: 'Thursday',
+  lovCode:"TH"
+ },
+ {
+  lovDesc: 'Friday',
+  lovCode:"FR"
+ },
+ {
+  lovDesc: 'Saturday',
+  lovCode:"SA"
+ },
+ {
+  lovDesc: 'Sunday',
+  lovCode:"SU"
+ },
+  ]; 
+  public time: Date = new Date(2000, 2, 10, 13, 30, 0);
 }
