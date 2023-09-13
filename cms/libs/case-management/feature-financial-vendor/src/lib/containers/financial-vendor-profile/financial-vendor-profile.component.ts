f99ecf9c670617f2a096dbaa2d92b0d3f427ad38
LakshmiKambham <103560729+LakshmiKambham@users.noreply.github.com>
2023-09-13 16:25:17 +0530

    provider gird ui changes

--- cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-profile/financial-vendor-profile.component.ts
+++ cms/libs/case-management/feature-financial-vendor/src/lib/containers/financial-vendor-profile/financial-vendor-profile.component.ts
@@ -32,6 +32,13 @@ export class FinancialVendorProfileComponent implements OnInit {
   selectedVendorInfo$ = this.financialVendorFacade.selectedVendor$;
   vendorProfile$ = this.financialVendorFacade.vendorProfile$
   vendorProfileSpecialHandling$ = this.financialVendorFacade.vendorProfileSpecialHandling$
+
+   providerList$ = this.financialVendorFacade.providerList$
+   providerLispageSizes = this.financialVendorFacade.gridPageSizes;
+   providerLissortValue = this.financialVendorFacade.sortValue;
+   providerLissortType = this.financialVendorFacade.sortType;
+   providerLissort = this.financialVendorFacade.sort;
+  
   constructor(private activeRoute: ActivatedRoute, private financialVendorFacade : FinancialVendorFacade,
               private readonly drugsFacade: DrugsFacade) {}
 
@@ -135,4 +142,9 @@ export class FinancialVendorProfileComponent implements OnInit {
   loadVendorDetailList(){
     this.financialVendorFacade.loadVendorList(this.vendorTypeCode);
   }
+
+  loadProviderList(data :any){
+    this.financialVendorFacade.getProviderList(data)
+  }
+
 }
