0dc288cc9a509bd18a750007abbca268e68a3183
Talha Afzal <talha.afzal@speridian.com>
2023-10-06 13:44:12 -0700

    Merged PR 2554: #910:(task)-Pending Payment grid
    ![image (2).png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/2554/attachments/image%20%282%29.png)
    
    Related work items: #910, #14616

---
+++ cms/libs/productivity-tools/domain/src/lib/enums/approval-type-code.enum.ts
@@ -0,0 +1,6 @@
+export enum ApprovalTypeCode{
+  L1Approval ='L1_APPROVAL',
+  L2Approval ='L2_APPROVAL',
+  ExceedApprovalLimit ='EXCEED_APPROVAL_LIMIT'
+  
+}
--- cms/libs/productivity-tools/feature-approval/src/lib/containers/approval-page/approval-page.component.ts
+++ cms/libs/productivity-tools/feature-approval/src/lib/containers/approval-page/approval-page.component.ts
@@ -3,10 +3,10 @@ import { Component,  ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
 import { State } from '@progress/kendo-data-query';
 /** Facades **/
-import { ApprovalFacade, PendingApprovalPaymentFacade } from '@cms/productivity-tools/domain';
+import { ApprovalFacade, PendingApprovalPaymentFacade, UserRoleType } from '@cms/productivity-tools/domain';
 import { ReminderNotificationSnackbarService, ReminderSnackBarNotificationType, DocumentFacade } from '@cms/shared/util-core';
 import { NotificationService } from '@progress/kendo-angular-notification';
-import { NavigationMenuFacade, UserManagementFacade } from '@cms/system-config/domain';
+import { NavigationMenuFacade, UserManagementFacade, UserDataService } from '@cms/system-config/domain';
 @Component({
   selector: 'productivity-tools-approval-page',
   templateUrl: './approval-page.component.html',
@@ -35,25 +35,25 @@ export class ApprovalPageComponent implements OnInit {
   userLevel = 1;
 
   state!: State;
-  approvalsGeneralLists$ = this.approvalFacade.approvalsGeneralList$; 
+  approvalsGeneralLists$ = this.approvalFacade.approvalsGeneralList$;
   approvalsImportedClaimsLists$ = this.approvalFacade.approvalsImportedClaimsLists$;
   pendingApprovalCount$ = this.navigationMenuFacade.pendingApprovalCount$;
   approvalsPaymentsLists$ = this.pendingApprovalPaymentFacade.pendingApprovalGrid$;
   approvalsPaymentsMainLists$ = this.pendingApprovalPaymentFacade.pendingApprovalMainList$;
   pendingApprovalSubmittedSummary$ = this.pendingApprovalPaymentFacade.pendingApprovalSubmittedSummary$;
+  pendingApprovalSubmit$ = this.pendingApprovalPaymentFacade.pendingApprovalSubmit$;
   batchDetailPaymentsList$ = this.pendingApprovalPaymentFacade.pendingApprovalBatchDetailPaymentsGrid$;
   batchDetailPaymentsCount$ = this.pendingApprovalPaymentFacade.pendingApprovalBatchDetailPaymentsCount$;
   /** Constructor **/
-  constructor(private readonly approvalFacade: ApprovalFacade, private notificationService: NotificationService,     
+  constructor(private readonly approvalFacade: ApprovalFacade, private notificationService: NotificationService,
               private readonly reminderNotificationSnackbarService : ReminderNotificationSnackbarService,
               private pendingApprovalPaymentFacade: PendingApprovalPaymentFacade,
               private userManagementFacade: UserManagementFacade,
               private navigationMenuFacade: NavigationMenuFacade,
-              private documentFacade :  DocumentFacade) {                
+              private documentFacade :  DocumentFacade,  private readonly userDataService: UserDataService) {
               }
   ngOnInit(): void {
     this.getUserRole();
-    this.navigationMenuFacade.getAllPendingApprovalPaymentCount(this.userLevel);
   }
 
    loadApprovalsGeneralGrid(event: any): void {
@@ -61,13 +61,17 @@ export class ApprovalPageComponent implements OnInit {
   }
 
   getUserRole(){
-    if(this.userManagementFacade.hasRole("FM1")){
-      this.userLevel = 1;
-      return;
-    }
-    if(this.userManagementFacade.hasRole("FM2")){
-      this.userLevel = 2;
-    }
+    this.userDataService.getProfile$.subscribe((profile:any)=>{
+      if(profile?.length>0){
+        if(this.userManagementFacade.hasRole(UserRoleType.Level2)){
+          this.userLevel = 2;
+        }
+        else if(this.userManagementFacade.hasRole(UserRoleType.Level1)){
+          this.userLevel = 1;
+        }
+        this.navigationMenuFacade.getAllPendingApprovalPaymentCount(this.userLevel);
+      }
+    })
   }
 
   loadApprovalsPaymentsGrid(gridDataValue : any): void {
@@ -77,12 +81,13 @@ export class ApprovalPageComponent implements OnInit {
     this.dataExportParameters = gridDataValue;
     this.pendingApprovalPaymentFacade.getPendingApprovalPaymentGrid(gridDataValue , gridDataValue.selectedPaymentType, this.userLevel)
   }
+
   loadImportedClaimsGrid(event: any): void {
     this.approvalFacade.loadImportedClaimsLists();
   }
   notificationTriger(){
     this.approvalFacade.NotifyShowHideSnackBar(ReminderSnackBarNotificationType.LIGHT, ' Generic reminder displays at 9AM on the day of the reminder Generic reminder displays at 9AM on the day of the reminder');
-    
+
   }
   loadApprovalsPaymentsMain(gridDataValue : any): void {
     if(!gridDataValue.selectedPaymentType || gridDataValue.selectedPaymentType.length == 0){
@@ -90,15 +95,20 @@ export class ApprovalPageComponent implements OnInit {
     }
     this.pendingApprovalPaymentFacade.getPendingApprovalPaymentMainList(gridDataValue , gridDataValue.selectedPaymentType, this.userLevel)
   }
+
   loadBatchDetailPaymentsGrid(gridDataValue : any): void {
     if(!gridDataValue.batchId || gridDataValue.batchId.length == 0){
       return;
     }
     this.pendingApprovalPaymentFacade.getPendingApprovalBatchDetailPaymentsGrid(gridDataValue, gridDataValue.batchId, gridDataValue.selectedPaymentType)
   }
-  
+
   loadSubmittedSummary(events:any): void {
     this.pendingApprovalPaymentFacade.loadSubmittedSummary(events);
+  }
+  submitData(events:any)
+  {
+    this.pendingApprovalPaymentFacade.submitForApproval(events);
   }
 
   exportPendingApprovalGridData(){
