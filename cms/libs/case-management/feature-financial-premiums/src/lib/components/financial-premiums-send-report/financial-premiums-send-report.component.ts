50a383c6e89ca7e60e973cb5fa3673b9628fe671
Bheem Sen Nayak Megavath <bheem.megavath@speridian.com>
2023-10-09 11:57:28 +0530

    #7669-send report button and snack bar change

--- cms/libs/case-management/feature-financial-premiums/src/lib/components/financial-premiums-send-report/financial-premiums-send-report.component.ts
+++ cms/libs/case-management/feature-financial-premiums/src/lib/components/financial-premiums-send-report/financial-premiums-send-report.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { LoaderService, LoggingService, SnackBarNotificationType, NotificationSnackbarService } from '@cms/shared/util-core';
 @Component({
   selector: 'cms-financial-premiums-send-report',
   templateUrl: './financial-premiums-send-report.component.html',
@@ -7,10 +7,39 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 })
 export class FinancialPremiumsSendReportComponent {
   @Output() sendReportCloseClickedEvent = new EventEmitter();
+
+  @Input() selectedSendReportList!: any;
+  @Input() sendReportCount: number = 0;
+
+      /** Constructor **/
+      constructor(
+          private readonly loaderService: LoaderService,
+          private readonly loggingService: LoggingService,
+          private readonly notificationSnackbarService : NotificationSnackbarService,
+        ) {}
+
   closeSendReportClicked() {
     this.sendReportCloseClickedEvent.emit(true);
   }
 
-  @Input() selectedSendReportList!: any;
-  @Input() sendReportCount: number = 0;
+  onSendReportClicked(){
+    this.showHideSnackBar(SnackBarNotificationType.SUCCESS , 'Report sent!')
+    this.closeSendReportClicked();
+  }
+
+  showHideSnackBar(type : SnackBarNotificationType , subtitle : any)
+  {
+      if(type == SnackBarNotificationType.ERROR)
+      {
+        const err= subtitle;
+        this.loggingService.logException(err)
+      }
+        this.notificationSnackbarService.manageSnackBar(type,subtitle)
+        this.hideLoader();
+  }
+
+  hideLoader()
+  {
+    this.loaderService.hide();
+  }
 }
