82cd042c21da8d15f9fa1559316d70651b6bdefb
sobia <sobia.kiran@speridian.com>
2023-10-10 18:34:08 +0500

    UI_complete

--- cms/libs/case-management/feature-financial-pharmacy-claims/src/lib/components/pharmacy-claims-provider-info/pharmacy-claims-provider-info.component.ts
+++ cms/libs/case-management/feature-financial-pharmacy-claims/src/lib/components/pharmacy-claims-provider-info/pharmacy-claims-provider-info.component.ts
@@ -3,8 +3,13 @@ import {
   ChangeDetectionStrategy,
   Output, 
   EventEmitter,
+  Input,
+  ChangeDetectorRef,
 } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'cms-pharmacy-claims-provider-info',
@@ -13,8 +18,40 @@ import { UIFormStyle } from '@cms/shared/ui-tpa';
 })
 export class PharmacyClaimsProviderInfoComponent {
   @Output() closeViewProviderDetailClickedEvent = new EventEmitter();
+  @Input()vendorProfile$: Observable<any> | undefined;
+  @Input() paymentMethodCode$ : Observable<any> | undefined;
+  @Input() ddlStates$ : Observable<any> | undefined;
+  @Input() updateProviderPanelSubject$ : Observable<any> | undefined;
+  @Output() updateProviderProfileEvent = new EventEmitter<any>();
+  @Output() getProviderPanelEvent = new EventEmitter<any>();
+  @Input() paymentRequestId:any
   isEditProvider = false;
- 
+  isSubmitted: boolean = false
+  profileForm = this.formBuilder.group({
+    tin: [''],
+    address: this.formBuilder.group({
+      vendorAddressId: [''],
+      paymentMethod: [''],
+      address1: ['', Validators.required],
+      address2: [''],
+      cityCode: ['', Validators.required],
+      stateCode: ['', Validators.required],
+      zip: ['', [Validators.required, Validators.pattern('^[A-Za-z0-9 \-]+$')]],
+      specialHandlingDesc: [''],
+      mailCode: [{ value: '', disabled: true }]
+    }),
+    contacts: new FormArray([])
+  })
+  vendorProfile:any
+  constructor(public formBuilder: FormBuilder, 
+    public activeRoute: ActivatedRoute,
+    private route: Router,
+    private readonly changeDetectorRef: ChangeDetectorRef) {
+}
+ngOnInit(): void {
+  this.paymentRequestId= this.paymentRequestId? this.paymentRequestId: this.activeRoute.snapshot.queryParams['pid'];
+   this.loadVendorInfo()
+ }
   public formUiStyle : UIFormStyle = new UIFormStyle();
   closeViewProviderClicked() {
     this.closeViewProviderDetailClickedEvent.emit(true);
@@ -22,5 +59,101 @@ export class PharmacyClaimsProviderInfoComponent {
 
   editProviderClicked(){
     this.isEditProvider = !this.isEditProvider
+  }
+  get addressForm(){
+    return this.profileForm.get("address")
+    }
+    getEmailsArray(contact:any) :FormArray{
+      return contact.get('emails') as unknown as FormArray<FormGroup>
+    }
+    getPhonesArray(contact: any):FormArray {
+      return contact.get('phones') as unknown as FormArray<FormGroup>
+    }
+    get contactsArray(): FormArray<FormGroup> {
+      return this.profileForm.get("contacts") as unknown as  FormArray<FormGroup>;
+    }
+    onVendorProfileViewClicked() {
+      const query = {
+        queryParams: {
+          v_id: this.vendorProfile.vendorId
+        },
+      };
+      this.route.navigate(['/financial-management/vendors/profile'], query)
+      this.closeViewProviderClicked()
+    }
+    loadVendorInfo() {
+      this.vendorProfile$?.subscribe(res => {
+        this.changeDetectorRef.markForCheck()
+        this.vendorProfile = res;
+        this.isEditProvider = false
+      })
+  
+      this.getProviderPanelEvent.emit(this.paymentRequestId)
+    }
+    updateProfile() {
+      this.isSubmitted =true;
+      this.profileForm.markAllAsTouched();
+      if(!this.profileForm.valid)
+      {
+          return;
+      }
+      let providerPanelDto = {
+        vendorId: this.vendorProfile.vendorId,
+        tin: this.profileForm?.controls['tin'].value,
+        Address: {
+          vendorAddressId: this.vendorProfile.address.vendorAddressId,
+          specialHandlingDesc: this.profileForm?.controls.address.controls['specialHandlingDesc']?.value,
+          paymentMethodCode: this.profileForm?.controls.address.controls['paymentMethod']?.value,
+          address1: this.profileForm?.controls.address.controls['address1']?.value,
+          address2: this.profileForm?.controls.address.controls['address2']?.value,
+          cityCode: this.profileForm?.controls.address.controls['cityCode']?.value,
+          stateCode: this.profileForm?.controls.address.controls['stateCode']?.value,
+          zip: this.profileForm?.controls.address.controls['zip']?.value,
+          contacts: this.getContactArrayFormValues()
+        }    
+      }
+      this.updateProviderProfileEvent.emit(providerPanelDto)
+      this.updateProviderPanelSubject$?.pipe(take(1)).subscribe(res=>{       
+          this.loadVendorInfo(); 
+      });
+    }
+    getContactArrayFormValues() {
+      let contact: any[] = []
+      this.profileForm.controls.contacts.controls.forEach(control => {
+        let contactForm = control as unknown as FormGroup
+        let emailsFormArray = contactForm.controls['emails'] as unknown as FormArray
+        let phonesFormArray = contactForm.controls['phones'] as unknown as FormArray
+        contact.push({
+          contactName: contactForm.controls['contactName']?.value,
+          vendorContactId: contactForm.controls['vendorContactId']?.value,
+          emails: this.getEmailArrayFormValues(emailsFormArray),
+          phones: this.getPhoneArrayFormValues(phonesFormArray)
+        })
+      });
+      return contact
+    }
+    getPhoneArrayFormValues(phonesFormArray: FormArray) {
+      let phones: any[] = []
+      phonesFormArray.controls.forEach(control => {
+       let phonesForm = control as unknown as FormGroup
+        phones.push({
+          PhoneNbr: phonesForm.controls['phoneNbr']?.value,
+          VendorContactPhoneId: phonesForm.controls['vendorContactPhoneId']?.value,
+          vendorContactId: phonesForm.controls['vendorContactId']?.value
+        })
+      })
+      return phones;
+    }
+  getEmailArrayFormValues(emailsFormArray: FormArray) {
+    let emails: any[] = []
+    emailsFormArray.controls.forEach(control => {
+      let emailForm = control as unknown as FormGroup
+      emails.push({
+        emailAddress: emailForm.controls['emailAddress']?.value,
+        VendorContactEmailId: emailForm.controls['vendorContactEmailId']?.value,  
+        vendorContactId: emailForm.controls['vendorContactId']?.value
+      })
+    })
+    return emails;
   }
 }
--- cms/libs/case-management/feature-financial-pharmacy-claims/src/lib/containers/pharmacy-claims/pharmacy-claims-page.component.ts
+++ cms/libs/case-management/feature-financial-pharmacy-claims/src/lib/containers/pharmacy-claims/pharmacy-claims-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
 import { State } from '@progress/kendo-data-query';
-import { FinancialPharmacyClaimsFacade } from '@cms/case-management/domain'; 
+import { ContactFacade, FinancialPharmacyClaimsFacade, FinancialVendorFacade } from '@cms/case-management/domain'; 
+import { LovFacade } from '@cms/system-config/domain';
 @Component({
   selector: 'cms-pharmacy-claims-page',
   templateUrl: './pharmacy-claims-page.component.html',
@@ -9,9 +10,10 @@ import { FinancialPharmacyClaimsFacade } from '@cms/case-management/domain';
 export class PharmacyClaimsPageComponent {
   public formUiStyle: UIFormStyle = new UIFormStyle();
   public uiTabStripScroll: UITabStripScroll = new UITabStripScroll();
- 
-
-
+  ddlStates$ = this.contactFacade.ddlStates$;
+  paymentMethodCode$ = this.lovFacade.paymentMethodType$
+  vendorProfile$ = this.financialVendorFacade.providePanelSubject$
+  updateProviderPanelSubject$ = this.financialVendorFacade.updateProviderPanelSubject$
    sortType = this.financialPharmacyClaimsFacade.sortType;
    pageSizes = this.financialPharmacyClaimsFacade.gridPageSizes;
    gridSkipCount = this.financialPharmacyClaimsFacade.skipCount;
@@ -25,13 +27,17 @@ export class PharmacyClaimsPageComponent {
    
 
    state!: State;
+   paymentRequestId: any;
   pharmacyClaimsProcessGridLists$ =
   this.financialPharmacyClaimsFacade.pharmacyClaimsProcessData$;
   pharmacyClaimsBatchGridLists$ = this.financialPharmacyClaimsFacade.pharmacyClaimsBatchData$;
 
   pharmacyClaimsAllPaymentsGridLists$ = this.financialPharmacyClaimsFacade.pharmacyClaimsAllPaymentsData$;
   constructor( 
-    private readonly financialPharmacyClaimsFacade: FinancialPharmacyClaimsFacade 
+    private readonly financialPharmacyClaimsFacade: FinancialPharmacyClaimsFacade ,
+    private readonly financialVendorFacade: FinancialVendorFacade,
+    private readonly lovFacade: LovFacade,
+    private readonly contactFacade: ContactFacade
   ) {}
 
 
@@ -49,5 +55,12 @@ export class PharmacyClaimsPageComponent {
   loadPharmacyClaimsAllPaymentsListGrid(event: any) {
   
     this.financialPharmacyClaimsFacade.loadPharmacyClaimsAllPaymentsListGrid();
+  }
+  updateProviderProfile(event:any){
+    console.log(event)
+    this.financialVendorFacade.updateProviderPanel(event)
+  }
+  getProviderPanel(event:any){
+    this.financialVendorFacade.getProviderPanel(event)
   }
 }
