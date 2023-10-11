db2d264f034c0735dea6202291b83a873a2483e3
Talha Afzal <talha.afzal@speridian.com>
2023-10-11 14:39:37 -0700

    Merged PR 2663: #910:(fix)- Applied fix of batch name URL
    Fixed batch name URL.
    
    Code smells are not from my files.
    
    ![image.png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/2663/attachments/image.png)
    
    Related work items: #910, #15930

--- cms/libs/productivity-tools/feature-approval/src/lib/components/approvals-payments-list/approvals-payments-list.component.ts
+++ cms/libs/productivity-tools/feature-approval/src/lib/components/approvals-payments-list/approvals-payments-list.component.ts
@@ -78,6 +78,7 @@ export class ApprovalsPaymentsListComponent implements OnInit, OnChanges{
   isApproveAllClicked : boolean = false;
   approvalsPaymentsGridPagedResult:any =[];
   approvalsPaymentsGridUpdatedResult: any=[];
+  approvalsPaymentsGridUpdatedResultDummy: any=[];
   selectedApprovalSendbackDataRows: any[] = [];
   selectedBatchIds: any = [];
   totalAmountSubmitted:any=0;
@@ -347,6 +348,7 @@ export class ApprovalsPaymentsListComponent implements OnInit, OnChanges{
     }
     this.loadApprovalPaymentsListGrid();
     this.sortByProperty();
+   
   }
 
   // updating the pagination infor based on dropdown selection
@@ -726,10 +728,12 @@ export class ApprovalsPaymentsListComponent implements OnInit, OnChanges{
       sortColumn: this.sortValue,
       sortType: this.sortType,
       columnName: this.selectedColumn,
+      sorting: null,
       filter:[]
     };
     let selectedPaymentType = this.selectedPaymentType;
     this.loadApprovalsPaymentsMainListEvent.emit({gridDataRefinerValue, selectedPaymentType});
+    this.approvalsPaymentsGridUpdatedResultDummy = this.approvalsPaymentsGridUpdatedResult;
     this.approvalsPaymentsGridUpdatedResult = [];
     this.hasPaymentPendingApproval=false;
     this.approvalsPaymentsMainLists$.subscribe((response: any) => {
@@ -889,13 +893,31 @@ export class ApprovalsPaymentsListComponent implements OnInit, OnChanges{
 
   navToBatchDetails(data : any){
     let type;
-    if(this.selectedPaymentType === PendingApprovalPaymentTypeCode.MedicalClaim)
+    switch(data.paymentRequestSubTypeCode)
     {
-      type = 'medical';
+      case PendingApprovalPaymentTypeCode.MedicalClaim: {
+        type = 'claims/medical';
+        break;
+      }  
+      case PendingApprovalPaymentTypeCode.DentalClaim: {
+        type = 'claims/dental';
+        break;
+      }        
+      case PendingApprovalPaymentTypeCode.MedicalPremium: {
+        type = 'premiums/medical';
+        break;
+      }
+      case PendingApprovalPaymentTypeCode.DentalPremium: {
+        type = 'premiums/dental';
+        break;
+      }
+      case PendingApprovalPaymentTypeCode.Pharmacy: {
+        type = 'claims/pharmacy';
+        break;
+      }      
     }
-    this.route.navigate([`/financial-management/claims/${type}/batch`],
+
+    this.route.navigate([`/financial-management/${type}/batch`],
     { queryParams :{bid: data?.paymentRequestBatchId}});
   }
-
-
 }
