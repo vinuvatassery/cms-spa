34a7dba698bcdc9c75b739e35feb35042f7a51fd
Talha Afzal <talha.afzal@speridian.com>
2023-10-13 10:55:29 +0500

    binded api to ui for general approvals

--- cms/libs/productivity-tools/domain/src/lib/infrastructure/pending-approval-general.data.service.ts
+++ cms/libs/productivity-tools/domain/src/lib/infrastructure/pending-approval-general.data.service.ts
@@ -7,22 +7,10 @@ import { of } from 'rxjs/internal/observable/of';
 @Injectable({ providedIn: 'root' })
 export class PendingApprovalGeneralService {
   /** Constructor **/
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient,private configurationProvider: ConfigurationProvider) {}
 
   /** Public methods **/
   loadApprovalsGeneral() {
-    return of([
-      { id: 1, name: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet' },
-      {
-        id: 2,
-        name: 'At vero eos',
-        description: 'At vero eos et accusam et justo duo dolores',
-      },
-      {
-        id: 3,
-        name: 'Duis autem',
-        description: 'Duis autem vel eum iriure dolor in hendrerit',
-      },
-    ]);
+   return this.http.get(`${this.configurationProvider.appSettings.productivityToolsApiUrl}/productivity-tools/approvals/general`);
   }
 }
