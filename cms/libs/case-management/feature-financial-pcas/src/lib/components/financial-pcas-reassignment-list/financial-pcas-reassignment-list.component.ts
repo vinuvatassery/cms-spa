ea6ff1da8e6662a4899a84f151f8e7635ee3a65f
Talha Afzal <talha.afzal@speridian.com>
2023-10-15 14:59:53 +0500

    default columns fix

--- cms/libs/case-management/feature-financial-pcas/src/lib/components/financial-pcas-reassignment-list/financial-pcas-reassignment-list.component.ts
+++ cms/libs/case-management/feature-financial-pcas/src/lib/components/financial-pcas-reassignment-list/financial-pcas-reassignment-list.component.ts
@@ -12,7 +12,7 @@ import {
 } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { DialogService } from '@progress/kendo-angular-dialog';
-import { GridDataResult } from '@progress/kendo-angular-grid';
+import { GridDataResult , ColumnVisibilityChangeEvent} from '@progress/kendo-angular-grid';
 import {
   CompositeFilterDescriptor,
   State,
@@ -75,6 +75,9 @@ export class FinancialPcasReassignmentListComponent
   allNotAssignedPcaSList:any;
   notSelectedPcaS:any;
   selectedPcaData:any;
+  filteredByColumnDesc = '';
+  sortColumnDesc = 'PCA #';
+  columnChangeDesc = 'Default Columns';
 
   public gridMoreActions = [
     {
@@ -122,6 +125,19 @@ export class FinancialPcasReassignmentListComponent
     },
   ];
 
+  gridColumns: { [key: string]: string } = {
+    ALL: 'All Columns',
+    pcaCode: 'PCA #',
+    objectName: 'Object',
+    group:'Group',
+    closeDate: 'Close Date',
+    originalAmount:'Orignal Amount',
+    spendAfterExpiration : 'Amount Spend After Expiration',
+    overSpendAmount:'Overspend Amount',
+    totalOverSpendAmount:'Total Overspend Amount',
+    unlimitedFlag:'Unlimited'
+  };
+
   /** Constructor **/
   constructor(
     private dialogService: DialogService,
@@ -147,12 +163,8 @@ export class FinancialPcasReassignmentListComponent
   }
 
   ngOnChanges(): void {
-    this.state = {
-      skip: 0,
-      take: this.pageSizes[0]?.value,
-      sort: this.sort,
-    };
-    this.loadPcaReassignment();
+    this.initializePCAReassignmentGrid();
+        this.loadPcaReassignment();
   }
 
   loadPcaReassignment() {
@@ -210,7 +222,11 @@ export class FinancialPcasReassignmentListComponent
     this.sortType = stateData.sort[0]?.dir ?? 'asc';
     this.state = stateData;
     this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+    this.sortColumnDesc = this.gridColumns[this.sortValue];
     this.filter = stateData?.filter?.filters;
+    if (this.filter !== null) {
+      this.filteredByColumnDesc = this.dropDownColumns?.find(i => i.columnCode === this.selectedColumn)?.columnDesc ?? ''; 
+    }
     this.loadPcaReassignment();
   }
 
@@ -339,5 +355,36 @@ public itemDisabled(itemArgs:any)
   }
   onPcaReassignmentSearch(searchValue : any){
       this.onChange(searchValue);
+  }
+
+  restPcaReassignmentGrid() {
+    this.sortValue = 'pcaCode';
+    this.sortType = 'asc';
+    this.initializePCAReassignmentGrid();
+    this.sortColumn = 'pcaCode';
+    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : "";
+    this.sortDir = this.sort[0]?.dir === 'desc' ? 'Descending' : "";
+    this.filter = [];
+    this.searchValue = '';
+    this.selectedColumn = 'ALL';
+    this.filteredByColumnDesc = '';
+    this.sortColumnDesc = this.gridColumns[this.sortValue];
+    this.columnChangeDesc = 'Default Columns';
+    // this.showDateSearchWarning = false;
+    // this.showNumberSearchWarning = false;
+    this.loadPcaReassignment();
+  }
+
+  private initializePCAReassignmentGrid(){
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+    };
+  }
+
+  columnChange(event: ColumnVisibilityChangeEvent) {
+    const columnsRemoved = event?.columns.filter(x => x.hidden).length
+    this.columnChangeDesc = columnsRemoved > 0 ? 'Columns Removed' : 'Default Columns';
   }
 }
