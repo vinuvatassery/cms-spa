66f27065c7e009506ed6f78fdd5a00650b2b8e03
Talha Afzal <talha.afzal@speridian.com>
2023-10-20 08:35:28 -0700

    Merged PR 2776: #14421: Resolved PCA code issue in edit financial claim
    none of the code smells are from the component this PR is been created.
    ![image.png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/2776/attachments/image.png)
    
    Related work items: #14421

--- cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-detail-form/financial-claims-detail-form.component.ts
+++ cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-detail-form/financial-claims-detail-form.component.ts
@@ -132,6 +132,7 @@ export class FinancialClaimsDetailFormComponent implements OnDestroy, OnInit {
   addOrEdit: any;
   selectedCPTCode: any = null;
   isSpotsPayment!: boolean;
+  pcaCode!: any;
   textMaxLength: number = 300;
   exceptionReasonMaxLength = 150;
 
@@ -369,7 +370,8 @@ export class FinancialClaimsDetailFormComponent implements OnDestroy, OnInit {
       exceptionArray : new FormArray([]),
       providerNotEligibleExceptionFlag: new FormControl(false),
       showProviderNotEligibleExceptionReason: new FormControl(false),
-      providerNotEligibleExceptionFlagText: new FormControl(this.isExcededMaxBanifitButtonText)
+      providerNotEligibleExceptionFlagText: new FormControl(this.isExcededMaxBanifitButtonText),
+      pcaCode : new FormControl('')
     });
   }
 
@@ -637,7 +639,8 @@ export class FinancialClaimsDetailFormComponent implements OnDestroy, OnInit {
         exceptionReasonCode: element.reasonForException,
         tpaInvoiceId: element.tpaInvoiceId,
         exceptionFlag: element.exceptionFlag,
-        exceptionTypeCode: element.exceptionTypeCode
+        exceptionTypeCode: element.exceptionTypeCode,
+        pcaCode:element.pcaCode
       };
       this.validateStartEndDate(service.serviceStartDate,
         service.serviceEndDate);
@@ -887,6 +890,7 @@ export class FinancialClaimsDetailFormComponent implements OnDestroy, OnInit {
       serviceForm.controls['cptCodeId'].setValue(service.cptCodeId);
       serviceForm.controls['exceptionFlag'].setValue(service.exceptionFlag);
       serviceForm.controls['exceptionTypeCode'].setValue(service.exceptionTypeCode);
+      serviceForm.controls['pcaCode'].setValue(service.pcaCode);
       let exceptionForm = this.addExceptionForm.at(i) as FormGroup;
       if(serviceForm.controls['exceptionFlag'].value === StatusFlag.Yes && !this.claimForm.controls['parentExceptionTypeCode'])
       {
