c1390f4e49070f446687180fdc93556a0d850b36
Dinesh <dinesh.panneerselvam@speridian.com>
2023-10-23 14:50:19 +0530

    #16441 ReAssign Case popup method updated

--- cms/libs/shared/ui-common/src/lib/user-profile-card/user-profile-card.component.ts
+++ cms/libs/shared/ui-common/src/lib/user-profile-card/user-profile-card.component.ts
@@ -1,82 +1,87 @@
 /** Angular **/
-import { Component, ChangeDetectionStrategy, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  OnInit,
+  EventEmitter,
+  TemplateRef,
+} from '@angular/core';
 import { UserManagementFacade } from '@cms/system-config/domain';
 import { UserDefaultRoles } from '@cms/case-management/domain';
+import { DialogService } from '@progress/kendo-angular-dialog';
 
 @Component({
   selector: 'common-user-profile-card',
   templateUrl: './user-profile-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class UserProfileCardComponent implements OnInit {
-
-  @Input() userId !: any 
-  @Input() reassign? : boolean  = false
-  @Input() sendEmail? : boolean = false
+  @Input() userId!: any;
+  @Input() reassign?: boolean = false;
+  @Input() sendEmail?: boolean = false;
   @Input() clientId: any;
   @Input() clientCaseId: any;
   @Output() reassignClicked = new EventEmitter<any>();
   userImage$ = this.userManagementFacade.userImage$;
   userById$ = this.userManagementFacade.usersById$;
   caseOwners$ = this.userManagementFacade.usersByRole$;
-  imageLoaderVisible =true;
+  imageLoaderVisible = true;
   businessLogicPopupOpen = false;
   hasReassignPermission = false;
- 
-    /** Constructor**/
-    constructor(     
-      private userManagementFacade : UserManagementFacade,
-    ) { 
-      this.hasReassignPermission = userManagementFacade.hasPermission(['Reassign_Cases']);
-    }
+  private reassignCaseTemp: any;
+  /** Constructor**/
+  constructor(
+    private userManagementFacade: UserManagementFacade,
 
+    private dialogService: DialogService
+  ) {
+    this.hasReassignPermission = userManagementFacade.hasPermission([
+      'Reassign_Cases',
+    ]);
+  }
 
   /** Lifecycle hooks **/
-  ngOnInit(): void {  
-   this.loadProfilePhoto();
-   this.loadProfileData();
-   this.loadUsersByRole();
- }
- 
-    loadProfilePhoto()
-    {   
-      if(this.userId)
-      {
-        this.userManagementFacade.getUserImage(this.userId)
-      }
-    }
+  ngOnInit(): void {
+    this.loadProfilePhoto();
+    this.loadProfileData();
+    this.loadUsersByRole();
+  }
 
-    loadProfileData()
-    { 
-      if(this.userId)
-      {
-        this.userManagementFacade.getUserById(this.userId)  
-      }  
+  loadProfilePhoto() {
+    if (this.userId) {
+      this.userManagementFacade.getUserImage(this.userId);
     }
-   
-    onLoad()
-    {    
-     this.imageLoaderVisible = false;
-    }
-    
-    openBusinessPopup()
-    {
-       this.businessLogicPopupOpen = true;
-    }
+  }
 
-    businessLogicPopupClose()
-    {
-      this.businessLogicPopupOpen = false;
+  loadProfileData() {
+    if (this.userId) {
+      this.userManagementFacade.getUserById(this.userId);
     }
+  }
 
-    loadUsersByRole()
-    {
-      this.userManagementFacade.getUsersByRole(UserDefaultRoles.CACaseWorker);
-    }
+  onLoad() {
+    this.imageLoaderVisible = false;
+  }
 
-    onReassignClicked(data:any) {
-      this.reassignClicked.emit(data);    
-    }
+  loadUsersByRole() {
+    this.userManagementFacade.getUsersByRole(UserDefaultRoles.CACaseWorker);
+  }
 
- }
+  onReassignClicked(data: any) {
+    this.reassignClicked.emit(data);
+  }
+  public openBusinessPopup(template: TemplateRef<unknown>): void {
+    this.reassignCaseTemp = this.dialogService.open({
+      content: template,
+      cssClass: 'app-c-modal app-c-modal-md app-c-modal-np',
+    });
+    this.businessLogicPopupOpen = true;
+  }
+
+  businessLogicPopupClose() {
+    this.reassignCaseTemp.close();
+    this.businessLogicPopupOpen = false;
+  }
+}
