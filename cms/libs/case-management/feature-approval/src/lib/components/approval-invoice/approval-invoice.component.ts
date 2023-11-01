71646f84068baef9dfee83337dc202bb5a92718b
kiran.prakash <kiran.prakash@speridian.com>
2023-10-31 21:57:58 -0700

    feature-approval moved from to case-management

---
+++ cms/libs/case-management/feature-approval/src/lib/components/approval-invoice/approval-invoice.component.ts
@@ -0,0 +1,322 @@
+/** Angular libraries **/
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { Router } from '@angular/router';
+
+/** External libraries **/
+import { UIFormStyle } from '@cms/shared/ui-tpa';
+import { CompositeFilterDescriptor } from '@progress/kendo-data-query';
+import { Subscription, Subject } from 'rxjs';
+/** Facade **/
+import {
+  ColumnComponent,
+  ColumnVisibilityChangeEvent,
+  FilterService,
+  GridDataResult,
+} from '@progress/kendo-angular-grid';
+import { LovFacade } from '@cms/system-config/domain';
+@Component({
+  selector: 'productivity-tools-approval-invoice',
+  templateUrl: './approval-invoice.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ApprovalInvoiceComponent implements OnInit {
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  popupClassAction = 'TableActionPopup app-dropdown-action-list';
+  isInvoiceGridLoaderShow = false;
+  @Input() exceptionId: any;
+  @Input() pageSizes: any;
+  @Input() sortValue: any;
+  @Input() sortType: any;
+  @Input() sort: any;
+  @Input() gridSkipCount: any;
+  @Input() invoiceData$: any;
+  @Input() isInvoiceLoading$: any;
+  @Output() loadApprovalsExceptionInvoiceEvent = new EventEmitter<any>();
+  public state!: any;
+  sortColumn = 'Service Start';
+  sortDir = 'Ascending';
+  sortColumnDesc: string = 'Service Start';
+  columnsReordered = false;
+  filteredBy = '';
+  searchValue = '';
+  isFiltered = false;
+  filter!: any;
+  selectedColumn!: any;
+  gridDataResult!: GridDataResult;
+
+  invoiceGridViewDataSubject = new Subject<any>();
+  invoiceGridView$ = this.invoiceGridViewDataSubject.asObservable();
+
+  isInvoiceLoadingSubject = new Subject<boolean>();
+  isInvoiceLoadingData$ = this.isInvoiceLoadingSubject.asObservable();
+  providerId: any;
+  isInvoiceLoadingSubscription!: Subscription;
+
+  columnDropListSubject = new Subject<any[]>();
+  columnDropList$ = this.columnDropListSubject.asObservable();
+  filterData: CompositeFilterDescriptor = { logic: 'and', filters: [] };
+  addRemoveColumns = 'Default Columns';
+  columns: any;
+  dropDowncolumns: any;
+
+  selectedPaymentType: string | null = null;
+  paymentRequestTypes$ = this.lovFacade.paymentRequestType$;
+  paymentRequestTypes: any = [];
+
+  paymentTypeFilter = '';
+  /** Constructor **/
+  constructor(
+    private readonly cdr: ChangeDetectorRef,
+    private readonly lovFacade: LovFacade,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadColumnsData();
+    this.getCoPaymentRequestTypeLov();
+    this.initializeGridState();
+    this.loadGeneralExceptionInvoiceGrid();
+  }
+
+  private clearSelectedColumn() {
+    this.selectedColumn = '';
+    this.filter = '';
+    this.state.searchValue = '';
+    this.state.selectedColumn = '';
+    this.state.columnName = '';
+  }
+
+  onChange(data: any) {
+    this.defaultGridState();
+    let operator = 'startswith';
+
+    if (
+      this.selectedColumn === 'serviceCost' ||
+      this.selectedColumn === 'amountDue'
+    ) {
+      operator = 'eq';
+    }
+
+    this.filterData = {
+      logic: 'and',
+      filters: [
+        {
+          filters: [
+            {
+              field: this.selectedColumn ?? 'serviceStartDate',
+              operator: operator,
+              value: data,
+            },
+          ],
+          logic: 'and',
+        },
+      ],
+    };
+    const stateData = this.state;
+    stateData.filter = this.filterData;
+    this.dataStateChange(stateData);
+  }
+
+  defaultGridState() {
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+      filter: { logic: 'and', filters: [] },
+    };
+  }
+
+  onColumnReorder($event: any) {
+    this.columnsReordered = true;
+  }
+
+  dataStateChange(stateData: any): void {
+    this.sort = stateData.sort;
+    this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
+    this.sortType = stateData.sort[0]?.dir ?? 'asc';
+    this.state = stateData;
+    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+
+    this.sortColumn = this.columns[stateData.sort[0]?.field];
+
+    if (stateData.filter?.filters.length > 0) {
+      let stateFilter = stateData.filter?.filters.slice(-1)[0].filters[0];
+      this.filter = stateFilter.value;
+      this.isFiltered = true;
+      const filterList = [];
+      for (const filter of stateData.filter.filters) {
+        filterList.push(this.columns[filter.filters[0].field]);
+      }
+      this.filteredBy = filterList.toString();
+    } else {
+      this.filter = '';
+      this.isFiltered = false;
+    }
+    if (!this.filteredBy.includes('Payment Type'))
+      this.selectedPaymentType = '';
+    this.loadGeneralExceptionInvoiceGrid();
+  }
+
+  pageSelectionChange(data: any) {
+    this.state.take = data.value;
+    this.state.skip = 0;
+    this.loadGeneralExceptionInvoiceGrid();
+  }
+
+  public filterChange(filter: CompositeFilterDescriptor): void {
+    this.filterData = filter;
+  }
+
+  gridDataHandle() {
+    this.invoiceData$.subscribe((data: GridDataResult) => {
+      this.gridDataResult = data;
+      this.invoiceGridViewDataSubject.next(this.gridDataResult);
+      if (data?.total >= 0 || data?.total === -1) {
+        this.isInvoiceGridLoaderShow = false;
+      }
+      this.isInvoiceGridLoaderShow = false;
+    });
+  }
+
+  public columnChange(e: any) {
+    let event = e as ColumnVisibilityChangeEvent;
+    const columnsRemoved = event?.columns.filter((x) => x.hidden).length;
+    const columnsAdded = event?.columns.filter(
+      (x) => x.hidden === false
+    ).length;
+
+    if (columnsAdded > 0) {
+      this.addRemoveColumns = 'Columns Added';
+    } else {
+      this.addRemoveColumns =
+        columnsRemoved > 0 ? 'Columns Removed' : 'Default Columns';
+    }
+
+    event.columns.forEach((column) => {
+      if (column.hidden) {
+        const field = (column as ColumnComponent)?.field;
+        const mainFilters = this.state.filter.filters;
+
+        mainFilters.forEach((filter: any) => {
+          const filterList = filter.filters;
+
+          const foundFilter = filterList.find((x: any) => x.field === field);
+
+          if (foundFilter) {
+            filter.filters = filterList.filter((x: any) => x.field !== field);
+            this.clearSelectedColumn();
+          }
+        });
+      }
+      if (!column.hidden) {
+        let columnData = column as ColumnComponent;
+        this.columns[columnData.field] = columnData.title;
+      }
+    });
+  }
+  private loadColumnsData() {
+    this.columns = {
+      invoiceId: 'Invoice ID',
+      serviceStartDate: 'Service Start',
+      serviceEndDate: 'Service End',
+      cptCode: 'CPT Code',
+      medicalService: 'Medical Service',
+      serviceCost: 'Service Cost',
+      amountDue: 'Amount Due',
+      paymentTypeDesc: 'Payment Type',
+      entryDate: 'Entry Date',
+    };
+    this.dropDowncolumns = [
+      { columnCode: 'invoiceId', columnDesc: 'Invoice ID' },
+      { columnCode: 'serviceStartDate', columnDesc: 'Service Start' },
+      { columnCode: 'serviceEndDate', columnDesc: 'Service End' },
+      { columnCode: 'cptCode', columnDesc: 'CPT Code' },
+      { columnCode: 'medicalService', columnDesc: 'Medical Service' },
+      { columnCode: 'serviceCost', columnDesc: 'Service Cost' },
+      { columnCode: 'amountDue', columnDesc: 'Amount Due' },
+      { columnCode: 'paymentTypeDesc', columnDesc: 'Payment Type' },
+      { columnCode: 'entryDate', columnDesc: 'Entry Date' },
+    ];
+  }
+
+  loadGeneralExceptionInvoiceGrid() {
+    this.loadInvoice(
+      this.state?.skip ?? 0,
+      this.state?.take ?? 0,
+      this.sortValue,
+      this.sortType
+    );
+  }
+
+  loadInvoice(
+    skipCountValue: number,
+    maxResultCountValue: number,
+    sortValue: string,
+    sortTypeValue: string
+  ) {
+    this.isInvoiceGridLoaderShow = true;
+    const gridDataRefinerValue = {
+      exceptionId: this.exceptionId,
+      skipCount: skipCountValue,
+      pageSize: maxResultCountValue,
+      sort: sortValue,
+      sortType: sortTypeValue,
+      filter: this.state?.['filter']?.['filters'] ?? [],
+    };
+    this.loadInvoiceListGrid(gridDataRefinerValue);
+    this.gridDataHandle();
+  }
+
+  loadInvoiceListGrid(data: any) {
+    this.loadApprovalsExceptionInvoiceEvent.emit(data);
+  }
+
+  dropdownFilterChange(
+    field: string,
+    value: any,
+    filterService: FilterService
+  ): void {
+    if (field === 'paymentTypeDesc') this.selectedPaymentType = value;
+    filterService.filter({
+      filters: [
+        {
+          field: field,
+          operator: 'eq',
+          value: value,
+        },
+      ],
+      logic: 'and',
+    });
+  }
+
+  private getCoPaymentRequestTypeLov() {
+    this.lovFacade.getCoPaymentRequestTypeLov();
+    this.paymentRequestTypes$.subscribe({
+      next: (data: any) => {
+        data.forEach((item: any) => {
+          item.lovDesc = item.lovDesc.toUpperCase();
+        });
+        this.paymentRequestTypes = data.sort(
+          (value1: any, value2: any) => value1.sequenceNbr - value2.sequenceNbr
+        );
+      },
+    });
+  }
+
+  private initializeGridState() {
+    this.state = {
+      skip: this.gridSkipCount,
+      take: this.pageSizes[0]?.value,
+      sort: [{ field: 'serviceStartDate', dir: 'desc' }],
+    };
+    this.sortColumnDesc = 'Service Start';
+  }
+}
---
+++ cms/libs/case-management/feature-approval/src/lib/containers/approval-page/approval-page.component.ts
@@ -0,0 +1,245 @@
+/** Angular **/
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { UIFormStyle, UITabStripScroll } from '@cms/shared/ui-tpa';
+import { State } from '@progress/kendo-data-query';
+/** Facades **/
+import {
+  ApprovalFacade,
+  PendingApprovalGeneralFacade,
+  PendingApprovalPaymentFacade,
+  UserRoleType,
+} from '@cms/productivity-tools/domain';
+import {
+  ReminderNotificationSnackbarService,
+  ReminderSnackBarNotificationType,
+  DocumentFacade,
+  ApiType,
+} from '@cms/shared/util-core';
+import { NotificationService } from '@progress/kendo-angular-notification';
+import {
+  NavigationMenuFacade,
+  UserManagementFacade,
+  UserDataService,
+  UserDefaultRoles,
+} from '@cms/system-config/domain';
+import { DialogService } from '@progress/kendo-angular-dialog';
+@Component({
+  selector: 'productivity-tools-approval-page',
+  templateUrl: './approval-page.component.html',
+  styleUrls: ['./approval-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ApprovalPageComponent implements OnInit {
+  /** Public properties **/
+
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  public uiTabStripScroll: UITabStripScroll = new UITabStripScroll();
+  commonReminderSnackbar$ = this.reminderNotificationSnackbarService.snackbar$;
+  sortType = this.pendingApprovalGeneralFacade.sortType;
+  pageSizes = this.pendingApprovalGeneralFacade.gridPageSizes;
+  gridSkipCount = this.pendingApprovalGeneralFacade.skipCount;
+  dataExportParameters!: any;
+
+  sortValue = this.pendingApprovalGeneralFacade.sortValue;
+  sort = this.pendingApprovalGeneralFacade.sort;
+  sortValueGeneralAPproval = this.approvalFacade.sortValueGeneralAPproval;
+  sortGeneralList = this.approvalFacade.sortGeneralList;
+  sortApprovalPaymentsList =
+    this.pendingApprovalPaymentFacade.sortApprovalPaymentsList;
+  sortValueApprovalPaymentsApproval =
+    this.pendingApprovalPaymentFacade.sortValueApprovalPaymentsApproval;
+  sortImportedClaimsList = this.approvalFacade.sortImportedClaimsList;
+  sortValueImportedClaimsAPproval =
+    this.approvalFacade.sortValueImportedClaimsAPproval;
+  exportButtonShow$ = this.documentFacade.exportButtonShow$;
+  pendingApprovalPaymentsCount$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalPaymentsCount$;
+
+  userLevel = 1;
+
+  state!: State;
+  approvalsGeneralLists$ =
+    this.pendingApprovalGeneralFacade.approvalsGeneralList$;
+  approvalsImportedClaimsLists$ =
+    this.approvalFacade.approvalsImportedClaimsLists$;
+  pendingApprovalCount$ = this.navigationMenuFacade.pendingApprovalCount$;
+  approvalsPaymentsLists$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalGrid$;
+  approvalsPaymentsMainLists$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalMainList$;
+  pendingApprovalSubmittedSummary$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalSubmittedSummary$;
+  pendingApprovalSubmit$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalSubmit$;
+  batchDetailPaymentsList$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalBatchDetailPaymentsGrid$;
+  batchDetailPaymentsCount$ =
+    this.pendingApprovalPaymentFacade.pendingApprovalBatchDetailPaymentsCount$;
+  approvalsExceptionCard$ =
+    this.pendingApprovalGeneralFacade.approvalsGeneralExceptionCardSubjectList$;
+  invoiceData$ = this.pendingApprovalGeneralFacade.invoiceData$;
+  isInvoiceLoading$ = this.pendingApprovalGeneralFacade.isInvoiceLoading$;
+  submitGenerealRequest$ =
+    this.pendingApprovalGeneralFacade.submitGenerealRequest$;
+
+  providerDetailsDialog: any;
+  @ViewChild('providerDetailsTemplate', { read: TemplateRef })
+  providerDetailsTemplate!: TemplateRef<any>;
+  paymentRequestId!: any;
+  usersByRole$ = this.userManagementFacade.usersByRole$;
+  selectedVendor$ = this.pendingApprovalGeneralFacade.selectedVendor$;
+
+  /** Constructor **/
+  constructor(
+    private readonly approvalFacade: ApprovalFacade,
+    private notificationService: NotificationService,
+    private readonly reminderNotificationSnackbarService: ReminderNotificationSnackbarService,
+    private pendingApprovalPaymentFacade: PendingApprovalPaymentFacade,
+    private userManagementFacade: UserManagementFacade,
+    private navigationMenuFacade: NavigationMenuFacade,
+    private documentFacade: DocumentFacade,
+    private readonly userDataService: UserDataService,
+    private readonly pendingApprovalGeneralFacade: PendingApprovalGeneralFacade,
+    private dialogService: DialogService
+  ) {}
+  ngOnInit(): void {
+    this.getUserRole();
+    this.userManagementFacade.getUsersByRole(UserDefaultRoles.CACaseWorker);
+    this.pendingApprovalPaymentsCount$.subscribe((response: any) => {
+      if (response) {
+        this.navigationMenuFacade.getAllPendingApprovalPaymentCount(
+          this.userLevel
+        );
+      }
+    });
+  }
+  loadApprovalsGeneralGrid(event: any): void {
+    this.pendingApprovalGeneralFacade.loadApprovalsGeneral();
+  }
+
+  getUserRole() {
+    this.userDataService.getProfile$.subscribe((profile: any) => {
+      if (profile?.length > 0) {
+        if (this.userManagementFacade.hasRole(UserRoleType.Level2)) {
+          this.userLevel = 2;
+        } else if (this.userManagementFacade.hasRole(UserRoleType.Level1)) {
+          this.userLevel = 1;
+        }
+        this.navigationMenuFacade.getAllPendingApprovalPaymentCount(
+          this.userLevel
+        );
+      }
+    });
+  }
+
+  loadApprovalsPaymentsGrid(gridDataValue: any): void {
+    if (
+      !gridDataValue.selectedPaymentType ||
+      gridDataValue.selectedPaymentType.length == 0
+    ) {
+      return;
+    }
+    this.dataExportParameters = gridDataValue;
+    this.pendingApprovalPaymentFacade.getPendingApprovalPaymentGrid(
+      gridDataValue,
+      gridDataValue.selectedPaymentType,
+      this.userLevel
+    );
+  }
+
+  loadImportedClaimsGrid(event: any): void {
+    this.approvalFacade.loadImportedClaimsLists();
+  }
+  notificationTriger() {
+    this.approvalFacade.NotifyShowHideSnackBar(
+      ReminderSnackBarNotificationType.LIGHT,
+      ' Generic reminder displays at 9AM on the day of the reminder Generic reminder displays at 9AM on the day of the reminder'
+    );
+  }
+  loadApprovalsPaymentsMain(gridDataValue: any): void {
+    if (
+      !gridDataValue.selectedPaymentType ||
+      gridDataValue.selectedPaymentType.length == 0
+    ) {
+      return;
+    }
+    this.pendingApprovalPaymentFacade.getPendingApprovalPaymentMainList(
+      gridDataValue,
+      gridDataValue.selectedPaymentType,
+      this.userLevel
+    );
+  }
+
+  loadBatchDetailPaymentsGrid(gridDataValue: any): void {
+    if (!gridDataValue.batchId || gridDataValue.batchId.length == 0) {
+      return;
+    }
+    this.pendingApprovalPaymentFacade.getPendingApprovalBatchDetailPaymentsGrid(
+      gridDataValue,
+      gridDataValue.batchId,
+      gridDataValue.selectedPaymentType
+    );
+  }
+
+  loadSubmittedSummary(events: any): void {
+    this.pendingApprovalPaymentFacade.loadSubmittedSummary(events);
+  }
+  submitData(events: any) {
+    this.pendingApprovalPaymentFacade.submitForApproval(events);
+  }
+
+  exportPendingApprovalGridData() {
+    const data = this.dataExportParameters;
+    if (data) {
+      const filter = JSON.stringify(data?.gridDataRefinerValue.filter);
+
+      const approvalPageAndSortedRequest = {
+        SortType: data?.gridDataRefinerValue.sortType,
+        Sorting: data?.gridDataRefinerValue.sortColumn,
+        SkipCount: data?.gridDataRefinerValue.skipcount,
+        MaxResultCount: data?.gridDataRefinerValue.maxResultCount,
+        Filter: filter,
+      };
+      let fileName =
+        data.selectedPaymentType[0].toUpperCase() +
+        data.selectedPaymentType.substr(1).toLowerCase() +
+        '_approvals';
+
+      this.documentFacade.getExportFile(
+        approvalPageAndSortedRequest,
+        `payment-batches?serviceType=${data.selectedPaymentType}&level=${this.userLevel}`,
+        fileName,
+        ApiType.ProductivityToolsApi
+      );
+    }
+  }
+  loadCasereassignmentExpanedInfoParentEvent(approvalId: any) {
+    this.pendingApprovalGeneralFacade.loadCasereassignmentExpandedInfo(
+      approvalId
+    );
+  }
+
+  loadApprovalsExceptionCard(data: any) {
+    this.pendingApprovalGeneralFacade.loadExceptionCard(data);
+  }
+
+  loadApprovalsExceptionInvoice(data: any) {
+    this.pendingApprovalGeneralFacade.loadInvoiceListGrid(data);
+  }
+  submitGeneralRequests(requests: any) {
+    this.pendingApprovalGeneralFacade.submitGeneralRequests(requests);
+  }
+
+  getVendorDetail(userObject: any) {
+    this.pendingApprovalGeneralFacade.getVendorDetails(
+      userObject.approvalEntityId,
+      userObject.subTypeCode
+    );
+  }
+}
