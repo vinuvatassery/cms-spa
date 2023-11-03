b258e07b796b2864d372d83c4b8a0ff8d97fcba3
Dinesh Panneerselvam <dinesh.panneerselvam@speridian.com>
2023-11-03 09:27:16 -0700

    Merged PR 2925: Add Vendor Refund Popup Process UI Updated
    Updating the Vendore refund popup design looks like claims
    
    Related work items: #16593, #16738

--- cms/libs/case-management/feature-financial-vendor-refund/src/lib/components/vendor-refund-insurance-premium-list/vendor-refund-insurance-premium-list.component.ts
+++ cms/libs/case-management/feature-financial-vendor-refund/src/lib/components/vendor-refund-insurance-premium-list/vendor-refund-insurance-premium-list.component.ts
@@ -1,7 +1,126 @@
-import { Component } from '@angular/core';
+/** Angular **/
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { UIFormStyle } from '@cms/shared/ui-tpa';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import {
+  CompositeFilterDescriptor,
+  State,
+  filterBy,
+} from '@progress/kendo-data-query';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'cms-vendor-refund-insurance-premium-list',
   templateUrl: './vendor-refund-insurance-premium-list.component.html', 
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VendorRefundInsurancePremiumListComponent {}
+export class VendorRefundInsurancePremiumListComponent  implements OnInit, OnChanges{
+
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  isPremiumLoaderShow = false;
+  /** Constructor **/
+  @Input() pageSizes: any;
+  @Input() sortValue: any;
+  @Input() sortType: any;
+  @Input() sort: any;
+  @Output() loadVendorRefundProcessListEvent = new EventEmitter<any>();
+  public state!: State;
+  @Input() premiumsListData$: any;
+  @Output() loadPremiumListEvent = new EventEmitter<any>();
+  sortColumn = 'vendorName';
+  sortDir = 'Ascending';
+  columnsReordered = false;
+  filteredBy = '';
+  searchValue = '';
+  isFiltered = false;
+  filter!: any;
+  selectedColumn!: any;
+  gridDataResult!: GridDataResult;
+
+  gridPremiumDataSubject = new Subject<any>();
+  gridPremiumData$ = this.gridPremiumDataSubject.asObservable();
+  columnDropListSubject = new Subject<any[]>();
+  columnDropList$ = this.columnDropListSubject.asObservable();
+  filterData: CompositeFilterDescriptor = { logic: 'and', filters: [] };
+  
+ 
+  ngOnInit(): void {
+    this.loadPremiumListGrid();
+  }
+  ngOnChanges(): void {
+    this.state = {
+      skip: 0,
+      take: this.pageSizes[0]?.value,
+      sort: this.sort,
+    };
+    this.loadPremiumListGrid();
+  }
+
+  private loadPremiumListGrid(): void {
+    this.loadPremiumList(
+      this.state?.skip ?? 0,
+      this.state?.take ?? 0,
+      this.sortValue,
+      this.sortType
+    );
+  }
+  loadPremiumList
+  (    skipCountValue: number,
+    maxResultCountValue: number,
+    sortValue: string,
+    sortTypeValue: string) {
+      this.isPremiumLoaderShow = true;
+      const gridDataRefinerValue = {
+        skipCount: skipCountValue,
+        pagesize: maxResultCountValue,
+        sortColumn: sortValue,
+        sortType: sortTypeValue,
+      };
+    this.loadPremiumListEvent.emit(gridDataRefinerValue);
+    this.gridDataHandle();
+  }
+  
+  dataStateChange(stateData: any): void {
+    this.sort = stateData.sort;
+    this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
+    this.sortType = stateData.sort[0]?.dir ?? 'asc';
+    this.state = stateData;
+    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+    this.loadPremiumListGrid();
+  }
+
+  // updating the pagination infor based on dropdown selection
+  pageSelectionChange(data: any) {
+    this.state.take = data.value;
+    this.state.skip = 0;
+    this.loadPremiumListGrid();
+  }
+
+  public filterChange(filter: CompositeFilterDescriptor): void {
+    this.filterData = filter;
+  }
+
+  gridDataHandle() {
+    this.premiumsListData$.subscribe((data: GridDataResult) => {
+      this.gridDataResult = data;
+      this.gridDataResult.data = filterBy(
+        this.gridDataResult.data,
+        this.filterData
+      );
+      this.gridPremiumDataSubject.next(this.gridDataResult);
+      if (data?.total >= 0 || data?.total === -1) { 
+        this.isPremiumLoaderShow = false;
+      }
+    });
+    this.isPremiumLoaderShow = false;
+
+  }
+}
