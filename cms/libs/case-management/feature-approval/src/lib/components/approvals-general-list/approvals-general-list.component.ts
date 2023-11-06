ab6b7b583be35de6d0265beb8d371e246b036801
Dinesh <dinesh.panneerselvam@speridian.com>
2023-11-06 14:15:46 +0530

    Updated the UI in #16822

--- cms/libs/case-management/feature-approval/src/lib/components/approvals-general-list/approvals-general-list.component.ts
+++ cms/libs/case-management/feature-approval/src/lib/components/approvals-general-list/approvals-general-list.component.ts
@@ -271,7 +271,7 @@ export class ApprovalsGeneralListComponent implements OnInit, OnChanges {
   onSubmitClicked(template: TemplateRef<unknown>): void {
     this.submitRequestDialogService = this.dialogService.open({
       content: template,
-      cssClass: 'app-c-modal app-c-modal-lg app-c-modal-np',
+      cssClass: 'app-c-modal app-c-modal-sm app-c-modal-np',
     });
   }
 
