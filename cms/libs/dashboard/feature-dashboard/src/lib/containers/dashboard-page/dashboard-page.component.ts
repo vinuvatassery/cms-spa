803cc734aa58056137fb60cc0ab1e1d84ad42be6
Dinesh <dinesh.panneerselvam@speridian.com>
2023-11-08 20:09:35 +0530

    chart add first push

--- cms/libs/dashboard/feature-dashboard/src/lib/containers/dashboard-page/dashboard-page.component.ts
+++ cms/libs/dashboard/feature-dashboard/src/lib/containers/dashboard-page/dashboard-page.component.ts
@@ -5,7 +5,9 @@ import { CaseFacade, SearchHeaderType } from '@cms/case-management/domain';
 import { DashboardFacade } from '@cms/dashboard/domain';
 /** Services **/
 import { LocalStorageService } from '@cms/shared/util-core';
-
+import { SeriesLabelsContentArgs } from '@progress/kendo-angular-charts';
+import { TileLayoutReorderEvent } from "@progress/kendo-angular-layout";
+import { TileLayoutResizeEvent } from "@progress/kendo-angular-layout";
 @Component({
   selector: 'dashboard-dashboard-page',
   templateUrl: './dashboard-page.component.html',
@@ -15,7 +17,7 @@ import { LocalStorageService } from '@cms/shared/util-core';
 export class DashboardPageComponent implements OnInit {
   /** Public properties **/
   dashboard$ = this.dashboardFacade.dashboard$;
-
+  public events: string[] = [];
   /** Constructor **/
   constructor(
     private readonly dashboardFacade: DashboardFacade,
@@ -29,6 +31,23 @@ export class DashboardPageComponent implements OnInit {
     this.loadDashboard();
   }
 
+
+  public onReorder(e: TileLayoutReorderEvent): void {
+    this.log(e, "reorder");
+  }
+
+  public onResize(e: TileLayoutResizeEvent): void {
+    this.log(e, "resize");
+  }
+
+  private log(
+    event: TileLayoutReorderEvent | TileLayoutResizeEvent,
+    eventName: string
+  ): void {
+ 
+ 
+    this.events.unshift(`${eventName}: ${JSON.stringify(event)}`);
+  }
   /** Private methods **/
   private loadDashboard(): void {
     this.dashboardFacade.loadDashboard();
@@ -38,4 +57,94 @@ export class DashboardPageComponent implements OnInit {
   testLocalStorageService() {
     this.localStorageService.setItem('test', 'test');
   }
+
+
+  public clientByStatus = [
+    {
+      kind: "Hydroelectric",
+      share: 0.175,
+    },
+    {
+      kind: "Nuclear",
+      share: 0.238,
+    },
+    {
+      kind: "Coal",
+      share: 0.118,
+    },
+    {
+      kind: "Solar",
+      share: 0.052,
+    },
+    {
+      kind: "Wind",
+      share: 0.225,
+    },
+    {
+      kind: "Other",
+      share: 0.192,
+    },
+  ];
+  public clientByStatusLabelContent(e: SeriesLabelsContentArgs): string {
+    return e.category;
+  }
+
+  public clientByGroup = [
+    {
+      kind: "Hydroelectric",
+      share: 0.175,
+    },
+    {
+      kind: "Nuclear",
+      share: 0.238,
+    },
+    {
+      kind: "Coal",
+      share: 0.118,
+    },
+    {
+      kind: "Solar",
+      share: 0.052,
+    },
+    {
+      kind: "Wind",
+      share: 0.225,
+    },
+    {
+      kind: "Other",
+      share: 0.192,
+    },
+  ];
+  public clientByGroupLabelContent(e: SeriesLabelsContentArgs): string {
+    return e.category;
+  } 
+  public pharmacyClaims = [
+    {
+      kind: "Hydroelectric",
+      share: 0.175,
+    },
+    {
+      kind: "Nuclear",
+      share: 0.238,
+    },
+    {
+      kind: "Coal",
+      share: 0.118,
+    },
+    {
+      kind: "Solar",
+      share: 0.052,
+    },
+    {
+      kind: "Wind",
+      share: 0.225,
+    },
+    {
+      kind: "Other",
+      share: 0.192,
+    },
+  ];
+  public pharmacyClaimsLabelContent(e: SeriesLabelsContentArgs): string {
+    return e.category;
+  } 
 }
