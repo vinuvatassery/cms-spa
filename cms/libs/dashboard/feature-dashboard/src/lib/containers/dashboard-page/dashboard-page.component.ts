df029f34d90c7f7fe4374111e9951f5b05909d4e
Dinesh <dinesh.panneerselvam@speridian.com>
2023-11-09 17:11:09 +0530

    dashboard click option

--- cms/libs/dashboard/feature-dashboard/src/lib/containers/dashboard-page/dashboard-page.component.ts
+++ cms/libs/dashboard/feature-dashboard/src/lib/containers/dashboard-page/dashboard-page.component.ts
@@ -18,6 +18,7 @@ export class DashboardPageComponent implements OnInit {
   /** Public properties **/
   dashboard$ = this.dashboardFacade.dashboard$;
   public events: string[] = [];
+  isReorderEnable = false;
   /** Constructor **/
   constructor(
     private readonly dashboardFacade: DashboardFacade,
@@ -30,7 +31,9 @@ export class DashboardPageComponent implements OnInit {
     this.caseFacade.enableSearchHeader(SearchHeaderType.CaseSearch);
     this.loadDashboard();
   }
-
+  editDashboardClicked(){
+    this.isReorderEnable = true;
+  }
 
   public onReorder(e: TileLayoutReorderEvent): void {
     this.log(e, "reorder");
@@ -61,63 +64,50 @@ export class DashboardPageComponent implements OnInit {
 
   public clientByStatus = [
     {
-      kind: "Hydroelectric",
-      share: 0.175,
+      kind: "ACCEPT",
+      share: 70,
     },
     {
-      kind: "Nuclear",
-      share: 0.238,
+      kind: "PENDING",
+      share: 5,
     },
     {
-      kind: "Coal",
-      share: 0.118,
+      kind: "WAITLIST",
+      share: 8,
     },
     {
-      kind: "Solar",
-      share: 0.052,
-    },
-    {
-      kind: "Wind",
-      share: 0.225,
-    },
-    {
-      kind: "Other",
-      share: 0.192,
-    },
+      kind: "RESTRICTED",
+      share: 17,
+    }, 
   ];
-  public clientByStatusLabelContent(e: SeriesLabelsContentArgs): string {
-    return e.category;
-  }
-
+  
   public clientByGroup = [
     {
-      kind: "Hydroelectric",
-      share: 0.175,
+      kind: "UPP",
+      share: 50,
     },
     {
-      kind: "Nuclear",
-      share: 0.238,
+      kind: "GROUP I",
+      share: 5,
     },
     {
-      kind: "Coal",
-      share: 0.118,
+      kind: "GROUP II",
+      share: 7,
     },
     {
-      kind: "Solar",
-      share: 0.052,
+      kind: "BRIDGE",
+      share: 9,
     },
     {
-      kind: "Wind",
-      share: 0.225,
+      kind: "GROUP I / INS GAP",
+      share: 10,
     },
     {
-      kind: "Other",
-      share: 0.192,
+      kind: "GROUP II / INS GAP",
+      share: 11,
     },
   ];
-  public clientByGroupLabelContent(e: SeriesLabelsContentArgs): string {
-    return e.category;
-  } 
+  
   public pharmacyClaims = [
     {
       kind: "Hydroelectric",
