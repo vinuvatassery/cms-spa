758173ea545d3f1c3f4867bc4265cc29abfebc40
Shahab Ud Din <shahab.haider@speridian.com>
2023-11-13 09:12:14 -0800

    Merged PR 3043: 16623 - Populate Imported Claims Tab Grid (SPA)
    Code smells are not from my files.
    ![image.png](https://orhive-devops.speridian.com/DefaultCollection/9d08088e-8ad6-4d7a-b3c0-3021a98f3c5a/_apis/git/repositories/6a887e8c-48aa-4213-b02d-5f523e9aa91e/pullRequests/3043/attachments/image.png)
    
    Related work items: #16888

---
+++ cms/libs/case-management/domain/src/lib/application/approval/imported-claim.facade.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { LoaderService, LoggingService, NotificationSnackbarService, SnackBarNotificationType } from '@cms/shared/util-core';
+import { SortDescriptor } from '@progress/kendo-data-query';
+import { ImportedClaimService } from '../../infrastructure/approval/imported-claim.data.service';
+
+@Injectable({ providedIn: 'root' })
+export class ImportedClaimFacade {
+
+  public sortValueImportedClaimsAPproval = 'clientName';
+  public sortImportedClaimsList: SortDescriptor[] = [{
+    field: this.sortValueImportedClaimsAPproval,
+  }];
+
+  /** Private properties **/ 
+  private ImportedClaimsSubject =  new Subject<any>();
+  /** Public properties **/
+  approvalsImportedClaimsLists$ = this.ImportedClaimsSubject.asObservable();
+
+  constructor(
+    private readonly ImportedClaimService: ImportedClaimService,
+    private readonly loggingService : LoggingService,
+    private readonly notificationSnackbarService : NotificationSnackbarService,
+    private readonly loaderService: LoaderService,
+  ) {
+
+  }
+
+  showHideSnackBar(type : SnackBarNotificationType , subtitle : any)
+  {
+      if(type == SnackBarNotificationType.ERROR)
+      {
+        const err= subtitle;
+        this.loggingService.logException(err)
+      }
+        this.notificationSnackbarService.manageSnackBar(type,subtitle)
+        this.hideLoader();
+  }
+
+  showLoader()
+  {
+    this.loaderService.show();
+  }
+
+  hideLoader()
+  {
+    this.loaderService.hide();
+  }
+
+  loadImportedClaimsLists(gridSetupData: any) { 
+    this.ImportedClaimService.loadImportedClaimsListServices(gridSetupData).subscribe(
+      {
+        next: (dataResponse: any) => {
+          const gridView = {
+            data: dataResponse["items"],
+            total: dataResponse["totalCount"]
+          };
+            this.ImportedClaimsSubject.next(gridView);
+        },
+        error: (err) => {
+          this.showHideSnackBar(SnackBarNotificationType.ERROR , err)
+        },
+      }
+    );
+  }
+}
