c8c1334e1dd218ad8b084bfaf5d92bd4c509f991
Kiran Bosco <Kiran.Prakash@speridian.com>
2023-11-21 04:43:50 -0800

    Merged PR 3231: Imported claims grid UI changes
    Imported claims grid UI  - Search, Export, Sort, Filter
    All tab & Pending approval menu counters.
    
    Related work items: #12060

--- cms/libs/shared/ui-common/src/lib/enums/menu-badge.enum.ts
+++ cms/libs/shared/ui-common/src/lib/enums/menu-badge.enum.ts
@@ -1,5 +1,6 @@
 export enum MenuBadge {
     financialManagement = "FINANCIAL_MANAGEMENT",
     fundsAndPcas = "FUNDSANDPCAS",
-    pendingApprovals = "PENDING_APPROVALS"
+    pendingApprovals = "PENDING_APPROVALS",
+    productivityTools = "PRODUCTIVITY_TOOLS",
 }
