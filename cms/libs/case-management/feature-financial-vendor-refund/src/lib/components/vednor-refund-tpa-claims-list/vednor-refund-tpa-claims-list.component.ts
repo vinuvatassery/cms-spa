16229f3b568f27835e92d4825df9a91537c37602
Dinesh <dinesh.panneerselvam@speridian.com>
2023-11-24 18:19:06 +0530

    VednorRefundTpaClaims components created

---
+++ cms/libs/case-management/feature-financial-vendor-refund/src/lib/components/vednor-refund-tpa-claims-list/vednor-refund-tpa-claims-list.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-vednor-refund-tpa-claims-list',
+  templateUrl: './vednor-refund-tpa-claims-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VednorRefundTpaClaimsListComponent {}
---
+++ cms/libs/case-management/feature-financial-vendor-refund/src/lib/components/vednor-refund-tpa-selected-claims-list/vednor-refund-tpa-selected-claims-list.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-vednor-refund-tpa-selected-claims-list',
+  templateUrl: './vednor-refund-tpa-selected-claims-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VednorRefundTpaSelectedClaimsListComponent {}
