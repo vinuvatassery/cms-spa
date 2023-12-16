6b78d688106e0b48396d03cf20f16fcd276eeb94
Jojish Pathrose <jojish.pathrose@speridian.com>
2023-12-15 18:00:16 -0700

    #7900-(fix): temporary fix to remove QA release block

--- cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-process-list/financial-claims-process-list.component.ts
+++ cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-process-list/financial-claims-process-list.component.ts
@@ -369,7 +369,7 @@ export class FinancialClaimsProcessListComponent implements OnChanges , OnInit ,
 
   gridDataHandle() {
     this.financialClaimsProcessGridLists$.subscribe((data: any) => {
-      data.data = this.sortRecordsByCreationTime(data); 
+      //data.data = this.sortRecordsByCreationTime(data); 
       this.gridDataResult = data;
       this.gridFinancialClaimsProcessDataSubject.next(this.gridDataResult);
       if (data?.total >= 0 || data?.total === -1) {
