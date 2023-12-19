12ceb8d52ceeae09e7b0e71da08b25ef5611406c
Dinesh <dinesh.panneerselvam@speridian.com>
2023-12-19 13:48:49 +0530

    profile account popover close fixed

--- cms/libs/shared/ui-common/src/lib/auth/login-status/login-status.component.ts
+++ cms/libs/shared/ui-common/src/lib/auth/login-status/login-status.component.ts
@@ -1,28 +1,34 @@
 /** Angular **/
-import { Component, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ElementRef,
+  HostListener,
+  ViewEncapsulation,
+} from '@angular/core';
 /** Services **/
 import { AuthService } from '@cms/shared/util-oidc';
 @Component({
   selector: 'common-login-status',
   templateUrl: './login-status.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginStatusComponent {
   /** Constructor **/
- 
+  @ViewChild('accountSettingsPopover', { read: ElementRef })
+  public accountSettingsPopover!: ElementRef;
+  @ViewChild('profileAnchor')
+  profileAnchor!: ElementRef;
+
   isAccountSettingsPopup = false;
   isProfilePopoverOpen = false;
-  constructor(private authService: AuthService) { }
-
-  @ViewChild('anchor')
-  anchor!: ElementRef;
-  @ViewChild('popup', { read: ElementRef })
-  popup!: ElementRef;
-
   data: Array<any> = [{}];
   popupClass = 'user-setting-dropdown';
- 
 
+  
+  constructor(private authService: AuthService) {}
   user() {
     return this.authService.getUser();
   }
@@ -40,6 +46,40 @@ export class LoginStatusComponent {
     return this.authService.isAuthenticated;
   }
 
-  onCloseAccountSettingsClicked() { this.isAccountSettingsPopup = false; }
-  onAccountSettingsClicked() { this.isAccountSettingsPopup = true; }
+  @HostListener('document:keydown', ['$event'])
+  public keydown(event: KeyboardEvent): void {
+    if (event) {
+      if (event.code === 'Escape') {
+        this.toggleProfilePopoverOpen(false);
+      }
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  public documentClick(event: KeyboardEvent): void {
+    if (event) {
+      if (!this.contains(event.target)) {
+        this.toggleProfilePopoverOpen(false);
+      }
+    }
+  }
+
+  public toggleProfilePopoverOpen(show?: boolean): void {
+    this.isProfilePopoverOpen = show ?? !this.isProfilePopoverOpen;
+  }
+
+  private contains(target: any): boolean {
+    return (
+      this.profileAnchor.nativeElement.contains(target) ||
+      (this.accountSettingsPopover
+        ? this.accountSettingsPopover.nativeElement.contains(target)
+        : false)
+    );
+  }
+  onCloseAccountSettingsClicked() {
+    this.isAccountSettingsPopup = false;
+  }
+  onAccountSettingsClicked() {
+    this.isAccountSettingsPopup = true;
+  }
 }
