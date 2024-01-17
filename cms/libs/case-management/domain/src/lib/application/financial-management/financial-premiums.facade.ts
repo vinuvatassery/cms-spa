faa458b0e8cc4bcc5d13ee30d4b429319518ec7b
Anoop V J <anoop.jose@speridian.com>
2024-01-17 20:47:46 +0530

    changes

--- cms/libs/case-management/domain/src/lib/application/financial-management/financial-premiums.facade.ts
+++ cms/libs/case-management/domain/src/lib/application/financial-management/financial-premiums.facade.ts
@@ -655,9 +655,9 @@ batchPremium(batchPremiums: BatchPremium, claimsType: string) {
       return this.financialPremiumsDataService.removeSelectedPremiums(selectedPremiumPayments, premiumsType);
     }
 
-    checkWarrantNumber(batchId:any,warrantNumber:any,vendorId:any){
+    checkWarrantNumber(batchId:any,warrantNumber:any,vendorId:any,premiumType:any){
       this.warrantNumberChangeLoaderSubject.next(true);
-      this.financialPremiumsDataService.checkWarrantNumber(batchId,warrantNumber,vendorId).subscribe({
+      this.financialPremiumsDataService.checkWarrantNumber(batchId,warrantNumber,vendorId,premiumType).subscribe({
         next: (dataResponse:any) => {
           this.warrantNumberChangeSubject.next(dataResponse);
           this.warrantNumberChangeLoaderSubject.next(false);
