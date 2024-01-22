bd21df8c971cd899c5bb62b5e8dc264c3cd12a4a
Vikas Birendraprasad Shukla <vikas.shukla@speridian.com>
2024-01-22 13:56:05 +0530

    (fix)-Hotfix#19529 #19322 #17716 #19506 #19570 and #19691
    (fix)-Hotfix#19529 #19322 #17716 #19506 #19570 and #19691

--- cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-batches-reconcile-payments/financial-claims-batches-reconcile-payments.component.ts
+++ cms/libs/case-management/feature-financial-claims/src/lib/components/financial-claims-batches-reconcile-payments/financial-claims-batches-reconcile-payments.component.ts
@@ -168,6 +168,7 @@ export class FinancialClaimsBatchesReconcilePaymentsComponent implements OnInit,
   ];
 
   warrantCalculationArray:any[]=[];
+  warrantInfoArray:any[]=[];
   warrantNumberChanged: any = false;
   /** Constructor **/
   constructor(private route: Router,   private dialogService: DialogService, public activeRoute: ActivatedRoute,
@@ -962,6 +963,8 @@ export class FinancialClaimsBatchesReconcilePaymentsComponent implements OnInit,
       }
       this.claimReconcileCount =  this.reconcilePaymentGridUpdatedResult.filter((x: any) => x.warrantNumberChanged && x.entityId == this.entityId  && x.checkNbr !== undefined && x.checkNbr !== '').length;
       this.loadReconcilePaymentSummary(ReconcilePaymentResponseDto);
+      this.warrantInfoArray = [];
+      this.warrantInfoArray = ReconcilePaymentResponseDto.warrantCalculation;      
     }
 
     loadReconcilePaymentSummary(ReconcilePaymentResponseDto:any)
@@ -978,7 +981,8 @@ export class FinancialClaimsBatchesReconcilePaymentsComponent implements OnInit,
       pageSize:event.pagesize,
       sort:event.sortColumn,
       sortType:event.sortType,
-      filter:event.filter
+      filter:event.filter,
+      warrantCalculation : event.warrantCalculation
     });
   }
   getItemNumber() {
--- cms/libs/case-management/feature-financial-premiums/src/lib/components/financial-premiums-batches-reconcile-payments/financial-premiums-batches-reconcile-payments.component.ts
+++ cms/libs/case-management/feature-financial-premiums/src/lib/components/financial-premiums-batches-reconcile-payments/financial-premiums-batches-reconcile-payments.component.ts
@@ -166,7 +166,8 @@ export class FinancialPremiumsBatchesReconcilePaymentsComponent implements OnIni
   warrantNumberChanged: any = false;
   checkingPaymentRequest!:any;
   loadType:any = null;
-  loadTypeAllPayments:any = LoadTypes.allPayments
+  loadTypeAllPayments:any = LoadTypes.allPayments;
+  warrantInfoArray:any[]=[];
 
   /** Constructor **/
   constructor(private route: Router,   private dialogService: DialogService, 
@@ -982,6 +983,8 @@ export class FinancialPremiumsBatchesReconcilePaymentsComponent implements OnIni
       }
 
       this.loadIPBreakoutSummary(ReconcilePaymentResponseDto);
+      this.warrantInfoArray = [];
+      this.warrantInfoArray = ReconcilePaymentResponseDto.warrantCalculation;
     }
 
     loadIPBreakoutSummary(ReconcilePaymentResponseDto:any)
@@ -998,7 +1001,8 @@ export class FinancialPremiumsBatchesReconcilePaymentsComponent implements OnIni
       pageSize:event.pagesize,
       sort:event.sortColumn,
       sortType:event.sortType,
-      filter:event.filter
+      filter:event.filter,
+      warrantCalculation : event.warrantCalculation
     });
   }
   getItemNumber() {
