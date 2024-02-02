e9e9ea83c0f6570bf6654557b1f2a5e8ba169922
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-02-02 07:31:37 +0500

    update

--- cms/libs/system-interface/feature-dashboard/src/lib/containers/web-service-logs-page/web-service-logs-page.component.ts
+++ cms/libs/system-interface/feature-dashboard/src/lib/containers/web-service-logs-page/web-service-logs-page.component.ts
@@ -24,7 +24,7 @@ export class WebServiceLogsPageComponent implements OnInit {
   sortValue = this.systemInterfaceDashboardFacade.sortValue;
   sortType = this.systemInterfaceDashboardFacade.sortType;
 
-  webLogLovs$ = this.lovFacade.WebInterfaceLogLovs$;
+  webLogLovs$ = this.lovFacade.webInterfaceLogLovs$;
   webLogsList$ = this.systemInterfaceDashboardFacade.webLogLists$;
   webLogsDataList$ :any ;
   constructor(
