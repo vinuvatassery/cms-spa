72fb1e7b4f7e0b804c9ca0310329b07a65531247
LakshmiKambham <103560729+LakshmiKambham@users.noreply.github.com>
2024-02-27 23:34:05 +0530

    Bug 21768: [QA] - "Program Expenses - Widget Filters - Frequency" - The frequency "Annually" is coined as "Yearly" in UI. It should be rephrased to "Annually" per Acceptance Criteria(Step 3)

--- cms/libs/dashboard/feature-dashboard/src/lib/components/widget-program-expenses/widget-program-expenses.component.ts
+++ cms/libs/dashboard/feature-dashboard/src/lib/components/widget-program-expenses/widget-program-expenses.component.ts
@@ -57,7 +57,7 @@ export class WidgetProgramExpensesComponent implements OnInit, OnDestroy  {
                 {Name : 'Quarterly',
                 Value : 'Q'
                 },
-                {Name : 'Yearly',
+                {Name : 'Annually',
                 Value : 'Y'
                 }]
 
