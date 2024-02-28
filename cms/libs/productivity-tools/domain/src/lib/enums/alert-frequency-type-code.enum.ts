bed3192931d548f4204903af696ec6035e7d5db2
Mahesh K Chacko <mahesh.chacko@speridian.com>
2024-02-27 21:53:44 -0700

    Feature 11670: To-Do Items

---
+++ cms/libs/productivity-tools/domain/src/lib/enums/alert-frequency-type-code.enum.ts
@@ -0,0 +1,9 @@
+export enum AlertFrequencyTypeCode {
+  Never = 'NEVER',
+  Daily = 'DAILY',
+  Weekly = 'WEEKLY',
+  Monthy = 'MONTHLY',
+  Second = 'SECOND',
+  Hour = 'HOUR',
+  Minute = 'MINUTE',
+}
