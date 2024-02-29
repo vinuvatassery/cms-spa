c9cc7b8d3d68e58709a8aa45cd0b2f81a8445e02
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-02-29 22:29:21 +0500

    update

--- cms/libs/system-interface/feature-dashboard/src/lib/components/exception-detail/exception-detail.component.ts
+++ cms/libs/system-interface/feature-dashboard/src/lib/components/exception-detail/exception-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { GridFilterParam, SystemInterfaceDashboardFacade } from '@cms/system-interface/domain';
 import { Subject } from 'rxjs';
@@ -8,44 +8,48 @@ import { Subject } from 'rxjs';
   templateUrl: './exception-detail.component.html',
   styleUrls: ['./exception-detail.component.scss'],
 })
-export class ExceptionDetailComponent {
+export class ExceptionDetailComponent implements OnInit {
   public formUiStyle: UIFormStyle = new UIFormStyle();
   @Input() fileId: any;
   @Input() interfaceTypeCode: any;
   @Input() processTypeCode: any;
-  batchLogExcptionListsSubject = new Subject<any>();
-  batchLogExcptionLists$ =  this.batchLogExcptionListsSubject.asObservable();
- constructor(
+  batchLogExceptionListsSubject = new Subject<any>();
+  batchLogExceptionLists$ = this.batchLogExceptionListsSubject.asObservable();
+
+  constructor(
     private systemInterfaceDashboardFacade: SystemInterfaceDashboardFacade
   ) { }
+
   public state!: any;
+
   ngOnInit(): void {
     this.initializeGrid();
+    alert(this.interfaceTypeCode)
   }
+
   gridLoaderShow = false
+
   initializeGrid() {
     this.gridLoaderShow = true;
     const param = new GridFilterParam(
       this.state?.skip ?? 0,
       this.state?.take ?? 0,
       JSON.stringify({ logic: 'and', filters: [] }));
-     this.systemInterfaceDashboardFacade.getBatchLogExceptionsLists(this.fileId,this.interfaceTypeCode,this.processTypeCode, param) .subscribe({
+    this.systemInterfaceDashboardFacade.getBatchLogExceptionsLists(this.fileId, this.interfaceTypeCode, this.processTypeCode, param).subscribe({
       next: (dataResponse) => {
         const gridView = {
           data: dataResponse["items"],
           total: dataResponse["totalCount"]
         };
         this.gridLoaderShow = false;
-        this.batchLogExcptionListsSubject.next(gridView);
+        this.batchLogExceptionListsSubject.next(gridView);
       },
       error: (err) => {
         this.gridLoaderShow = false;
-      
-      },
-    });;
-  
-    }
-    
 
-  };
+      },
+    });
+  }
+
+};
 
