a60395df994cd59a2bc44a1f66d8c02485f1eceb
Mahesh Chacko <mahesh.chacko@speridian.com>
2024-03-08 12:40:01 -0800

    Merged PR 4559: (fix) :fix #22133
    bug fix #22133
    
    Related work items: #22133

--- cms/libs/dashboard/feature-dashboard/src/lib/components/widget-pharmacy-claims/widget-pharmacy-claims.component.ts
+++ cms/libs/dashboard/feature-dashboard/src/lib/components/widget-pharmacy-claims/widget-pharmacy-claims.component.ts
@@ -108,10 +108,7 @@ export class WidgetPharmacyClaimsComponent implements OnInit {
       CountOrAmount: this.selectedDataCount,
       TimeFrame: this.selectedTimeFrame,
     };
-    this.widgetFacade.loadPharmacyClaimsChart(
-      this.dashboardId,
-      payload
-    );
+    this.widgetFacade.loadPharmacyClaimsChart( this.dashboardId,      payload    );
     this.widgetFacade.pharmacyClaimsChart$
       .pipe(takeUntil(this.destroy$))
       .subscribe({
