c66dcd1a49dc3609b16c843384bbeee81b9d8fe0
Dinesh Panneerselvam <dinesh.panneerselvam@speridian.com>
2024-03-11 09:35:46 -0700

    Merged PR 4554: Accessibility Issue Fixes
    Updated the grid button based on the IF condition it will show
    Updated the common No data component in the grid
    and fixed a few bugs related to accessibility
    
    Related work items: #20559, #20943, #22172

--- cms/libs/shared/ui-common/src/index.ts
+++ cms/libs/shared/ui-common/src/index.ts
@@ -61,4 +61,5 @@ export * from './lib/document-upload/document-upload.component';
 export * from './lib/clients-attachment/clients-attachment.component';
 export * from './lib/vendor-details/vendor-details.component';
 export * from './lib/directives/placeholder.directive';
+export * from './lib/grid-no-records/grid-no-records.component';
 export * from './lib/read-more/read-more.component';
