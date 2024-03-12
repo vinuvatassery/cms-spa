b156c6bb32c66bd398312069f3c2c09817c127fd
LakshmiKambham <103560729+LakshmiKambham@users.noreply.github.com>
2024-03-12 18:57:19 +0530

    (feat) : User Story 2386: Adding/Creating a New Reminder

--- cms/libs/productivity-tools/feature-todo/src/lib/containers/todo-page/todo-page.component.ts
+++ cms/libs/productivity-tools/feature-todo/src/lib/containers/todo-page/todo-page.component.ts
@@ -38,7 +38,7 @@ export class TodoPageComponent implements OnInit {
   @ViewChild('todoList', { static: false })
   todoList!: TodoListComponent;
   selectedAlertId! :any
-  createTodo$ = this.todoFacade.createTodo$
+  createTodo$ = this.todoFacade.curdAlert$
   /** Constructor **/
   constructor(private dialogService: DialogService, 
     public todoFacade: TodoFacade,
@@ -93,11 +93,11 @@ export class TodoPageComponent implements OnInit {
   }
 
   onTodoItemCreateClick(payload:any){
-    this.todoFacade.createTodoItem(payload);
+    this.todoFacade.createAlertItem(payload);
   }
 
   onUpdateTodoItemClick(payload:any){
-    this.todoFacade.updateTodoItem(payload)
+    this.todoFacade.updateAlertItem(payload)
   }
 
   onGetTodoItem($event:any){
