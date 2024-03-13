d36615ac3aa6920a89ae5372142027ef53f4c223
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-03-13 06:02:13 +0500

    update

--- cms/libs/system-interface/domain/src/lib/infrastructure/system-interface-support.service.ts
+++ cms/libs/system-interface/domain/src/lib/infrastructure/system-interface-support.service.ts
@@ -150,7 +150,7 @@ export class SystemInterfaceSupportService {
 
   // distribution ----------------------------------------
   getDistributionList(paginationParameters: any) {
-    return this.http.post(`${this.configurationProvider.appSettings.sysInterfaceApiUrl}` + `/system-interface/interface-support/distribution`, paginationParameters);
+    return this.http.post(`${this.configurationProvider.appSettings.sysInterfaceApiUrl}` + `/system-interface/interface-support/distributions`, paginationParameters);
   }
 
   addDistributionListUser(user: any) {
