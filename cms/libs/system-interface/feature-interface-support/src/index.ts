dc8d776b6e450d5b999ba5d6d61ca11f9701cbf4
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-03-15 21:42:10 +0500

    update

--- cms/libs/system-interface/feature-interface-support/src/index.ts
+++ cms/libs/system-interface/feature-interface-support/src/index.ts
@@ -1,14 +1,8 @@
 export * from './lib/system-interface-feature-interface-support.module';
 export * from './lib/system-interface-feature-interface-support.routing.module';
-
- 
-
 export * from './lib/container/interface-support-page/interface-support-page.component';
-
 export * from './lib/components/support-group/support-group.component';
-
 export * from './lib/components/distribution-lists/distribution-lists.component';
-
 export * from './lib/components/notification-category/notification-category.component';
 export * from './lib/components/deactivate-notification-category/deactivate-notification-category.component';
 export * from './lib/components/deactivate-support-group/deactivate-support-group.component';
