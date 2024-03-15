fa0674ca95a6c7eee1d997f96b47c8d8cbab335f
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-03-15 21:38:55 +0500

    update

--- cms/libs/system-interface/domain/src/lib/infrastructure/system-interface-support.service.ts
+++ cms/libs/system-interface/domain/src/lib/infrastructure/system-interface-support.service.ts
@@ -155,14 +155,11 @@ export class SystemInterfaceSupportService {
   }
 
   addDistributionListUser(user: any) {
-    debugger;
     return this.http.post(`${this.configurationProvider.appSettings.sysInterfaceApiUrl}` + `/system-interface/interface-support/distribution`, user);
   }
 
 
-
   // Notification Category Services 
-
 
   getNotificationCategoryList(paginationParameters: any) {
     return this.http.post(`${this.configurationProvider.appSettings.sysInterfaceApiUrl}` + `/system-interface/interface-support/notification-category/list`, paginationParameters);
