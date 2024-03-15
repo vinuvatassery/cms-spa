7c6a6846dbc1b1fc5b5a146b93c0c75cedc3a4d9
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-03-15 21:46:21 +0500

    update

--- cms/libs/system-interface/feature-interface-support/src/lib/components/remove-support-group/remove-support-group.component.ts
+++ cms/libs/system-interface/feature-interface-support/src/lib/components/remove-support-group/remove-support-group.component.ts
@@ -1,35 +1,33 @@
 import {
-    ChangeDetectionStrategy,
-    Component,
-    EventEmitter,
-    Input,
-    OnInit,
-    Output,
-  } from '@angular/core';
-  import { LoaderService } from '@cms/shared/util-core';
-  
-  @Component({
-    selector: 'remove-support-group',
-    templateUrl: './remove-support-group.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-  })
-  export class RemoveSupportGroupComponent implements OnInit {
-    @Input() notificationGroupId: any;
-    @Output() close = new EventEmitter<any>();
-    @Output() deleteConfimEvent = new EventEmitter<any>();
- 
-  
-    /** Constructor **/
-    constructor(private readonly loaderService: LoaderService,) {}
-  
-    ngOnInit(): void { }
-  
-    onCancelClick() {
-      this.close.emit();
-    }
-    onDeleteConfirm(status: boolean) {
-        this.deleteConfimEvent.emit(status);
-      }
-  
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { LoaderService } from '@cms/shared/util-core';
+
+@Component({
+  selector: 'remove-support-group',
+  templateUrl: './remove-support-group.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RemoveSupportGroupComponent implements OnInit {
+  @Output() close = new EventEmitter<any>();
+  @Output() deleteConfimEvent = new EventEmitter<any>();
+
+
+  /** Constructor **/
+  constructor(private readonly loaderService: LoaderService,) {}
+
+  ngOnInit(): void { }
+
+  onCancelClick() {
+    this.close.emit();
   }
-  
+  onDeleteConfirm(status: boolean) {
+      this.deleteConfimEvent.emit(status);
+    }
+
+}
