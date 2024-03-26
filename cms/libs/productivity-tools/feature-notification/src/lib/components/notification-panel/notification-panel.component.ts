d51f7fca59817f24f7df66e162c6a507e99bf599
Sobia.kiran <sobia.kiran@speridian.com>
2024-03-26 13:41:59 +0500

    reminder_features

--- cms/libs/productivity-tools/feature-notification/src/lib/components/notification-panel/notification-panel.component.ts
+++ cms/libs/productivity-tools/feature-notification/src/lib/components/notification-panel/notification-panel.component.ts
@@ -44,7 +44,7 @@ export class NotificationPanelComponent implements OnInit {
   // data: Array<any> = [{}];
   medicalProviderSearchLoaderVisibility$ = this.financialVendorFacade.medicalProviderSearchLoaderVisibility$
   providerSearchResult$ =this.financialVendorFacade.searchProvider$ 
-  clientSearchLoaderVisibility$ = this.financialRefundFacade.clientSearchLoaderVisibility$;
+  clientSearchLoaderVisibility$= this.financialRefundFacade.clientSearchLoaderVisibility$;
   clientSearchResult$ = this.financialRefundFacade.clients$;
   clientSubject = this.financialRefundFacade.clientSubject;
   searchProviderSubject = this.financialVendorFacade.searchProviderSubject
@@ -147,14 +147,8 @@ export class NotificationPanelComponent implements OnInit {
 
   /** Lifecycle hooks **/
   ngOnInit(): void {
-    this.loadNotificationsAndReminders();
     this.loadSignalrGeneralNotifications();
     this.loadSignalrReminders();
-    this.notificationList$.subscribe((data: any) => {
-      this.alertsData = data;
-      this.cdr.detectChanges();
-      this.loadNotificationsAndReminders;
-    });
   }
 
   /** Private methods */
@@ -247,6 +241,11 @@ export class NotificationPanelComponent implements OnInit {
       show !== undefined ? show : !this.isNotificationPopupOpened;
        
      if(this.isNotificationPopupOpened){
+      this.loadNotificationsAndReminders();
+      this.notificationList$.subscribe((data: any) => {
+        this.alertsData = data;
+        this.cdr.detectChanges();
+      });
         this.viewNotifications();
       }
   }
