6d26b07f3da3756041cffa4ee505e81f27f5f4f0
Dinesh <dinesh.panneerselvam@speridian.com>
2024-03-26 17:24:56 +0530

    #22892 added ui for view template in case, alert banner fixes and height adjust ments

---
+++ cms/libs/case-management/feature-communication/src/lib/components/preview-notification-templates/preview-notification-templates.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'case-management-preview-notification-templates',
+  templateUrl: './preview-notification-templates.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PreviewNotificationTemplatesComponent {}
