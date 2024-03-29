67675c7601f2ed7fa85e2b01908e5415bb91ab0f
Kiran Prakash <Kiran.Prakash@speridian.com>
2024-03-28 23:16:48 -0700

    Event log fab count implementation

--- cms/libs/productivity-tools/feature-event-log/src/lib/containers/event-log-fab-page/event-log-fab-page.component.ts
+++ cms/libs/productivity-tools/feature-event-log/src/lib/containers/event-log-fab-page/event-log-fab-page.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectionStrategy,
   Component,
 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FabMenuFacade } from '@cms/productivity-tools/domain';
 import { LovFacade } from '@cms/system-config/domain';
 
@@ -13,14 +14,32 @@ import { LovFacade } from '@cms/system-config/domain';
 })
 export class EventLogComponentFabPageComponent
 {
+  /** Public properties **/
+  clientId : any;
+  vendorId : any;
+  entityId : any;
+
   eventAttachmentTypeLov$ = this.lovFacade.eventAttachmentTypeLov$
   constructor(
     public readonly fabMenuFacade: FabMenuFacade,
-    private readonly lovFacade: LovFacade
+    private readonly lovFacade: LovFacade,
+    private route: ActivatedRoute,
   ) {
   }
 
-  /** Public properties **/
+  ngOnInit() {
+    debugger;
+      this.clientId = this.route.snapshot.queryParams['id'];
+      this.vendorId = this.route.snapshot.queryParams['v_id'];
+      if(this.clientId){
+        this.entityId = this.clientId.toString();
+      }
+      else if(this.vendorId){
+        this.entityId = this.vendorId.toString();
+      }
+      
+  }
+
   closeAction() {
 
     this.fabMenuFacade.isShownEventLog = !this.fabMenuFacade.isShownEventLog;
