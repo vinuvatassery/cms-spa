ceac868bc67a9640252a3bd1eb781d4068794385
Toqeer Yousaf <toqeer.yousaf@speridian.com>
2024-04-02 01:37:14 +0500

    usps implementation

---
+++ cms/libs/system-interface/domain/src/lib/enums/system-interface-Usps.ts
@@ -0,0 +1,5 @@
+export enum SystemInterfaceUsps {
+  USPS_ADDRESS_VALIDATION_FAILED = "Address Validation Falied",
+  USPS_ADDRESS_VALIDATED = "Address Validated" ,
+  USPS_WEBSERVICE_DOWN = "WebService Down",
+  };
