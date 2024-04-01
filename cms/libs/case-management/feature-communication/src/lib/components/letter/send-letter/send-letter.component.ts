da26af488e37c290e1bae1f9c84da1b6c160de4f
Anoop V J <anoop.jose@speridian.com>
2024-04-01 16:01:24 +0530

    templateTypeCode modified

--- cms/libs/case-management/feature-communication/src/lib/components/letter/send-letter/send-letter.component.ts
+++ cms/libs/case-management/feature-communication/src/lib/components/letter/send-letter/send-letter.component.ts
@@ -349,16 +349,16 @@ export class SendLetterComponent implements OnInit, OnDestroy {
     let templateTypeCode = '';
     switch (this.communicationLetterTypeCode) {
       case CommunicationEventTypeCode.PendingNoticeLetter:
-        templateTypeCode = CommunicationEventTypeCode.PendingLetterSent;
+        templateTypeCode = CommunicationEventTypeCode.PendingLetterGenerated;
         break;
       case CommunicationEventTypeCode.RejectionNoticeLetter:
-        templateTypeCode = CommunicationEventTypeCode.RejectionLetterSent;
+        templateTypeCode = CommunicationEventTypeCode.RejectionLetterGenerated;
         break;
       case CommunicationEventTypeCode.ApprovalNoticeLetter:
-        templateTypeCode = CommunicationEventTypeCode.ApprovalLetterSent;
+        templateTypeCode = CommunicationEventTypeCode.ApprovalLetterGenerated;
         break;
       case CommunicationEventTypeCode.DisenrollmentNoticeLetter:
-        templateTypeCode = CommunicationEventTypeCode.DisenrollmentLetterSent;
+        templateTypeCode = CommunicationEventTypeCode.DisenrollmentLetterGenerated;
         break;
         case CommunicationEventTypeCode.ApplicationAuthorizationLetter || CommunicationEventTypeCode.CerAuthorizationLetter || CommunicationEventTypeCode.ClientLetter || CommunicationEventTypeCode.VendorLetter:
         templateTypeCode = CommunicationEventTypeCode.ClientANdVendorLetterSent;
