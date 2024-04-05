aa95cc467a2f94543fbe295f286866de515753e7
Vikas Birendraprasad Shukla <vikas.shukla@speridian.com>
2024-04-05 16:12:34 +0530

    ViewEmail_ViewLetter_And_ViewText_functionalities

--- cms/libs/productivity-tools/feature-event-log/src/lib/components/event-log-description/event-log-description.component.ts
+++ cms/libs/productivity-tools/feature-event-log/src/lib/components/event-log-description/event-log-description.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ConstantValue } from '@cms/productivity-tools/domain';
 
 @Component({
   selector: 'productivity-tools-event-log-description',
@@ -17,45 +18,138 @@ export class EventLogDescriptionComponent {
   anchorArray:any[]=[];
   data:any="";  
   hasUrl:boolean=false;
-
+  isViewLetter:boolean = false;
+  isViewEmail:boolean = false;
+  isViewSmsText:boolean = false;
+  viewText:string = '';
+  isViewLetterEmailTextDialog:boolean = false;
+  ViewLetter:string = "{View Letter}";
+  ViewEmail:string = "{View Email}";
+  ViewSmsText:string = "{View Text(s)}";
+  
   constructor() {}
 
   ngOnInit() {
     this.formatContent();
-    this.content = this.hasUrl ? this.data : this.content;
+    this.content = this.data;
+  }
+
+  setHasUrl(anchorArray:any)
+  {
+    let array = anchorArray.filter((res: any) =>
+      res.indexOf(this.baseUrl) !== -1
+    );
+    return array.length > 0;
   }
   
   formatContent()
   {
     this.anchorArray=[];
     let anchorArray = this.content.split(this.urlSeparator);
+    this.hasUrl = this.setHasUrl(anchorArray);
+    if(this.content.indexOf(this.baseUrl) !== -1){
+      this.setHasUrlConstructingData(anchorArray);
+    }
+    else if(this.content.indexOf(this.urlSeparator) !== -1 && this.content.indexOf(this.baseUrl) == -1)
+    {
+      debugger;
+      this.setAnchorWithOutBaseUrl(anchorArray);
+    }
+    else
+    {
+      this.setHasViewEmailAddressSMSTextFlag();
+    }
+  }
+
+  setAnchorWithOutBaseUrl(anchorArray:any)
+  {
+    this.data = anchorArray[0];
     anchorArray.forEach((item: any) => {
-      if(item.indexOf(this.baseUrl) !== -1)
-      {
-        let itemDataArray = item.split(this.titleOrlinkSeparator);
+        let itemDataArray = item.split(this.titleOrlinkSeparator); 
+        if(itemDataArray.length>1) 
+        {
+          let object = {
+            url : itemDataArray[0],
+            text : itemDataArray[1],
+            title : itemDataArray[1],
+            isBaseUrlFlag : false , 
+            isFilePathUrl : true     
+          }
+          this.anchorArray.push(object);
+        }
+    });
+    this.hasUrl = anchorArray.length >1;
+  }
+  
+  setHasUrlConstructingData(anchorArray:any)
+  {
+    anchorArray.forEach((item: any) => {
+      if(item.indexOf(this.baseUrl) !== -1){    
+        let itemDataArray = item.split(this.titleOrlinkSeparator);      
         let object = {
           url : itemDataArray[0].replace(this.baseUrl,window.location.origin),
           text : itemDataArray[1],
           title : itemDataArray[1],
-          isUrlFlag : true            
+          isBaseUrlFlag : true ,  
+          isFilePathUrl : false     
         }
         this.anchorArray.push(object);
+        this.data += object.text;
       }
-      else
-      {
+      else{
         let object = {
           url : "",
           text : item,
           title : "",
-          isUrlFlag : false            
+          isBaseUrlFlag : false,  
+          isFilePathUrl : false     
         }
         this.anchorArray.push(object);
-        this.data += item;
+        this.data += object.text;
       }
     });
-    let array = anchorArray.filter((res: any) =>
-      res.indexOf(this.baseUrl) !== -1
-    );
-    this.hasUrl = array.length>0;
   }
+    
+  setHasViewEmailAddressSMSTextFlag()
+  {
+    this.isViewLetter = this.content.indexOf(this.ViewLetter) !== -1;
+    this.isViewEmail = this.content.indexOf(this.ViewEmail) !== -1;
+    this.isViewSmsText = this.content.indexOf(this.ViewSmsText) !== -1;
+    this.data=this.content;
+    this.data = this.data.replace(this.ViewLetter,'');
+    this.data = this.data.replace(this.ViewEmail,'');
+    this.data = this.data.replace(this.ViewSmsText,'');
+    this.hasUrl = (this.isViewLetter || this.isViewEmail || this.isViewSmsText);
+    this.viewText = this.setViewText();
+  }
+  setViewText()
+  {
+    if(this.isViewLetter)
+    {
+      return this.ViewLetter.replace('{','').replace('}','');
+    }
+    if(this.isViewEmail)
+    {
+      return this.ViewEmail.replace('{','').replace('}','');;
+    }
+    if(this.isViewSmsText)
+    {
+      return this.ViewSmsText.replace('{','').replace('}','');;
+    }
+    return "";
+  }
+  
+  getViewFlag()
+  {
+    return (this.isViewEmail || this.isViewLetter || this.ViewSmsText);
+  }
+
+  OpenModalPopUp()
+  {
+    this.isViewLetterEmailTextDialog = true;
+  }
+  onCloseLetterEmailSmsTextClicked()
+  {
+    this.isViewLetterEmailTextDialog = false;
+  } 
 }
