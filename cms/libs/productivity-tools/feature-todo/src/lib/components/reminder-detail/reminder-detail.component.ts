ac7fc71d71c042185bc991180c3edda2d4943ad0
LakshmiKambham <103560729+LakshmiKambham@users.noreply.github.com>
2024-04-07 21:02:09 +0530

    (fix): reminder-detail-time-fix

--- cms/libs/productivity-tools/feature-todo/src/lib/components/reminder-detail/reminder-detail.component.ts
+++ cms/libs/productivity-tools/feature-todo/src/lib/components/reminder-detail/reminder-detail.component.ts
@@ -407,8 +407,9 @@ export class ReminderDetailComponent implements OnInit {
    
     if ( this.clientReminderForm.controls['dueDate'].value && dueDate == todayDate) {
    {
-    if ( this.clientReminderForm.controls['time'].value && timeInMinutes < new Date().getMinutes() 
-    && timeInHours <  new Date().getHours() ) {
+    if ( this.clientReminderForm.controls['time'].value &&
+    (timeInHours ==  new Date().getHours() && timeInMinutes <= new Date().getMinutes())
+    || timeInHours <  new Date().getHours() ) {
       this.clientReminderForm.controls['time'].setErrors({ 'incorrect': true });
       return;
     }
