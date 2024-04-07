e50d344a2bb6139e4f733c0d44231b76e3d2c73e
LakshmiKambham <103560729+LakshmiKambham@users.noreply.github.com>
2024-04-07 18:24:21 +0530

    (fix) : size of todo, reminder-over-due time

--- cms/libs/shared/ui-common/src/lib/todo-detail/todo-detail.component.ts
+++ cms/libs/shared/ui-common/src/lib/todo-detail/todo-detail.component.ts
@@ -254,8 +254,8 @@ if(this.todoDetailsForm.controls['linkTo'].value =='CLIENT'){
 }
     const payload ={
       alertName :  this.todoDetailsForm.controls['title'].value,
-      alertDueDate : dueDate,
-      alertEndDate : endDate,  
+      alertDueDate : this.todoDetailsForm.controls['dueDate'].value,
+      alertEndDate : this.todoDetailsForm.controls['endDate'].value,  
       alertDesc : this.todoDetailsForm.controls['alertDesc'].value,
       entityTypeCode : entityTypeCode,
       entityId :entityId ,
