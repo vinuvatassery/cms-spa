d45bff7c22fe684c43d84fcb589f1fe68a063be5
Dinesh <dinesh.panneerselvam@speridian.com>
2024-04-15 12:51:45 +0530

    #23586 UI review changes done

--- cms/libs/system-interface/feature-interface-support/src/lib/system-interface-feature-interface-support.routing.module.ts
+++ cms/libs/system-interface/feature-interface-support/src/lib/system-interface-feature-interface-support.routing.module.ts
@@ -6,6 +6,9 @@ const routes: Routes = [
         {
           path: '',
           component: InterfaceSupportPageComponent,
+          data: {
+            title: 'Interface Support',
+          },
         },
       
     ];
