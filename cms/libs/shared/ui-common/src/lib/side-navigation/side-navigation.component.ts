ec72d7369f136a8c2d0108eedc3946d49c707761
Shahab.Haider <shahab.haider@speridian.com>
2024-04-29 21:45:49 +0500

    uncomment the code for message count in secondary menu

--- cms/libs/shared/ui-common/src/lib/side-navigation/side-navigation.component.ts
+++ cms/libs/shared/ui-common/src/lib/side-navigation/side-navigation.component.ts
@@ -231,7 +231,7 @@ export class SideNavigationComponent implements OnInit, OnDestroy {
   }
 
   private getDirectMessageCount(){
-    //this.navigationMenuFacade.getDirectMessageCount();
+    this.navigationMenuFacade.getDirectMessageCount();
     this.subscribeToDirectMessageCount();
   }
   subscribeToDirectMessageCount(){
