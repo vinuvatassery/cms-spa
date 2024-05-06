2f599dcd0d76a597e47f1331f097aed54b326c66
Anoop V J <anoop.jose@speridian.com>
2024-05-06 21:51:47 +0530

    changes for the hiv verification section

--- cms/libs/case-management/feature-verification/src/lib/components/hiv-verification-request/hiv-verification-request.component.ts
+++ cms/libs/case-management/feature-verification/src/lib/components/hiv-verification-request/hiv-verification-request.component.ts
@@ -82,6 +82,8 @@ export class HivVerificationRequestComponent implements OnInit, OnDestroy{
   public formUiStyle : UIFormStyle = new UIFormStyle();
   public uploadFileRestrictions: UploadFileRistrictionOptions =
   new UploadFileRistrictionOptions();
+  modifiedUserName:any;
+  updatedUserId:any;
   showDocInputLoader = false;
   hivDocument = new HivVerificationDocument();
   popupClass1 = 'more-action-dropdown app-dropdown-action-list ';
@@ -177,12 +179,19 @@ export class HivVerificationRequestComponent implements OnInit, OnDestroy{
             uid: data?.hivVerification?.documentId,
             documentId: data?.hivVerification?.documentId,
             clientHivVerificationId: data?.clientHivVerificationId,
-            verificationStatusCode: data?.verificationStatusCode
+            verificationStatusCode: data?.verificationStatusCode,
+            verificationStatusDate : data?.verificationStatusDate
           },
         ];
         this.uploadedAttachment = documentData;
         this.userId = data?.creatorId;
-        this.loginUserId = data?.creatorId;
+        if(data?.verificationStatusCode === VerificationStatusCode.Accept || data?.verificationStatusCode === VerificationStatusCode.Reject){
+          this.loginUserId = data?.lastModifiedId;
+          this.modifiedUserName = data?.modifiedUser
+        }
+        else{
+          this.loginUserId = data?.creatorId;
+        }
         this.uploadedDate = data?.verificationUploadedDate;
         this.uploadedBy = data?.uploadedBy
         this.cdr.detectChanges();
@@ -424,16 +433,17 @@ loadPendingEsignRequestInfo(){
     this.verificationFacade.getHivVerificationByMethodCode(this.clientId, ProviderOption.HealthCareProvider)
     .subscribe({
       next: (data: any) =>{
-         if (data?.hivVerification != null && data?.requestedUserName != null) {
-          this.emailSentDate = this.intl.formatDate(new Date(data?.creationTime), "MM/dd/yyyy");
-          this.loginUserName = data?.requestedUserName;
-          this.isEmailFieldVisible = false;
-          this.isSendEmailVisiable = false;
-          this.isSendRequest = true;
-          this.isResendRequest = true;
-          this.providerEmail = data?.hivVerification?.verificationToEmail;
-          this.loginUserId = data?.loginUserId;
-           }else{
+          if (data?.hivVerification != null && data?.requestedUserName != null) {
+            this.emailSentDate = this.intl.formatDate(new Date(data?.creationTime), "MM/dd/yyyy");
+            this.loginUserName = data?.requestedUserName;
+            this.isEmailFieldVisible = false;
+            this.isSendEmailVisiable = false;
+            this.isSendRequest = true;
+            this.isResendRequest = true;
+            this.providerEmail = data?.hivVerification?.verificationToEmail;
+           }
+           else
+           {
             this.isEmailFieldVisible = true;
             this.isSendEmailVisiable = true;
             this.isSendRequest = false;
