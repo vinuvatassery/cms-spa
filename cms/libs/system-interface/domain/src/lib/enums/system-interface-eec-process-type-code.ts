d58d6c895c760c1941379ddadcf0ce84a9efebdd
Toqeer Yousaf <>
2024-05-07 21:17:02 +0500

    eligibiity add

--- cms/libs/system-interface/domain/src/lib/enums/system-interface-eec-process-type-code.ts
+++ cms/libs/system-interface/domain/src/lib/enums/system-interface-eec-process-type-code.ts
@@ -1,6 +1,6 @@
 export enum SystemInterfaceEecProcessTypeCode {
   NEW_ENROLLMENT = "New Enrollment",
-  ELIGIBILITY_CHANGE = "Eligibility Change" ,
+  ELIGIBILITY_RENEWAL= "Eligibility Renewal" ,
   MAINTENANCE = "Maintenance",
   CARD_REQUEST = "Card Request"
   };
