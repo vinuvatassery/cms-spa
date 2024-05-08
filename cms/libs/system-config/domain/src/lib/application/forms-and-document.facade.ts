45cd08e1e90a36e7b77a09858c1349754c77f968
sobia <sobia.kiran@speridian.com>
2024-05-09 00:03:23 +0500

    fixes

--- cms/libs/system-config/domain/src/lib/application/forms-and-document.facade.ts
+++ cms/libs/system-config/domain/src/lib/application/forms-and-document.facade.ts
@@ -1,17 +1,10 @@
 /** Angular **/
 import { Injectable } from '@angular/core';
 
-/** External libraries **/
-import { MailAddress } from '../entities/address-validation';
-import { AddressValidationDataService } from '../infrastructure/address-validation.data.service';
 
 @Injectable({ providedIn: 'root' })
 export class FormsAndDocumentFacade {
 
-    constructor(private readonly addressValidationDataService: AddressValidationDataService,) { }
+    constructor() {}
 
-    /** Public methods **/
-    validate(addres: MailAddress) {
-        return this.addressValidationDataService.validateAddress(addres);
-    }
 }
--- cms/libs/system-config/domain/src/lib/infrastructure/forms-and-document.data.service.ts
+++ cms/libs/system-config/domain/src/lib/infrastructure/forms-and-document.data.service.ts
@@ -1,23 +1,12 @@
 /** Angular **/
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-/** External libraries **/
-/** Providers **/
-import { ConfigurationProvider } from '@cms/shared/util-core';
 
-/** Data services **/
-import { MailAddress, AddressValidation } from '../entities/address-validation';
 
 @Injectable({ providedIn: 'root' })
 export class FormsAndDocumentDataService {
     /** Constructor **/
-    constructor(private readonly http: HttpClient,
-        private configurationProvider: ConfigurationProvider) { }
+    constructor() {}
 
     /** Public methods **/
-    validateAddress(address: MailAddress) {
-        return this.http.get<AddressValidation>(
-            `${this.configurationProvider.appSettings.sysConfigApiUrl}/system-config/address-validation?Address1=${address?.address1}&Address2=${address?.address2}&City=${address?.city}&State=${address?.state}&Zip5=${address?.zip5}`
-        );
-    }
+  
 }
