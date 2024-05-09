e09435f6ed7403f5fad68ce8ca93236e72345940
talha afzal <talha.afzal@speridian.com>
2024-05-09 17:55:38 +0500

    Updated lovs in add user modal

--- cms/libs/system-config/feature-user-management/src/lib/components/user-detail/user-detail.component.ts
+++ cms/libs/system-config/feature-user-management/src/lib/components/user-detail/user-detail.component.ts
@@ -7,7 +7,7 @@ import {
   Output,
   EventEmitter,
 } from '@angular/core';
-import { UserManagementFacade } from '@cms/system-config/domain';
+import { LovFacade, UserManagementFacade } from '@cms/system-config/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 @Component({
   selector: 'system-config-user-detail',
@@ -18,12 +18,27 @@ export class UserDetailComponent implements OnInit {
   @Input() isEditValue!: boolean;
   @Output() isDeactivatePopupOpened = new EventEmitter();
   ddlUserRole$ = this.userManagementFacade.ddlUserRole$;
-  isDeactivateValue !: boolean;
-  public formUiStyle : UIFormStyle = new UIFormStyle();
-  constructor(private userManagementFacade: UserManagementFacade) { }
+  userAccessTypeLov$ = this.lovFacade.userAccessTypeLov$;
+  userRoleTypeCode$ = this.lovFacade.userRoleTypeLov$;
+  caseManagerDomainLov$ = this.lovFacade.caseManagerDomainLov$;
+  isDeactivateValue!: boolean;
+  userAccessTypeLovData: any = null;
+  userRoleTypeCodeLovData: any = null;
+  isCaseManagerSelected = false;
+  isAccessTypeInternal = false;
+
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  constructor(
+    private userManagementFacade: UserManagementFacade,
+    private lovFacade: LovFacade
+  ) {}
 
   ngOnInit(): void {
     this.loadDdlUserRole();
+    this.subscribeLovData();
+    this.lovFacade.getUserAccessTypeLov();
+    this.lovFacade.getRoleTypeLov();
+    this.lovFacade.getCaseManagerDomainLov();
   }
 
   private loadDdlUserRole() {
@@ -33,5 +48,37 @@ export class UserDetailComponent implements OnInit {
   onDeactivateClicked() {
     this.isDeactivatePopupOpened.emit();
     this.isDeactivateValue = true;
+  }
+
+  subscribeLovData() {
+    this.userAccessTypeLov$.subscribe({
+      next: (data) => {
+        this.userAccessTypeLovData = data;
+      },
+      error: (err) => {},
+    });
+
+    this.userRoleTypeCode$.subscribe({
+      next: (data) => {
+        this.userRoleTypeCodeLovData = data;
+      },
+      error: (err) => {},
+    });
+  }
+
+  onRoleValuechange(role: any) {
+    if (role.roleCode == 'CACM') {
+      this.isCaseManagerSelected = true;
+    } else {
+      this.isCaseManagerSelected = false;
+    }
+  }
+
+  onUserAccessValueChange(userAccess: any){
+    if(userAccess.lovCode == 'INTERNAL'){
+      this.isAccessTypeInternal = true;
+    }else{
+      this.isAccessTypeInternal = false;
+    }
   }
 }
