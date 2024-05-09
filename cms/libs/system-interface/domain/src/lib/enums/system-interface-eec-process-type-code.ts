09537774ba2cfb318a37cdae4b424eaa37da7558
Toqeer Yousaf <>
2024-05-09 15:45:40 +0500

    update

--- cms/libs/system-interface/domain/src/lib/enums/system-interface-eec-process-type-code.ts
+++ cms/libs/system-interface/domain/src/lib/enums/system-interface-eec-process-type-code.ts
@@ -1,6 +1,6 @@
 export enum SystemInterfaceEecProcessTypeCode {
   NEW_ENROLLMENT = "New Enrollment",
-  ELIGIBILITY_RENEWAL= "Eligibility Renewal" ,
+  ELIGIBILITY_RENEWAL_REQUEST= "Eligibility Renewal" ,
   MAINTENANCE = "Maintenance",
   CARD_REQUEST = "Card Request"
   };
