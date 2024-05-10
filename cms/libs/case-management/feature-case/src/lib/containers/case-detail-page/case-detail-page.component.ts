e6674d1b4446264933759ea747503ac11fa1f1c0
Lakshmi Kambham <lakshmi.kambham@speridian.com>
2024-05-10 09:40:39 -0700

    Merged PR 5488: (feat) : save-later-amendment
    (feat) : save-later-amendment
    
    Related work items: #23200

--- cms/libs/case-management/feature-case/src/lib/containers/case-detail-page/case-detail-page.component.ts
+++ cms/libs/case-management/feature-case/src/lib/containers/case-detail-page/case-detail-page.component.ts
@@ -7,7 +7,7 @@ import { DateInputSize, DateInputRounded, DateInputFillMode, } from '@progress/k
 import { forkJoin, mergeMap, of, Subscription, tap, first, filter } from 'rxjs';
 
 /** Internal Libraries **/
-import { CommunicationEvents, ScreenType, NavigationType, CaseFacade, WorkflowFacade, ButtonType, CaseStatusCode, ContactFacade, WorkflowTypeCode } from '@cms/case-management/domain';
+import { CommunicationEvents, ScreenType, NavigationType, CaseFacade, WorkflowFacade, ButtonType, CaseStatusCode, ContactFacade, WorkflowTypeCode, ContactInfo } from '@cms/case-management/domain';
 import { UIFormStyle } from '@cms/shared/ui-tpa'
 import { LoaderService, LoggingService, NotificationSnackbarService, SnackBarNotificationType } from '@cms/shared/util-core';
 import { DialogService } from '@progress/kendo-angular-dialog';
@@ -65,6 +65,7 @@ export class CaseDetailPageComponent implements OnInit, OnDestroy {
   sendNewEmailModalDialogService : any;
   paperless$ = this.contactFacade.paperless$;
   paperlessFlag!: any;
+  ifClientHasAddress = false;
   data: Array<any> = [
     {
       text: '',
@@ -397,10 +398,30 @@ export class CaseDetailPageComponent implements OnInit, OnDestroy {
   private addConfirmationPopupSubscription(): void {
     this.showConfirmationPopupSubscription = this.workflowFacade.saveForLaterConfirmationClicked$.subscribe((val) => {
       if (val) {
-        this.isShowSaveLaterPopup = true;
-        this.sendLetterFlag = '';
-        this.currentStatusCode = '';
-        this.isSubmitted = false;
+        if(!this.clientCaseEligibilityId){
+          this.ifClientHasAddress = false;
+          this.isShowSaveLaterPopup = true;
+          return;
+        }
+        this.loaderService.show()
+        this.contactFacade.loadContactInfo(this.workflowFacade.clientId ?? 0, this.workflowFacade.clientCaseEligibilityId ?? '', this.prevClientCaseEligibilityId ?? '').subscribe({
+          next: (data: ContactInfo) => {
+            this.loaderService.hide()
+            console.log(data)
+            if( data.address?.length && data.address?.length >0 || data.email?.email){
+              this.ifClientHasAddress = true;
+            }      
+            else{
+              this.ifClientHasAddress = false;
+            }  
+            this.isShowSaveLaterPopup = true;
+            this.sendLetterFlag = '';
+            this.currentStatusCode = '';
+            this.isSubmitted = false;
+            this.cdr.detectChanges()
+          }
+      });
+     
       }
     });
   }
@@ -457,21 +478,28 @@ export class CaseDetailPageComponent implements OnInit, OnDestroy {
 
   onUpdateCaseStatusClicked() {    
     this.isSubmitted = true;
+ 
     if (this.currentStatusCode != "") {
       this.loaderService.show();
+      if (this.sendLetterFlag == StatusFlag.Yes) {
+        this.workflowFacade.sendLetterEmailFlag = this.sendLetterFlag;
+        this.workflowFacade.saveForLater(true);
+      }
+      else {
+        this.workflowFacade.sendLetterEmailFlag = this.sendLetterFlag;
+        this.workflowFacade.saveForLater(false);
+      }
       this.clientCaseEligibilityId = this.clientCaseEligibilityId == null ? this.workflowFacade.clientCaseEligibilityId : this.clientCaseEligibilityId;
-      this.caseFacade.updateCaseStatus(this.clientCaseId, this.currentStatusCode,this.clientCaseEligibilityId).subscribe({
+   this.workflowFacade.savedForLaterCompleted$
+   .pipe(first((res: any) => res != null))
+   .subscribe(res =>{
+    if(res){
+      this.caseFacade.updateCaseStatus(this.clientCaseId, this.currentStatusCode,this.clientCaseEligibilityId)
+      .subscribe({
         next: (casesResponse: any) => {
           this.loaderService.hide();
           this.workflowFacade.caseStatus = this.currentStatusCode;
-          if (this.sendLetterFlag == StatusFlag.Yes) {
-            this.workflowFacade.sendLetterEmailFlag = this.sendLetterFlag;
-            this.workflowFacade.saveForLater(true);
-          }
-          else {
-            this.workflowFacade.sendLetterEmailFlag = this.sendLetterFlag;
-            this.workflowFacade.saveForLater(false);
-          }
+        
           this.isShowSaveLaterPopup = false;
           this.cdr.detectChanges()
           this.caseFacade.loadActiveSession();
@@ -482,6 +510,9 @@ export class CaseDetailPageComponent implements OnInit, OnDestroy {
           this.caseFacade.showHideSnackBar(SnackBarNotificationType.ERROR, err)
         }
       })
+    }
+   })
+     
     }
   }
 
