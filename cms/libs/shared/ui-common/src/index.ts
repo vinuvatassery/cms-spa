31f61cb688c0f945c162530c1ee4db3628c7a33f
Shahab.Haider <shahab.haider@speridian.com>
2024-05-10 21:23:49 +0500

    added export in index.ts

--- cms/libs/shared/ui-common/src/index.ts
+++ cms/libs/shared/ui-common/src/index.ts
@@ -68,4 +68,5 @@ export * from './lib/grid-no-records/grid-no-records.component';
 export * from './lib/read-more/read-more.component';
 export * from './lib/delete-todo-confirmation/delete-todo-confirmation.component';
 export * from './lib/directives/placeholder.directive';
-export * from './lib/client-attachment-model/client-attachment-model.component'
+export * from './lib/client-attachment-model/client-attachment-model.component';
+export * from './lib/common-form-document-tree/common-form-document-tree.component'
