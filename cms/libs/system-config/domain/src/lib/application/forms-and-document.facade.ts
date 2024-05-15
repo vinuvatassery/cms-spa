ecd2116c3cacd58d3fe08de58f56fa2fd6dec3dc
Sobia Kiran <sobia.kiran@speridian.com>
2024-05-15 09:03:30 -0700

    Merged PR 5572:  feature-upload-multiple-file-and-get-folder-list-spa
    Related work items: #24651

--- cms/libs/system-config/domain/src/lib/application/forms-and-document.facade.ts
+++ cms/libs/system-config/domain/src/lib/application/forms-and-document.facade.ts
@@ -1,18 +1,22 @@
 /** Angular **/
 import { Injectable } from '@angular/core';
 import { FormsAndDocumentDataService } from '../infrastructure/forms-and-document.data.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { LoaderService, LoggingService, NotificationSnackbarService, SnackBarNotificationType } from '@cms/shared/util-core';
 
 
 @Injectable({ providedIn: 'root' })
 export class FormsAndDocumentFacade {
-    private addFolderSubject = new BehaviorSubject<any>([]);
+    private addFolderSubject = new Subject<any>();
     addNewFolder$ =  this.addFolderSubject.asObservable();
-    private formsDocumentsSubject = new BehaviorSubject<any>([]);
+    private formsDocumentsSubject = new Subject<any>();
     formsDocumentsList$ = this.formsDocumentsSubject.asObservable();
-    private folderSortSubject = new BehaviorSubject<any>([]);
+    private folderSortSubject = new Subject<any>();
     folderSort$ = this.folderSortSubject.asObservable();
+    private getFolderSubject = new Subject<any>();
+    getFolder$ =  this.getFolderSubject.asObservable();
+    private uploadFilesSubject = new Subject<any>();
+    uploadFiles$ =  this.uploadFilesSubject.asObservable();
     showLoader() { this.loaderService.show(); }
     hideLoader() { this.loaderService.hide(); }
     constructor(private readonly uploadFormandDocumentService: FormsAndDocumentDataService,
@@ -65,6 +69,7 @@ export class FormsAndDocumentFacade {
             {
                 this.showHideSnackBar(SnackBarNotificationType.SUCCESS,response.message);
                 this.hideLoader();
+                this.loadFolderFile(true);
             } 
         },
           error: (err) => 
@@ -74,4 +79,30 @@ export class FormsAndDocumentFacade {
         },
         })
       }
+
+      getFolderName() {
+        this.uploadFormandDocumentService.getFolderName().subscribe({
+          next:(response) => {
+            this.getFolderSubject.next(response);
+          },
+          error: (err) => {
+            this.showHideSnackBar(SnackBarNotificationType.ERROR, err)
+          },
+           }); 
+        }
+        uploadFiles( formData:any){
+            this.showLoader()
+            this.uploadFormandDocumentService.uploadFiles(formData).subscribe({
+              next:(response) => {
+                this.uploadFilesSubject.next(response);
+                this.hideLoader();
+                this.showHideSnackBar(SnackBarNotificationType.SUCCESS,response.message);
+                this.loadFolderFile(true);
+              },
+              error: (err) => {
+                this.showHideSnackBar(SnackBarNotificationType.ERROR, err)
+                this.hideLoader();
+              },
+               });
+          }
 }
---
+++ cms/libs/system-config/feature-communication/src/lib/components/upload-files/upload-files.component.ts
@@ -0,0 +1,85 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UIFormStyle } from '@cms/shared/ui-tpa';
+
+@Component({
+  selector: 'system-config-upload-files',
+  templateUrl: './upload-files.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UploadFilesComponent implements OnInit {
+  showAttachmentRequiredError: boolean = false;
+  public selectedAttachedFile: any;
+  public uploadedAttachedFile: any;
+  attachedFileValidatorSize: boolean = false;
+  public formUiStyle: UIFormStyle = new UIFormStyle();
+  value: any
+  forms!: FormGroup;
+  @Input()getFolders$: any;
+  attachedFiles: any;
+  isValidateForm= false;
+  multipleFilesUpload = true;
+  @Output() uploadFilesEvent = new EventEmitter<any>();
+  @Output () onCloseUploadFileDetailClicked = new EventEmitter<any>();
+  constructor(public formBuilder: FormBuilder){
+}
+ 
+  ngOnInit(): void {
+    this.forms = this.formBuilder.group({
+      folderName: ['', Validators.required],
+    });
+  }
+  uploadFile(){
+    this.isValidateForm= true;
+    this.showAttachmentRequiredError= true;
+    const formData = new FormData();
+  if (this.selectedAttachedFile){
+    this.showAttachmentRequiredError= false;
+    for (let index = 0; index < this.selectedAttachedFile.length; index++) {
+      const element = this.selectedAttachedFile[index];
+      formData.append('uploadFiles', element);
+    }
+      formData.append("documentTemplateId",this.forms.controls["folderName"].value );
+      this.uploadFilesEvent.emit(formData);
+      this.isValidateForm= false;
+      this.onCloseUploadFileClicked();
+  }
+  }
+  handleFileSelected(event: any) 
+  {  
+    if(event != undefined)
+     {
+           this.selectedAttachedFile=event.files.map(function (option :any) {
+                return option.rawFile;
+        });   ;
+           this.showAttachmentRequiredError = false;
+           this.attachedFileValidatorSize=false;
+           let fileSize = 0;
+           this.selectedAttachedFile.forEach((file:any) => 
+            { fileSize =+ file.size});
+           if (fileSize > 25 * 1024 * 1024)
+           {
+            this.attachedFileValidatorSize = true;
+           }
+           else
+           {
+           this.attachedFileValidatorSize = false;
+            }
+   }
+ }
+ handleFileRemoved(event: any) 
+ {
+  this.selectedAttachedFile= undefined;
+  this.showAttachmentRequiredError = true;
+   this.attachedFileValidatorSize=false;
+  this.attachedFiles = null;
+}
+onCloseUploadFileClicked() {
+  this.onCloseUploadFileDetailClicked.emit(false);
+}
+changeOnfolderName(){
+  if(this.getFolders$){
+    this.isValidateForm= false;
+  }
+}
+}
