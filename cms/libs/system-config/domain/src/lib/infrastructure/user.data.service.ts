76ae55bfc36ef59b711ae46e0b95a50c1fc01eb0
Muhammad Umer <muhammad.umer1@speridian.com>
2024-05-15 12:55:25 +0500

    REVIEW COMMENTS RESOLVED

--- cms/libs/system-config/domain/src/lib/infrastructure/user.data.service.ts
+++ cms/libs/system-config/domain/src/lib/infrastructure/user.data.service.ts
@@ -113,22 +113,6 @@ export class UserDataService {
     );
   }
 
-  loadUsers(): Observable<User[]> {
-    return of([
-      { id: 1, name: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet' },
-      {
-        id: 2,
-        name: 'At vero eos',
-        description: 'At vero eos et accusam et justo duo dolores',
-      },
-      {
-        id: 3,
-        name: 'Duis autem',
-        description: 'Duis autem vel eum iriure dolor in hendrerit',
-      },
-    ]);
-  }
-
   loadUsersData(params: any) {
     const paginationAndSortingDto =
     {
