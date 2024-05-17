cac30a22ffc78e5281e060604f22df5ac2bc1a72
Shahab.Haider <shahab.haider@speridian.com>
2024-05-17 21:45:26 +0500

    fix param for loadfolderfile

--- cms/libs/system-config/feature-communication/src/lib/containers/form-documents-page/form-documents-page.component.ts
+++ cms/libs/system-config/feature-communication/src/lib/containers/form-documents-page/form-documents-page.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActiveInactiveFlag } from '@cms/shared/ui-common';
 import { FormsAndDocumentFacade, LovFacade } from '@cms/system-config/domain';
 
 @Component({
@@ -19,7 +20,11 @@ fileFolderPayload :any
     this.getFolderName();
     this.formsAndDocumentFacade.uploadNewVersionDocument$.subscribe(res =>{
       if(res){
-        this.formsAndDocumentFacade.loadFolderFile(true)
+        var filter={
+          sort : true,
+          active: ActiveInactiveFlag.All
+        }
+        this.formsAndDocumentFacade.loadFolderFile(filter)
       }
   })
   }
