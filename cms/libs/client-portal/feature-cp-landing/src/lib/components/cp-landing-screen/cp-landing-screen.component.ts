5dc8cdd661aa5027e8967ba05a9afb530f439c9f
Mithun Cheruvalath <mithun.cheruvalath@speridian.com>
2024-06-04 15:11:57 +0530

    CP-Landing page styles

--- cms/libs/client-portal/feature-cp-landing/src/lib/components/cp-landing-screen/cp-landing-screen.component.ts
+++ cms/libs/client-portal/feature-cp-landing/src/lib/components/cp-landing-screen/cp-landing-screen.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { BreadCrumbItem } from '@progress/kendo-angular-navigation';
+
+
+
+const defaultItems: BreadCrumbItem[] = [
+  {
+    text: "Home",
+    title: "Home",
+  },
+  {
+    text: "CAREAssist",
+    title: "CAREAssist",
+  },
+
+];
+
 
 @Component({
   selector: 'cms-cp-landing-screen',
   templateUrl: './cp-landing-screen.component.html',
 })
-export class CpLandingScreenComponent {}
+
+
+
+export class CpLandingScreenComponent {
+  dropDowncolumns: any = [
+    'English', 'Arabic' 
+  ];
+
+  showFirstDiv: boolean = true;
+  toggleDivs() {
+    this.showFirstDiv = !this.showFirstDiv;
+  }
+
+  
+
+  
+  public items: BreadCrumbItem[] = [...defaultItems];
+  
+}
+
+
--- cms/libs/client-portal/feature-cp-landing/src/lib/feature-cp-landing.module.ts
+++ cms/libs/client-portal/feature-cp-landing/src/lib/feature-cp-landing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedUiTpaModule } from '@cms/shared/ui-tpa';
+import { SharedUiCommonModule } from '@cms/shared/ui-common';
 import { CpAccountSettingsComponent } from './components/cp-account-settings/cp-account-settings.component';
 import { CpHomeComponent } from './components/cp-home/cp-home.component';
 import { CpMyProfileComponent } from './components/cp-my-profile/cp-my-profile.component';
@@ -9,7 +10,7 @@ import { FeatureCpLandingRoutingModule } from './feature-cp-landing.module.routi
 import { CpLandingScreenComponent } from './components/cp-landing-screen/cp-landing-screen.component';
 
 @NgModule({
-  imports: [CommonModule, SharedUiTpaModule, FeatureCpLandingRoutingModule],
+  imports: [CommonModule, SharedUiTpaModule, FeatureCpLandingRoutingModule, SharedUiCommonModule],
   declarations: [
     ProvidersInformationComponent,
     CpHomeComponent,
