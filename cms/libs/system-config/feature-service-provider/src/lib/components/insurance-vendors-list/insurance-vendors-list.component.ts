e8834e6bc5a41cbfdce1a26cc8a1b949d86de34c
Muhammad Hanif <muhammad.hanif@speridian.com>
2024-06-11 18:18:18 +0500

    Filter and search

--- cms/libs/system-config/feature-service-provider/src/lib/components/insurance-vendors-list/insurance-vendors-list.component.ts
+++ cms/libs/system-config/feature-service-provider/src/lib/components/insurance-vendors-list/insurance-vendors-list.component.ts
@@ -10,9 +10,10 @@ import {
 } from '@angular/core';
 import { UIFormStyle } from '@cms/shared/ui-tpa';
 import { CompositeFilterDescriptor, State, filterBy } from '@progress/kendo-data-query';
-import { GridDataResult } from '@progress/kendo-angular-grid';
+import { FilterService, GridDataResult } from '@progress/kendo-angular-grid';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
+import { LovFacade } from '@cms/system-config/domain';
 
  
 
@@ -67,7 +68,7 @@ export class InsuranceVendorsListComponent implements OnInit, OnChanges{
   @Output() insVendorsFilterColumnEvent = new EventEmitter<any>();
   @Output() exportGridDataEvent = new EventEmitter<any>();
   public state!: State;
-  sortColumn = 'vendorName';
+  sortColumn = 'VendorName';
   sortDir = 'Ascending';
   columnsReordered = false;
   filteredBy = '';
@@ -84,18 +85,30 @@ export class InsuranceVendorsListComponent implements OnInit, OnChanges{
     this.gridInsVendorsDataSubject.asObservable();
   columnDropListSubject = new Subject<any[]>();
   columnDropList$ = this.columnDropListSubject.asObservable();
-  vendorNameTitle ="Vendor Name"
+  vendorNameTitle ="VendorName"
   vendorNameTitleDataSubject = new Subject<any>();
 vendornameTitleData$ = this.vendorNameTitleDataSubject.asObservable();
   filterData: CompositeFilterDescriptor = { logic: 'and', filters: [] };
+  selectedPaymentMethod: string | null = null;showTinSearchWarning = false;
+  statusValue = null;
+  paymentMethodCodeValue = null;
+  paymentMethodVendorlovs:any=[];
+  paymentMethodVendorlov$ = this.lovFacade.paymentMethodVendorlov$;
+  statusFilter: any;
+  columns : any = {
+    vendorName:"VendorName",
+    tin:"Tin",
+  }
   /** Internal event methods **/
   constructor(private route: Router,
     private readonly  cdr :ChangeDetectorRef,
+    private readonly lovFacade: LovFacade,
   ) {
   }
   
   ngOnInit(): void {
     this.loadInsVendorsList(); 
+    this.loadVenderPaymentMethodsLovs();
   }
   ngOnChanges(): void {
     this.state = {
@@ -140,50 +153,164 @@ vendornameTitleData$ = this.vendorNameTitleDataSubject.asObservable();
   }
   
 
-  onChange(data: any) {
-    this.defaultGridState();
+  // onChange(data: any) {
+  //   this.defaultGridState();
   
-    this.filterData = {
-      logic: 'and',
-      filters: [
-        {
-          filters: [
-            {
-              field: this.selectedColumn ?? 'vendorName',
-              operator: 'startswith',
-              value: data,
-            },
-          ],
-          logic: 'and',
-        },
+  //   this.filterData = {
+  //     logic: 'and',
+  //     filters: [
+  //       {
+  //         filters: [
+  //           {
+  //             field: this.selectedColumn ?? 'vendorName',
+  //             operator: 'startswith',
+  //             value: data,
+  //           },
+  //         ],
+  //         logic: 'and',
+  //       },
+  //     ],
+  //   };
+  //   const stateData = this.state;
+  //   stateData.filter = this.filterData;
+  //   this.dataStateChange(stateData);
+  // }
+  
+  onChange(data: any)
+  {
+    this.defaultGridState()
+    let operator= "startswith"
+    // if(this.selectedColumn ==="totalClaims" || this.selectedColumn ==="unreconciledClaims" || this.selectedColumn ==="totalPayments"
+    // || this.selectedColumn ==="unreconciledPayments" || this.selectedColumn ==="insurancePlans"  || this.selectedColumn ==="clients"
+    // || this.selectedColumn ==="totalDrugs")
+    // {
+    //   operator = "eq"
+    // }
+
+    if(this.selectedColumn ==="vendorName")
+    {
+      operator = "contains"
+    }
+    if(this.selectedColumn ==="tin")
+    {
+      operator = "contains"
+    }
+    if(this.selectedColumn ==="tin"){
+      let noOfhypen =   data.split("-").length - 1
+      let index = data.lastIndexOf("-")
+      if(noOfhypen>=1 && (index!==2 && index !==3)){
+        this.showTinSearchWarning = true;
+        return;
+      }else{
+        this.showTinSearchWarning = false
+       data = data.replace("-","")
+      }
+
+    }
+    this.filterData = {logic:'and',filters:[{
+      "filters": [
+          {
+              "field": this.selectedColumn ?? "vendorName",
+              "operator": operator,
+              "value": data
+          }
       ],
-    };
-    const stateData = this.state;
-    stateData.filter = this.filterData;
-    this.dataStateChange(stateData);
+      "logic": "and"
+  }]}
+  const stateData = this.state
+  stateData.filter = this.filterData
+  this.dataStateChange(stateData);
   }
-  
-  defaultGridState() {
+
+  defaultGridState(){
     this.state = {
       skip: 0,
       take: this.pageSizes[0]?.value,
       sort: this.sort,
-      filter: { logic: 'and', filters: [] },
-    };
+      filter:{logic:'and',filters:[]}
+      };
   }
-  
-  onColumnReorder($event: any) {
+
+  onColumnReorder($event : any)
+  {
     this.columnsReordered = true;
   }
   
+  // dataStateChange(stateData: any): void {
+  //   this.sort = stateData.sort;
+  //   this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
+  //   this.sortType = stateData.sort[0]?.dir ?? 'asc';
+  //   this.state = stateData;
+  //   this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+  //   this.loadInsVendorsList();
+  // }
   dataStateChange(stateData: any): void {
+    this.showTinSearchWarning = false;
     this.sort = stateData.sort;
     this.sortValue = stateData.sort[0]?.field ?? this.sortValue;
-    this.sortType = stateData.sort[0]?.dir ?? 'asc';
-    this.state = stateData;
-    this.sortDir = this.sort[0]?.dir === 'asc' ? 'Ascending' : 'Descending';
+    this.sortType = stateData.sort[0]?.dir ?? 'asc'
+    this.state=stateData;
+
+    this.sortColumn = this.columns[stateData.sort[0]?.field];
+    this.sortDir = this.sort[0]?.dir === 'asc'? 'Ascending': 'Descending';
+
+    if(stateData.filter?.filters.length > 0)
+    {
+      let stateFilter = stateData.filter?.filters.slice(-1)[0].filters[0];
+      if(stateFilter.field ==="tin"){
+        let noOfhypen =   stateFilter.value.split("-").length - 1
+        let index = stateFilter.value.lastIndexOf("-")
+        if(noOfhypen>=1 && (index!==2 && index !==3)){
+          this.showTinSearchWarning = true;
+          return;
+        }else{
+          this.showTinSearchWarning = false;
+          stateFilter.value = stateFilter.value.replace("-","")
+        }
+
+      }
+      this.filter = stateFilter.value;
+      this.isFiltered = true;
+      const filterList = []
+      for(const filter of stateData.filter.filters)
+      {
+        filterList.push(this.columns[filter.filters[0].field]);
+      }
+      this.filteredBy =  filterList.toString();
+    }
+    else
+    {
+      this.filter = "";
+      this.isFiltered = false
+    }
+
     this.loadInsVendorsList();
+}
+
+dropdownFilterChange(field:string, value: any, filterService: FilterService): void {
+  filterService.filter({
+      filters: [{
+        field: field,
+        operator: "eq",
+        value:value.lovCode
+    }],
+      logic: "or"
+  });
+
+  if(field == "paymentMethodCode"){
+    this.paymentMethodCodeValue = value;
   }
+  if(field == "status"){
+    this.statusValue = value;
+  }
+  // if(field == "acceptsReportsFlag"){
+  //   this.acceptsReportsFlagValue = value;
+  // }
+}
+public columnChange(e: any) {
+  this.cdr.detectChanges();
+}
+
   
   // updating the pagination infor based on dropdown selection
   pageSelectionChange(data: any) {
@@ -260,4 +387,14 @@ vendornameTitleData$ = this.vendorNameTitleDataSubject.asObservable();
 
     })
   }
+  private loadVenderPaymentMethodsLovs() {
+    this.paymentMethodVendorlov$
+    .subscribe({
+      next: (data: any) => {
+        this.paymentMethodVendorlovs=data;
+      }
+    });
+  }
+  
+
 }
